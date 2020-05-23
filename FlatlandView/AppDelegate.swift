fc15165551d2d294b652f56a17803a9251bec07f
Stuart Rankin <sjrankin@gmail.com>
2020-05-23 21:09:41 +0900

    Initial Commit

---
+++ FlatlandView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FlatlandView
+//
+//  Created by Stuart Rankin on 5/23/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
