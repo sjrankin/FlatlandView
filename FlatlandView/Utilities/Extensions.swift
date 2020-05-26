4f1c6f6ae45d07e16b0812694d0d7749d7ea3f85
Stuart Rankin <sjrankin@gmail.com>
2020-05-26 15:03:28 +0900

    Update Extensions.swift
    Added NSBezierPath.cgPath.

--- FlatlandView/Utilities/Extensions.swift
+++ FlatlandView/Utilities/Extensions.swift
@@ -1080,6 +1080,40 @@ extension NSColor
     }
 }
 
+// MARK: - NSBezierPath extensions.
+
+extension NSBezierPath
+{
+    //https://www.smashingmagazine.com/2017/10/from-ios-to-macos-development/
+    public var cgPath: CGPath
+    {
+        let Path = CGMutablePath()
+        var Points = [CGPoint](repeating: .zero, count: 3)
+        for Index in 0 ..< self.elementCount
+        {
+            let SomeType = self.element(at: Index, associatedPoints: &Points)
+            switch SomeType
+            {
+                case .moveTo:
+                    Path.move(to: Points[0])
+                
+                case .lineTo:
+                    Path.addLine(to: Points[0])
+                
+                case .curveTo:
+                    Path.addCurve(to: Points[2], control1: Points[0], control2: Points[1])
+                
+                case .closePath:
+                    Path.closeSubpath()
+                
+                @unknown default:
+                fatalError("Sneaky extra case value enountered: \(SomeType)")
+            }
+        }
+        return Path
+    }
+}
+
 // MARK: - Array extenions.
 
 /// Array extensions.
