373b68b2fa4719b340b48b063b6de2012b00e1b5
Stuart Rankin <sjrankin@gmail.com>
2020-05-26 15:02:07 +0900

    Update MainView.swift
    Added variables needed by additional code.

--- FlatlandView/MainView.swift
+++ FlatlandView/MainView.swift
@@ -23,6 +23,8 @@ class MainView: NSViewController, MainProtocol, SettingChangedProtocol
         
         BackgroundView.wantsLayer = true
         BackgroundView.layer?.backgroundColor = NSColor.black.cgColor
+        
+                CityTestList = CityList.TopNCities(N: 50, UseMetroPopulation: true)
     }
     
     override var representedObject: Any?
@@ -168,11 +170,40 @@ class MainView: NSViewController, MainProtocol, SettingChangedProtocol
     
     func SettingChanged(Setting: SettingTypes, OldValue: Any?, NewValue: Any?)
     {
-        
+        print("\(Setting.rawValue) changed from \(OldValue) to \(NewValue)")
     }
+    
+    // MARK: - City variables.
+    
+    var CityTestList = [City]()
+    let CityList = Cities()
+    
+    // MARK: - Variables for extensions.
+    
+    var UnescoURL: URL? = nil
+    static var UnescoInitialized = false
+    static var UnescoHandle: OpaquePointer? = nil
+    var WorldHeritageSites: [WorldHeritageSite]? = nil
+    
+    /// Previous percent drawn. Used to prevent constant updates when an update would not result
+    /// in a visual change.
+    var PreviousPercent: Double = -1.0
+    
+        let HalfCircumference: Double = 40075.0 / 2.0
+    
+    var CityLayer: CAShapeLayer? = nil
     
     // MARK: - Interface builder outlets.
     
+    @IBOutlet weak var MainTimeLabelBottom: NSTextField!
+    @IBOutlet weak var MainTimeLabelTop: NSTextField!
+    @IBOutlet weak var SunViewBottom: NSImageView!
+    @IBOutlet weak var SunViewTop: NSImageView!
+    @IBOutlet weak var HourLayer2D: NSView!
+    @IBOutlet weak var GridOverlay: NSView!
+    @IBOutlet weak var CityView2D: NSView!
+    @IBOutlet weak var NightMaskImageView: NSImageView!
+    @IBOutlet weak var FlatViewMainImage: NSImageView!
     @IBOutlet weak var BackgroundView: NSView!
     @IBOutlet weak var FlatView: NSView!
     @IBOutlet weak var GlobeView: SCNView!
