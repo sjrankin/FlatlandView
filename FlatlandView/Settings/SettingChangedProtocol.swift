f6fc4034fb01aa3a615ec61bbbdca09c039c8977
Stuart Rankin <sjrankin@gmail.com>
2020-05-29 14:54:16 +0900

    Update SettingChangedProtocol.swift
    Added comments.

--- FlatlandView/Settings/SettingChangedProtocol.swift
+++ FlatlandView/Settings/SettingChangedProtocol.swift
@@ -11,6 +11,14 @@ import AppKit
 
 protocol SettingChangedProtocol: class
 {
+    /// The ID of the subscriber. This value should not change during the run-time
+    /// of the program.
+    /// - Returns: The ID of the subscriber.
     func SubscriberID() -> UUID
+    
+    /// Handle changed settings. Settings may be changed from anywhere at any time.
+    /// - Parameter Setting: The setting that changed.
+    /// - Parameter OldValue: The value of the setting before the change.
+    /// - Parameter NewValue: The new value of the setting.
     func SettingChanged(Setting: SettingTypes, OldValue: Any?, NewValue: Any?)
 }
