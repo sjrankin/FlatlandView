f5e2d8273d07b458a1479fc3c1a772e0d0083924
Stuart Rankin <sjrankin@gmail.com>
2020-05-30 16:33:25 +0900

    Update ActualMap.swift
    Added AsXML and CustomStringConvertible. Added functions to set values that don't set the IsDirty flag.

--- FlatlandView/Maps/Actual/ActualMap.swift
+++ FlatlandView/Maps/Actual/ActualMap.swift
@@ -10,7 +10,7 @@ import Foundation
 import AppKit
 
 /// Contains information about an actual map on the file system.
-class ActualMap
+class ActualMap: CustomStringConvertible
 {
     /// Get the dirty flag - if this property is true, someone set a property that needs to be saved.
     private(set) public var IsDirty = false
@@ -41,6 +41,14 @@ class ActualMap
         }
     }
     
+    /// Set the map title. The dirty flag is immediately reset.
+    /// - Parameter NewPath: New map title.
+    public func SetTitle(_ NewTitle: String)
+    {
+        Title = NewTitle
+        IsDirty = false
+    }
+    
     /// Get or set the ID of the map (not intended for user consumption).
     public var ID: UUID = UUID()
     {
@@ -48,6 +56,14 @@ class ActualMap
         {
             IsDirty = true
         }
+    }
+    
+    /// Set the map ID. The dirty flag is immediately reset.
+    /// - Parameter NewPath: New ID.
+    public func SetID(_ NewID: UUID)
+    {
+        ID = NewID
+        IsDirty = false
     }
     
     /// Get or set the file path for the 3D map.
@@ -59,6 +75,14 @@ class ActualMap
         }
     }
     
+    /// Set the global map image path. The dirty flag is immediately reset.
+    /// - Parameter NewPath: New file path.
+    public func SetGlobalPath(_ NewPath: String)
+    {
+        GlobalPath = NewPath
+        IsDirty = false
+    }
+    
     /// Get or set the file path for the north-centered 2D map.
     public var NorthPath: String = ""
     {
@@ -68,6 +92,14 @@ class ActualMap
         }
     }
     
+    /// Set the north-centered map image path. The dirty flag is immediately reset.
+    /// - Parameter NewPath: New file path.
+    public func SetNorthPath(_ NewPath: String)
+    {
+        NorthPath = NewPath
+        IsDirty = false
+    }
+    
     /// Get or set the file path for the south-centered 2D map.
     public var SouthPath: String = ""
     {
@@ -75,5 +107,40 @@ class ActualMap
         {
             IsDirty = true
         }
+    }
+    
+    /// Set the south-centered map image path. The dirty flag is immediately reset.
+    /// - Parameter NewPath: New file path.
+    public func SetSouthPath(_ NewPath: String)
+    {
+        SouthPath = NewPath
+        IsDirty = false
+    }
+    
+    /// Returns a run-time description of the contents of `ActualMap`.
+    var description: String
+    {
+        get
+        {
+            var MapData = "    \(Title), \(ID.uuidString)\n"
+            MapData.append("      Global: [\(GlobalPath)]\n")
+            MapData.append("      North: [\(NorthPath)]\n")
+            MapData.append("      South: [\(SouthPath)]\n")
+            return MapData
+        }
+    }
+    
+    /// Return the contents of the run-time class as an XML fragment.
+    func AsXML(_ Indent: Int = 8) -> String
+    {
+        var XString = String(repeating: " ", count: Indent)
+        XString.append("<Map ")
+        XString.append("Name=\"\(Title)\" ")
+        XString.append("ID=\"\(ID.uuidString)\" ")
+        XString.append("Global=\"\(GlobalPath)\" ")
+        XString.append("North=\"\(NorthPath)\" ")
+        XString.append("South=\"\(SouthPath)\"")
+        XString.append("/>")
+        return XString
     }
 }
