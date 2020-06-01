28aa55e27cc44b491ab65c0d3149c752eb6f9489
Stuart Rankin <sjrankin@gmail.com>
2020-06-01 21:59:56 +0900

    Create UserLocationEditorWindow.swift
    Placeholder code for the user location editor window.

---
+++ FlatlandView/SettingsCode/SettingsCode/UserLocationEditorWindow.swift
@@ -0,0 +1,18 @@
+//
+//  UserLocationEditorWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/1/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class UserLocationEditorWindow: NSWindowController
+{
+    func CloseWindow()
+    {
+        self.close()
+    }
+}
