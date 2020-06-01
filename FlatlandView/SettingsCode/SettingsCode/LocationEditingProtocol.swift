ee57998b1db4ff740619a15973b1a8b7ea7b1826
Stuart Rankin <sjrankin@gmail.com>
2020-06-01 21:59:37 +0900

    Create LocationEditingProtocol.swift
    Protocol for editing and adding new user locations.

---
+++ FlatlandView/SettingsCode/SettingsCode/LocationEditingProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  LocationEditingProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/1/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol LocationEditingProtocol: class
+{
+    func AddNewLocation() -> Bool
+    func GetLocationToEdit() -> (Name: String, Latitude: Double, Longitude: Double, Color: NSColor)
+    func SetEditedLocation(Name: String, Latitude: Double, Longitude: Double, Color: NSColor, IsValid: Bool)
+    func CancelEditing()
+}
