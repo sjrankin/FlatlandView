e4ca525b6078f1e6f5c959a447445efa107758ea
Stuart Rankin <sjrankin@gmail.com>
2020-06-02 18:18:23 +0900

    Create CityColorWindow.swift
    Placeholder code for the city color window.

---
+++ FlatlandView/SettingsCode/SettingsCode/CityColor/CityColorWindow.swift
@@ -0,0 +1,15 @@
+//
+//  CityColorWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/2/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class CityColorWindow: NSWindowController
+{
+    
+}
