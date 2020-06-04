c55311d9ef0d04cee2264e8b8af9e273583dca90
Stuart Rankin <sjrankin@gmail.com>
2020-06-04 16:43:15 +0900

    Create AboutDetailsWindow.swift
    Placeholder for the about details window.

---
+++ FlatlandView/About/AboutDetailsWindow.swift
@@ -0,0 +1,14 @@
+//
+//  AboutDetailsWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/4/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class AboutDetailsWindow: NSWindowController
+{
+}
