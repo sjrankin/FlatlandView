d84f5593a29cbd4d61beb487e675f32ee500d611
Stuart Rankin <sjrankin@gmail.com>
2020-06-04 16:43:27 +0900

    Create AboutWindow.swift
    Placeholder for the about dialog window.

---
+++ FlatlandView/About/AboutWindow.swift
@@ -0,0 +1,14 @@
+//
+//  AboutWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/4/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class AboutWindow: NSWindowController
+{
+}
