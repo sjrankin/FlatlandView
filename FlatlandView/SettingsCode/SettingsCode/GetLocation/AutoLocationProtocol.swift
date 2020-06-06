e66be85e909fed8110855e28c40bd9b21daa76d2
Stuart Rankin <sjrankin@gmail.com>
2020-06-06 15:10:57 +0900

    Create AutoLocationProtocol.swift
    Protocol to communicate new location availability.

---
+++ FlatlandView/SettingsCode/SettingsCode/GetLocation/AutoLocationProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  AutoLocationProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/6/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+protocol AutoLocationProtocol: class
+{
+    func HaveNewLocation()
+}
