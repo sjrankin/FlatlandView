c40f8a9351c92ebcdb963988896154e3a9f536e4
Stuart Rankin <sjrankin@gmail.com>
2020-06-06 15:11:23 +0900

    Create GetLocationWindow.swift
    Placeholder for the get location UI window.

---
+++ FlatlandView/SettingsCode/SettingsCode/GetLocation/GetLocationWindow.swift
@@ -0,0 +1,14 @@
+//
+//  GetLocationWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/6/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class GetLocationWindow: NSWindowController
+{
+}
