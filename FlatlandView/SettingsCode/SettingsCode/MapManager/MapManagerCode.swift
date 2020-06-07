d772ec577954ad99bd8c417eb8267ba88719e9f2
Stuart Rankin <sjrankin@gmail.com>
2020-06-07 21:09:23 +0900

    Added
    Added placeholder code/UI for map management.

---
+++ FlatlandView/SettingsCode/SettingsCode/MapManager/MapManagerCode.swift
@@ -0,0 +1,28 @@
+//
+//  MapManagerCode.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/7/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class MapManagerCode: NSViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func HandleOKPressed(_ sender: Any)
+    {
+        self.view.window?.close()
+    }
+    
+    @IBAction func HandleCancelPressed(_ sender: Any)
+    {
+        self.view.window?.close()
+    }
+}
