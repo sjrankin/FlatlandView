f609be7544ea35bf43a0bbe4ef95de315bf10334
Stuart Rankin <sjrankin@gmail.com>
2020-06-16 15:45:26 +0900

    Create LiveDataViewWindow.swift
    Placeholder.

---
+++ FlatlandView/Views/Ancillary/LiveDataView/LiveDataViewWindow.swift
@@ -0,0 +1,14 @@
+//
+//  LiveDataViewWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/16/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class LiveDataViewWindow: NSWindowController
+{
+}
