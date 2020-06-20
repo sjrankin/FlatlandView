637c7cc832eca2ac93d269cd84a1fc00c0d8d17d
Stuart Rankin <sjrankin@gmail.com>
2020-06-20 22:03:18 +0900

    Update MainSettings.swift
    Added code to run the map selection tab. Added code to show live data tables.

--- FlatlandView/SettingsCode/SettingsCode/MainSettings/MainSettings.swift
+++ FlatlandView/SettingsCode/SettingsCode/MainSettings/MainSettings.swift
@@ -31,6 +31,7 @@ class MainSettings: NSViewController, NSTableViewDataSource, NSTableViewDelegate
         InitializeCitySettingsUI()
         InitializeOtherSettings()
         InitializeAsynchronousEarthquakes()
+        InitializeMaps()
     }
     
     // MARK: - 2D Map settings.
@@ -1168,6 +1169,10 @@ class MainSettings: NSViewController, NSTableViewDataSource, NSTableViewDelegate
     
     @IBAction func HandleClosePressed(_ sender: Any)
     {
+        if OriginalMap != LastMap
+        {
+            Settings.SetEnum(LastMap, EnumType: MapTypes.self, ForKey: .MapType)
+        }
         MainDelegate?.Refresh("MainSettings")
         self.view.window?.close()
     }
@@ -1181,6 +1186,12 @@ class MainSettings: NSViewController, NSTableViewDataSource, NSTableViewDelegate
             
             case SunSelector:
                 return SunImageList.count
+            
+            case MapTypeTable:
+                return MapCategoryList.count
+            
+            case MapListTable:
+                return MapList.count
             
             default:
                 return 0
@@ -1205,6 +1216,28 @@ class MainSettings: NSViewController, NSTableViewDataSource, NSTableViewDelegate
         var CellIdentifier = "" 
         switch tableView
         {
+            case MapTypeTable:
+                if tableColumn == tableView.tableColumns[0]
+                {
+                    CellIdentifier = "MapTypeColumn"
+                    CellContents = MapCategoryList[row].rawValue
+                }
+                else
+                {
+                    return nil
+            }
+            
+            case MapListTable:
+                if tableColumn == tableView.tableColumns[0]
+                {
+                    CellIdentifier = "MapNameColumn"
+                    CellContents = MapList[row].rawValue
+                }
+                else
+                {
+                    return nil
+            }
+            
             case UserLocationTable:
                 if tableColumn == tableView.tableColumns[0]
                 {
@@ -1327,6 +1360,129 @@ class MainSettings: NSViewController, NSTableViewDataSource, NSTableViewDelegate
     func CancelEditing()
     {
     }
+    
+    // MARK: - Map selection.
+    
+    func InitializeMaps()
+    {
+        SampleType = Settings.GetEnum(ForKey: .SampleViewType, EnumType: ViewTypes.self, Default: .Globe3D)
+        MapSampleView.image = nil
+        LastMap = Settings.GetEnum(ForKey: .MapType, EnumType: MapTypes.self, Default: .Standard)
+        OriginalMap = LastMap
+        var LastCategory = MapManager.CategoryFor(Map: LastMap)
+        if LastCategory == nil
+        {
+            LastCategory = .Standard
+        }
+        LastSelectedLabel.stringValue = LastMap.rawValue
+        DisplayCurrentMap(LastMap)
+        MapCategoryList = MapManager.GetMapCategories()
+        if let CurrentCategory = MapManager.CategoryFor(Map: LastMap)
+        {
+            MapList = MapManager.GetMapsInCategory(CurrentCategory)
+        }
+        else
+        {
+            MapList.removeAll()
+        }
+        MapTypeTable.reloadData()
+        MapListTable.reloadData()
+        MapTypeTable.action = #selector(HandleMapCategoryClicked)
+        MapListTable.action = #selector(HandleMapListClicked)
+        
+        var CatIndex = MapCategoryList.firstIndex(of: LastCategory!)
+        if CatIndex == nil
+        {
+            CatIndex = 0
+        }
+        MapTypeTable.selectRowIndexes(IndexSet(integer: CatIndex!), byExtendingSelection: false)
+        var MapIndex = MapManager.GetMapsInCategory(LastCategory!).firstIndex(of: LastMap)
+        if MapIndex == nil
+        {
+            MapIndex = 0
+        }
+        MapListTable.selectRowIndexes(IndexSet(integer: MapIndex!), byExtendingSelection: false)
+    }
+    
+    func DisplayCurrentMap(_ Current: MapTypes)
+    {
+        if let Image = MapManager.ImageFor(MapType: Current, ViewType: SampleType)
+        {
+            MapSampleView.image = Image
+        }
+    }
+    
+    @objc func HandleMapCategoryClicked()
+    {
+        let NewCategory = MapCategoryList[MapTypeTable.clickedRow]
+        MapList = MapManager.GetMapsInCategory(NewCategory)
+        MapListTable.reloadData()
+    }
+    
+    @objc func HandleMapListClicked()
+    {
+        let ListIndex = MapListTable.clickedRow
+        LastMap = MapList[ListIndex]
+        LastSelectedLabel.stringValue = LastMap.rawValue
+        DisplayCurrentMap(LastMap)
+        Updated = true
+    }
+    
+    var OriginalMap = MapTypes.BlackWhite
+    var SampleType = ViewTypes.Globe3D
+    var LastMap: MapTypes = .Standard
+    var MapList = [MapTypes]()
+    var MapCategoryList = [MapCategories]()
+    var Updated = false
+    
+    /// This function does nothing but is required for `#selector(HandleMapListClicked)` to function.
+    @IBAction func MapListAction(_ sender: Any)
+    {
+    }
+    
+    /// This function does nothing but is required for `#selector(HandleMapCategoryClicked)` to function.
+    @IBAction func MapTypeAction(_ sender: Any)
+    {
+    }
+    
+    @IBAction func HandleApplyPressed(_ sender: Any)
+    {
+        //Set the updated flag to false. This way, if the user pressed the OK button, the map won't
+        //be reloaded.
+        Updated = false
+        Settings.SetEnum(LastMap, EnumType: MapTypes.self, ForKey: .MapType)
+        OriginalMap = LastMap
+        MainDelegate?.Refresh(#function)
+    }
+    
+    @IBAction func HandleSampleViewChanged(_ sender: Any)
+    {
+        if let Segment = sender as? NSSegmentedControl
+        {
+            switch Segment.selectedSegment
+            {
+                case 0:
+                    SampleType = .FlatNorthCenter
+                
+                case 1:
+                    SampleType = .FlatSouthCenter
+                
+                case 2:
+                    SampleType = .Globe3D
+                
+                default:
+                    return
+            }
+            Settings.SetEnum(SampleType, EnumType: ViewTypes.self, ForKey: .SampleViewType)
+            DisplayCurrentMap(LastMap)
+        }
+    }
+    
+    @IBOutlet weak var SampleViewType: NSSegmentedControl!
+    @IBOutlet weak var LastSelectedLabel: NSTextField!
+    @IBOutlet weak var MapSampleView: NSImageView!
+    @IBOutlet weak var MapTypeTable: NSTableView!
+    @IBOutlet weak var MapListTable: NSTableView!
     
     // MARK: - Confirmation protocol functions.
     
