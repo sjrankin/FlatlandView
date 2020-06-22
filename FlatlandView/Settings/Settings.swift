097f98937728aa2f4d092d79556d04a5e3cd4317
Stuart Rankin <sjrankin@gmail.com>
2020-06-22 17:34:28 +0900

    Update Settings.swift
    Added code to save and get lists of colors for earthquake magnitudes. Added DefaultMagnitudeColors.

--- FlatlandView/Settings/Settings.swift
+++ FlatlandView/Settings/Settings.swift
@@ -636,6 +636,95 @@ class Settings
     
     // MARK: - Special settings.
     
+    /// Parse a single entry in the stored earthquake magnitude level to color.
+    /// - Parameter Mag: Raw magnitude value.
+    /// - Parameter Color: Raw color value.
+    /// - Parameter FinalMag: On success, the magnitude level. On failure, undefined.
+    /// - Parameter FinalColor: On success, the color for the associated magnitude. On failure, undefined.
+    /// - Returns: True if `Mag` and `Color` are well-defined, false otherwise. If false is returned,
+    ///            `FinalMag` and `FinalColor` are undefined.
+    private static func IsValidMagColor(_ Mag: String, _ Color: String, _ FinalMag: inout Double,
+                                        _ FinalColor: inout NSColor) -> Bool
+    {
+        let SomeDouble = Double(Mag)
+        let SomeColor = NSColor(HexString: Color)
+        if SomeDouble == nil || SomeColor == nil
+        {
+            return false
+        }
+        FinalMag = SomeDouble!
+        FinalColor = SomeColor!
+        return true
+    }
+    
+    /// Get a dictionary of earthquake magnitudes to colors.
+    /// - Returns: Dictionary of colors to earthquake magnitude levels.
+    public static func GetMagnitudeColors() -> [EarthquakeMagnitudes: NSColor]
+    {
+        if let Raw = UserDefaults.standard.string(forKey: SettingTypes.EarthquakeMagnitudeColors.rawValue)
+        {
+            let Parts = Raw.split(separator: ";", omittingEmptySubsequences: true)
+            if Parts.count == 6
+            {
+                var ColorDict = [EarthquakeMagnitudes: NSColor]()
+                for Part in Parts
+                {
+                    let SubParts = String(Part).split(separator: ",", omittingEmptySubsequences: true)
+                    if SubParts.count != 2
+                    {
+                        SetMagnitudeColors(DefaultMagnitudeColors(), Notify: false)
+                        return DefaultMagnitudeColors()
+                    }
+                    var FinalMag: Double = 0.0
+                    var FinalColor: NSColor = NSColor.white
+                    if !IsValidMagColor(String(SubParts[0]), String(SubParts[1]), &FinalMag, &FinalColor)
+                    {
+                        SetMagnitudeColors(DefaultMagnitudeColors(), Notify: false)
+                        return DefaultMagnitudeColors()
+                    }
+                    let MagIndex = EarthquakeMagnitudes(rawValue: FinalMag)!
+                    ColorDict[MagIndex] = FinalColor
+                }
+                return ColorDict
+            }
+        }
+        SetMagnitudeColors(DefaultMagnitudeColors(), Notify: false)
+        return DefaultMagnitudeColors()
+    }
+    
+    /// Returns a dictionary of default colors for earthquake magnitudes.
+    /// - Returns: Dictionary of colors to earthquake magnitude levels.
+    public static func DefaultMagnitudeColors() -> [EarthquakeMagnitudes: NSColor]
+    {
+        let ColorDict: [EarthquakeMagnitudes: NSColor] =
+        [
+            .Mag4: NSColor.TeaGreen,
+            .Mag5: NSColor.ArtichokeGreen,
+            .Mag6: NSColor.orange,
+            .Mag7: NSColor.UltraPink,
+            .Mag8: NSColor.Sunglow,
+            .Mag9: NSColor.Scarlet
+        ]
+        return ColorDict
+    }
+    
+    /// Save a set of colors associated with earthquake magnitude levels.
+    /// - Parameter MagColors: A dictionary of colors for magnitude levels.
+    /// - Parameter Notify: If true, subscribers are notified of changes. Defaults to `true`.
+    public static func SetMagnitudeColors(_ MagColors: [EarthquakeMagnitudes: NSColor], Notify: Bool = true)
+    {
+        var Final = ""
+        for (Mag, Color) in MagColors
+        {
+            Final.append("\(Mag.rawValue),\(Color.Hex);")
+        }
+        UserDefaults.standard.set(Final, forKey: SettingTypes.EarthquakeMagnitudeColors.rawValue)
+        if Notify
+        {
+        NotifySubscribers(Setting: .EarthquakeMagnitudeColors, OldValue: nil, NewValue: MagColors)
+        }
+    }
+    
     /// Determines if both the local latitude and local longitude have been set.
     /// - Returns: True if the device location has been set, false if not.
     public static func HaveLocalLocation() -> Bool
