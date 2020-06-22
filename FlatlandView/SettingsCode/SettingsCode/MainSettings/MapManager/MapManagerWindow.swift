07d58c938cf9bdd4cb80463c9c4aae19533c7e39
Stuart Rankin <sjrankin@gmail.com>
2020-06-22 17:36:48 +0900

    Create MapManagerWindow.swift
    Map manager window.

---
+++ FlatlandView/SettingsCode/SettingsCode/MainSettings/MapManager/MapManagerWindow.swift
@@ -0,0 +1,14 @@
+//
+//  MapManagerWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/7/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class MapManagerWindow: NSWindowController
+{
+}
