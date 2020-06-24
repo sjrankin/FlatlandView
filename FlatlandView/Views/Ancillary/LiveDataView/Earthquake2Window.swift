4c68f42d17f27d4bf3984b493ef39cb06ba38f5a
Stuart Rankin <sjrankin@gmail.com>
2020-06-24 20:43:19 +0900

    Create Earthquake2Window.swift
    Window code for the Earthquake2 UI.

---
+++ FlatlandView/Views/Ancillary/LiveDataView/Earthquake2Window.swift
@@ -0,0 +1,35 @@
+//
+//  Earthquake2Window.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/24/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class Earthquake2DWindow: NSWindowController, NSWindowDelegate
+{
+    override func windowDidLoad()
+    {
+        super.windowDidLoad()
+        self.window?.delegate = self
+    }
+    
+    func windowDidResize(_ notification: Notification)
+    {
+        print("Window resized")
+    }
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        if let OldFrame = Settings.GetRect(.EarthquakeViewWindowFrame)
+        {
+            window?.setFrame(OldFrame, display: true)
+        }
+    }
+    
+    @IBOutlet weak var ListSegment: NSSegmentedControl!
+}
