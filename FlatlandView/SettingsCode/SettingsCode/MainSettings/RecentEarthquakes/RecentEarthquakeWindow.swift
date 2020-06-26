6b9f65a706ecfb282429d98ba5ef31ac3778caf6
Stuart Rankin <sjrankin@gmail.com>
2020-06-26 21:24:00 +0900

    Create RecentEarthquakeWindow.swift
    Placeholder.

---
+++ FlatlandView/SettingsCode/SettingsCode/MainSettings/RecentEarthquakes/RecentEarthquakeWindow.swift
@@ -0,0 +1,14 @@
+//
+//  RecentEarthquakeWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/26/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class RecentEarthquakeWindow: NSWindowController
+{
+}
