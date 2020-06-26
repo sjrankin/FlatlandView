c3db66f22f00546e24f621e00a220f9bcc2d50e5
Stuart Rankin <sjrankin@gmail.com>
2020-06-26 17:54:48 +0900

    Update AboutController.swift
    Minor change for test.

--- FlatlandView/About/AboutController.swift
+++ FlatlandView/About/AboutController.swift
@@ -10,7 +10,7 @@ import Foundation
 import AppKit
 import SceneKit
 
-class AboutController: NSViewController
+class AboutController: NSViewController, SCNSceneRendererDelegate
 {
     override func viewDidLoad()
     {
