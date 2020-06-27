3b85cf704a9947b73dadb398b5a15a8471ef93c1
Stuart Rankin <sjrankin@gmail.com>
2020-06-27 15:53:14 +0900

    Create WindowManagement.swift
    Protocol to help ensure windows opened in Flatland are closed at the same time the main window is closed.

---
+++ FlatlandView/WindowManagement.swift
@@ -0,0 +1,15 @@
+//
+//  WindowManagement.swift
+//  FlatlandView
+//
+//  Created by Stuart Rankin on 6/27/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol WindowManagement: class
+{
+    func MainClosing()
+}
