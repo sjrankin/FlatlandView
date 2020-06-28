e890b91f3cdebe8e5b57e38e005b6db08933d2d3
Stuart Rankin <sjrankin@gmail.com>
2020-06-28 13:46:47 +0900

    Create StarField3DWindow.swift
    Placeholder.

---
+++ FlatlandView/SettingsCode/SettingsCode/MainSettings/StarField3D/StarField3DWindow.swift
@@ -0,0 +1,14 @@
+//
+//  StarField3DWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/28/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class StarField3DWindow: NSWindowController
+{
+}
