598796b28da912a7653cb91bc932447e615b2d21
Stuart Rankin <sjrankin@gmail.com>
2020-06-28 13:43:08 +0900

    Update Settings.swift
    Added date-based settings.

--- FlatlandView/Settings/Settings.swift
+++ FlatlandView/Settings/Settings.swift
@@ -527,6 +527,56 @@ class Settings
         NotifySubscribers(Setting: Setting, OldValue: OldValue, NewValue: Value)
     }
     
+    // MARK: - Date-based settings.
+    
+    /// Initialize a date settings. No notification is sent to subscribers.
+    /// - Note: Internally, dates are saved as a double with the value being the number of seconds
+    ///         since 1970.
+    /// - Parameter NewValue: The value to set.
+    /// - Parameter ForKey: The setting key.
+    public static func InitializeDate(_ NewValue: Date, ForKey: SettingTypes)
+    {
+        UserDefaults.standard.set(NewValue.timeIntervalSince1970, forKey: ForKey.rawValue)
+    }
+    
+    /// Returns a date from the specified setting.
+    /// - Parameter Setting: The setting key whose date value will be returned.
+    /// - Returns: The date stored in the specified setting key. May be 1 January 1970 if not
+    ///            previously set.
+    public static func GetDate(_ Setting: SettingTypes) -> Date
+    {
+        let Raw = UserDefaults.standard.double(forKey: Setting.rawValue)
+        return Date(timeIntervalSince1970: Raw)
+    }
+    
+    /// Returns a date from the specified setting. If the date appears to have not been set previously,
+    /// the value in `IfZero` is returned (as well as being saved to the specified setting key).
+    /// - Parameter Setting: The setting key whose date value will be returned.
+    /// - Parameter IfZero: The value to save and return if the key holds `0.0` (which indicates
+    ///                     no value has previously been stored.
+    /// - Returns: The date stored in the specified setting, the value of `IfZero` if no previous
+    ///            date was stored.
+    public static func GetDate(_ Setting: SettingTypes, _ IfZero: Date) -> Date
+    {
+        let Raw = UserDefaults.standard.double(forKey: Setting.rawValue)
+        if Raw == 0.0
+        {
+            SetDate(Setting, IfZero)
+            return IfZero
+        }
+        return Date(timeIntervalSince1970: Raw)
+    }
+    
+    /// Save the passed date to the specified setting.
+    /// - Parameter Setting: The setting key where to save the date.
+    /// - Parameter Value: The value to save.
+    public static func SetDate(_ Setting: SettingTypes, _ Value: Date)
+    {
+        let OldValue = GetDate(Setting)
+        UserDefaults.standard.set(Value.timeIntervalSince1970, forKey: Setting.rawValue)
+        NotifySubscribers(Setting: Setting, OldValue: OldValue, NewValue: Value)
+    }
+    
     // MARK: - Enum-based settings.
     
     /// Initialize an Enum-based setting. No notification is sent to subscribers.
