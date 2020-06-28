713d937cd33a17db9c281c70fefe8a69b52415af
Stuart Rankin <sjrankin@gmail.com>
2020-06-28 13:46:27 +0900

    Create DebugSettingsCode.swift
    Code to handle the debug UI.

---
+++ FlatlandView/SettingsCode/SettingsCode/MainSettings/OptionsWindows/DebugSettingsCode.swift
@@ -0,0 +1,126 @@
+//
+//  DebugSettingsCode.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 6/28/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class DebugSettingsCode: NSViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        InitializeDebugSettings()
+        self.view.wantsLayer = true
+        self.view.layer?.backgroundColor = NSColor.clear.cgColor
+    }
+    
+    func InitializeDebugSettings()
+    {
+        DebugTimeSwitch.state = Settings.GetBool(.DebugTime) ? .on : .off
+        let TimeControl = Settings.GetEnum(ForKey: .TimeControl, EnumType: TimeControls.self, Default: .Run)
+        if TimeControl == .Run
+        {
+            TimeControlSegment.selectedSegment = 0
+        }
+        else
+        {
+            TimeControlSegment.selectedSegment = 1
+        }
+        TimeMultiplierCombo.removeAllItems()
+        TimeMultiplierCombo.addItem(withObjectValue: "0.5")
+        TimeMultiplierCombo.addItem(withObjectValue: "1.0")
+        TimeMultiplierCombo.addItem(withObjectValue: "2.0")
+        TimeMultiplierCombo.addItem(withObjectValue: "5.0")
+        TimeMultiplierCombo.addItem(withObjectValue: "10.0")
+        TimeMultiplierCombo.addItem(withObjectValue: "100.0")
+        TimeMultiplierCombo.addItem(withObjectValue: "600.0")
+        let Multiplier = Settings.GetDouble(.TimeMultiplier, 1.0)
+        TimeMultiplierCombo.selectItem(withObjectValue: "\(String.WithTrailingZero(Multiplier))")
+        TestTimeSetter.timeZone = TimeZone(abbreviation: "UTC")!
+        StopAtTimeSetter.timeZone = TimeZone(abbreviation: "UTC")!
+        let TestTime = Settings.GetDate(.TestTime, Date())
+        TestTimeSetter.dateValue = TestTime
+        let TimeBreakpoint = Settings.GetDate(.StopTimeAt, Date())
+        StopAtTimeSetter.dateValue = TimeBreakpoint
+        EnableStopTime.state = Settings.GetBool(.EnableStopTime) ? .on : .off
+    }
+    
+    @IBAction func HandleDebugTimeChanged(_ sender: Any)
+    {
+        if let Switch = sender as? NSSwitch
+        {
+            Settings.SetBool(.DebugTime, Switch.state == .on ? true : false)
+        }
+    }
+    
+    @IBAction func HandleTimeControlChanged(_ sender: Any)
+    {
+        if let Segment = sender as? NSSegmentedControl
+        {
+            let Index = Segment.selectedSegment
+            switch Index
+            {
+                case 0:
+                    Settings.SetEnum(.Run, EnumType: TimeControls.self, ForKey: .TimeControl)
+                    
+                case 1:
+                    Settings.SetEnum(.Pause, EnumType: TimeControls.self, ForKey: .TimeControl)
+                    
+                default:
+                    return
+            }
+        }
+    }
+    
+    @IBAction func HandleTestTimeChanged(_ sender: Any)
+    {
+        if let TimePicker = sender as? NSDatePicker
+        {
+            print("\(TimePicker.dateValue)")
+            Settings.SetDate(.TestTime, TimePicker.dateValue)
+        }
+    }
+    
+    @IBAction func HandleStopAtTimeChanged(_ sender: Any)
+    {
+        if let TimePicker = sender as? NSDatePicker
+        {
+            print("\(TimePicker.dateValue)")
+            Settings.SetDate(.StopTimeAt, TimePicker.dateValue)
+        }
+    }
+    
+    @IBAction func HandleTimeMultiplierChanged(_ sender: Any)
+    {
+        if let Combo = sender as? NSComboBox
+        {
+            if let Raw = Combo.objectValueOfSelectedItem as? String
+            {
+                if let Final = Double(Raw)
+                {
+                    Settings.SetDouble(.TimeMultiplier, Final)
+                }
+            }
+        }
+    }
+    
+    @IBAction func EnableStopTimeChanged(_ sender: Any)
+    {
+        if let Switch = sender as? NSSwitch
+        {
+            Settings.SetBool(.EnableStopTime, Switch.state == .on ? true : false)
+        }
+    }
+    
+    @IBOutlet weak var EnableStopTime: NSSwitch!
+    @IBOutlet weak var TimeMultiplierCombo: NSComboBox!
+    @IBOutlet weak var StopAtTimeSetter: NSDatePicker!
+    @IBOutlet weak var TimeControlSegment: NSSegmentedControl!
+    @IBOutlet weak var DebugTimeSwitch: NSSwitch!
+    @IBOutlet weak var TestTimeSetter: NSDatePicker!
+}
