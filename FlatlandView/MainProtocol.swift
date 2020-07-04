8d16aea044be8d1249f9894d9f431f1a6287ae8c
Stuart Rankin <sjrankin@gmail.com>
2020-07-04 14:15:24 +0900

    Update MainProtocol.swift
    Added InsertEarthquake (with polymorphic version) and ForceFetchEarthquakes.

--- FlatlandView/MainProtocol.swift
+++ FlatlandView/MainProtocol.swift
@@ -12,10 +12,22 @@ protocol MainProtocol: class
 {
     /// Called when the main window should be refreshed.
     func Refresh(_ From: String)
+    
     /// Called when a window is closed.
     func DidClose(_ WhatClosed: String)
+    
     /// Debug time changed.
     func DebugTimeChanged(_ NewTime: Date)
+    
     /// Debug rotation changed.
     func DebugRotationChanged(_ NewRotation: Double)
+    
+    /// Insert debug earthquake at the specified location.
+    func InsertEarthquake(Latitude: Double, Longitude: Double, Magnitude: Double)
+    
+    /// Insert debug earthquake at a random location.
+    func InsertEarthquake(Magnitude: Double)
+    
+    /// Fetch earthquakes out of sequence.
+    func ForceFetchEarthquakes()
 }
