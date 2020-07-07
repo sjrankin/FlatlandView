1354da87b19c717b149cc74018334fc3d35993fb
Stuart Rankin <sjrankin@gmail.com>
2020-07-07 11:32:32 +0900

    Create EarthquakeDetailWindow.swift
    Placeholder.

---
+++ FlatlandView/Views/Ancillary/LiveDataView/EarthquakeDetailWindow.swift
@@ -0,0 +1,14 @@
+//
+//  EarthquakeDetailWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 7/6/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class EarthquakeDetailWindow: NSWindowController
+{
+}
