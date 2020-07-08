fb2a2ad405473dab6ba66041d3e9bb9c4ad0247a
Stuart Rankin <sjrankin@gmail.com>
2020-07-08 17:11:46 +0900

    Create FontPickerWindow.swift
    Place holder.

---
+++ FlatlandView/Views/Ancillary/FontPicker/FontPickerWindow.swift
@@ -0,0 +1,14 @@
+//
+//  FontPickerWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 7/7/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class FontPickerWindow: NSWindowController
+{
+}
