8fedf826b7ee1903adf2e5fda61c1f3d0a727f7c
Stuart Rankin <sjrankin@gmail.com>
2020-07-08 17:12:11 +0900

    Create FontNameWindow.swift
    Place holder.

---
+++ FlatlandView/Views/Ancillary/FontPicker/FontNameWindow.swift
@@ -0,0 +1,14 @@
+//
+//  FontNameWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 7/7/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class FontNameWindow: NSWindowController
+{
+}
