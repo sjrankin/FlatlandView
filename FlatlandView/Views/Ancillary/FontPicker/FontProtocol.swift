c60640abbd910bbc42650e071ba8e5c554372900
Stuart Rankin <sjrankin@gmail.com>
2020-07-09 13:54:09 +0900

    Update FontProtocol.swift
    Added comments. Changed return type for CurrentFont.

--- FlatlandView/Views/Ancillary/FontPicker/FontProtocol.swift
+++ FlatlandView/Views/Ancillary/FontPicker/FontProtocol.swift
@@ -9,10 +9,19 @@
 import Foundation
 import AppKit
 
+/// Protocol for using the font picker.
 protocol FontProtocol: class
 {
-    func CurrentFont() -> StoredFont
+    /// Return the current font to display.
+    func CurrentFont() -> StoredFont?
+    
+    /// Returns a flag that indicates the caller wants continuous updates.
     func WantsContinuousUpdates() -> Bool
+    
+    /// Called when the user makes changes if `WantsContinuousUpdates` is true.
     func NewFont(_ NewFont: StoredFont)
+    
+    /// Called when the user closes the font picker. If `OK` is true,
+    /// `SelectedFont` contains the font as updated by the user.
     func Closed(_ OK: Bool, _ SelectedFont: StoredFont?)
 }
