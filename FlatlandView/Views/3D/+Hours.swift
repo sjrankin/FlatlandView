b46e72f27885029d58d5a42620d8babbd69c650d
Stuart Rankin <sjrankin@gmail.com>
2020-07-19 16:04:40 +0900

    Update +Hours.swift
    Changed solar hours for 3D house to properly reflect hours (eg, hours were displayed in reverse order).

--- FlatlandView/Views/3D/+Hours.swift
+++ FlatlandView/Views/3D/+Hours.swift
@@ -114,7 +114,8 @@ extension GlobeView
         
         for Hour in 0 ... 23
         {
-            let SolarHours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+//            let SolarHours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+            let SolarHours = [23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]
             let DisplayHour = SolarHours[(Hour + 6) % 24]
             let DisplayString = GetScriptHours("\(DisplayHour)", Actual: DisplayHour)
             HourLabelList.append((DisplayString, DisplayHour))
