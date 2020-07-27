b20ba03662f60ceabee89f892d4f19d9673c1a31
Stuart Rankin <sjrankin@gmail.com>
2020-07-27 21:10:53 +0900

    Create SolidColorParameters.swift
    Structures that tell the solid color image generator how to create the image.

---
+++ FlatlandView/Metal/SolidColor/SolidColorParameters.swift
@@ -0,0 +1,16 @@
+//
+//  SolidColorParameters.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 7/27/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import simd
+
+struct SolidColorParameters
+{
+    let Fill: simd_float4
+}
