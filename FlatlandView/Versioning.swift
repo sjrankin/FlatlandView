f8dbc1b509f0096345ea9ab8cfc6665f1d5e6a52
Stuart Rankin <sjrankin@gmail.com>
2020-07-27 21:04:10 +0900

    Update Versioning.swift
    Auto updated. Updated minor version to .99.

--- FlatlandView/Versioning.swift
+++ FlatlandView/Versioning.swift
@@ -33,7 +33,7 @@ public class Versioning: CustomStringConvertible
     public static let MajorVersion: String = "0"
     
     /// Minor version number.
-    public static let MinorVersion: String = "96"
+    public static let MinorVersion: String = "99"
     
     /// Potential version suffix.
     public static let VersionSuffix: String = ""
@@ -118,19 +118,19 @@ public class Versioning: CustomStringConvertible
     }
     
     /// Build number.
-    public static let Build: Int = 2102
+    public static let Build: Int = 2176
     
     /// Build increment.
     private static let BuildIncrement = 1
     
     /// Build ID.
-    public static let BuildID: String = "7A75FC16-24C3-4C3F-B51B-B8CCBD1A077B"
+    public static let BuildID: String = "6B05CD94-5D38-4E7C-AFAC-051FB98ACCD6"
     
     /// Build date.
-    public static let BuildDate: String = "19 July 2020"
+    public static let BuildDate: String = "27 July 2020"
     
     /// Build Time.
-    public static let BuildTime: String = "16:03"
+    public static let BuildTime: String = "20:34"
     
     /// Holds the release build flag.
     private static var _IsReleaseBuild: Bool = false
