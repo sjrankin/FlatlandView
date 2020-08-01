69064833272c0841688ada3795de2803d9e2cc87
Stuart Rankin <sjrankin@gmail.com>
2020-08-01 21:19:26 +0900

    Create EarthquakeRegionWindow.swift
    Placeholder.

---
+++ FlatlandView/SettingsCode/SettingsCode/MainSettings/EarthquakeRegion/EarthquakeRegionWindow.swift
@@ -0,0 +1,14 @@
+//
+//  EarthquakeRegionWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 8/1/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class EarthquakeRegionWindow: NSWindowController
+{
+}
