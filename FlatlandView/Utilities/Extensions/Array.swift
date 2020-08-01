80df502ee02a28d7e7e47f89c782a4abecc1faf3
Stuart Rankin <sjrankin@gmail.com>
2020-08-01 15:45:02 +0900

    Create Array.swift
    Array extensions.

---
+++ FlatlandView/Utilities/Extensions/Array.swift
@@ -0,0 +1,75 @@
+//
+//  Array.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 7/30/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+// MARK: - Array extensions.
+
+/// Array[Double] extensions. Used for k-means clustering of earthquakes.
+extension Array where Element == Double
+{
+    var Sum: Double
+    {
+        return self.reduce(0.0, +)
+    }
+    
+    var Mean: Double
+    {
+        return Sum / Double(self.count)
+    }
+    
+    var Variance: Double
+    {
+        let TheMean = self.Mean
+        return self.map{($0 - TheMean) * ($0 - TheMean)}.Mean
+    }
+    
+    var Std: Double
+    {
+        return sqrt(Variance)
+    }
+    
+    var ZScore: [Double]
+    {
+        let TheMean = self.Mean
+        let Standard = self.Std
+        return self.map{Standard != 0 ? (($0 - TheMean) / Standard) : 0.0}
+    }
+}
+
+/// Array extensions.
+extension Array
+{
+    /// Shift the contents of an array by a specified amount.
+    /// - Note: See [Shift arrays in Swift](https://stackoverflow.com/questions/31554670/shift-swift-array/44739098)
+    /// - Parameter By: The number of elements to shift the array, positive or negative.
+    /// - Rturns: Shifted array.
+    public func Shift(By Index: Int) -> Array
+    {
+        if Index == 0
+        {
+            return self
+        }
+        let AdjustedIndex = Index %% self.count
+        return Array(self[AdjustedIndex ..< self.count] + self[0 ..< AdjustedIndex])
+    }
+}
+
+infix operator %%
+/// Modulo operator.
+/// - Note: See [Shift arrays in Swift](https://stackoverflow.com/questions/31554670/shift-swift-array/44739098)
+/// - dividend: The dividend value.
+/// - divisor: The divisor value. Fails is 0.
+/// - Returns: Infix modulo.
+public func %%(_ dividend: Int, _ divisor: Int) -> Int
+{
+    precondition(divisor > 0, "modulus must be positive")
+    let Reminder = dividend % divisor
+    return Reminder >= 0 ? Reminder: Reminder + divisor
+}
