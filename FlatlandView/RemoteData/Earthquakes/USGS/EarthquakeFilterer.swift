23785d5c6ec623ac529379107f07240560760549
Stuart Rankin <sjrankin@gmail.com>
2020-08-04 17:46:10 +0900

    Create EarthquakeFilterer.swift
    Class to filter earthquakes based on various regions defined by the user.

---
+++ FlatlandView/RemoteData/Earthquakes/USGS/EarthquakeFilterer.swift
@@ -0,0 +1,144 @@
+//
+//  EarthquakeFilterer.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 8/3/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+/// Filters earthquakes as per user settings.
+class EarthquakeFilterer
+{
+    /// Determines if an earthquake is in a region.
+    /// - Parameter Quake: The quake to check against the passed region.
+    /// - Parameter Region: The region to test the passed earhquake against.
+    /// - Returns: True if the location of `Quake` is in `Region`, false if not.
+    private static func QuakeInRegion(_ Quake: Earthquake, _ Region: EarthquakeRegion) -> Bool
+    {
+        if Quake.Longitude < Region.UpperLeft.Longitude
+        {
+            return false
+        }
+        if Quake.Longitude > Region.LowerRight.Longitude
+        {
+            return false
+        }
+        if Quake.Latitude < Region.UpperLeft.Latitude
+        {
+            return false
+        }
+        if Quake.Latitude > Region.LowerRight.Latitude
+        {
+            return false
+        }
+        return true
+    }
+    
+    /// Converts the passed number of seconds to days.
+    /// - Parameter Seconds: The number of seconds to convert to days.
+    /// - Returns: Seconds converted to days.
+    private static func AgeInDays(_ Seconds: Double) -> Double
+    {
+        let Days = Seconds / Double(24 * 60 * 60)
+        return Days
+    }
+    
+    /// Determines if the passed earthquake can be displayed based on user-set filters.
+    /// - Parameter Quake: The earthquake to determine whether it can be shown or not.
+    /// - Parameter Filters: List of filters against which the quake is compared.
+    /// - Returns: True if the earthquake can be displayed (meets the filter criteria), false if not.
+    private static func IsValid(_ Quake: Earthquake, _ Filters: [EarthquakeRegion]) -> Bool
+    {
+        for Filter in Filters
+        {
+            if Filter.IsFallback
+            {
+                continue
+            }
+            if QuakeInRegion(Quake, Filter)
+            {
+                var MagnitudeOK = false
+                if Quake.Magnitude >= Filter.MinimumMagnitude && Quake.Magnitude <= Filter.MaximumMagnitude
+                {
+                    MagnitudeOK = true
+                }
+                let Days = AgeInDays(Quake.GetAge())
+                let AgeOK = Int(Days) <= Filter.Age
+                if AgeOK && MagnitudeOK
+                {
+                    return true
+                }
+            }
+        }
+        let FallBack = GetFallback(From: Filters)
+        if Quake.Magnitude >= FallBack.MinimumMagnitude && Quake.Magnitude <= FallBack.MaximumMagnitude
+        {
+            let Days = AgeInDays(Quake.GetAge())
+            if Days <= Double(FallBack.Age)
+            {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    /// Returns the fallback region.
+    /// - Parameter From: The array of earthquake filters to search for the fallback filter.
+    /// - Returns: The fallback region in the passed list. If one does not exist, a default fallback
+    ///            filter is generated and returned.
+    private static func GetFallback(From: [EarthquakeRegion]) -> EarthquakeRegion
+    {
+        for Filter in From
+        {
+            if Filter.IsFallback
+            {
+                return Filter
+            }
+        }
+        return EarthquakeRegion(FallBack: true)
+    }
+    
+    /// Returns the current set of earthquake regions set by the user.
+    /// - Returns: Array of earthquake regions.
+    private static func GetCurrentFilters() -> [EarthquakeRegion]
+    {
+        var Regions = Settings.GetEarthquakeRegions()
+        if Regions.count < 1
+        {
+            Regions.append(EarthquakeRegion(FallBack: true))
+        }
+        return Regions
+    }
+    
+    /// Filter the list of earthquakes against the current set of user-defined filters.
+    /// - Parameter Quakes: Array of earthquakes to filter against.
+    /// - Returns: Array of filtered earthquakes.
+    public static func FilterList(_ Quakes: [Earthquake]) -> [Earthquake]
+    {
+        var NewList = [Earthquake]()
+        let Filters = GetCurrentFilters()
+        for Quake in Quakes
+        {
+            if IsValid(Quake, Filters)
+            {
+                NewList.append(Quake)
+            }
+        }
+        return NewList
+    }
+    
+    public static func SameEarthquakes(_ OldList: [Earthquake], _ NewList: [Earthquake]) -> Bool
+    {
+        if OldList.count != NewList.count
+        {
+            return false
+        }
+        
+        return true
+
+    }
+}
