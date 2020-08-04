0ca7d6c95954f8c1964d2015f1ce5fa1bcc0c0bb
Stuart Rankin <sjrankin@gmail.com>
2020-08-04 21:01:51 +0900

    Create BoxMergerParameters.swift
    Parameters for merging color boxes with images.

---
+++ FlatlandView/Metal/BoxMerger/BoxMergerParameters.swift
@@ -0,0 +1,21 @@
+//
+//  BoxMergerParameters.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 8/4/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import simd
+
+struct BoxMergeParameters
+{
+    let FillColor: simd_float4
+    let X1: simd_uint1
+    let Y1: simd_uint1
+    let X2: simd_uint1
+    let Y2: simd_uint1
+}
+
