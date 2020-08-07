e4a7d322c9184f0dfdccad16faa7f0c2e7788d47
Stuart Rankin <sjrankin@gmail.com>
2020-08-07 14:03:19 +0900

    Create ImageBlendParameters.swift
    Parameters for blending images together with the ImageBlend Metal kernel.

---
+++ FlatlandView/Metal/ImageBlender/ImageBlendParameters.swift
@@ -0,0 +1,19 @@
+//
+//  ImageBlendParameters.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 8/6/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import simd
+
+
+struct ImageBlendParameters
+{
+    let XOffset: simd_uint1
+    let YOffset: simd_uint1
+}
+
