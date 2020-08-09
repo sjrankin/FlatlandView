196e3375e6abbe8c8aa879445313329ea33990c7
Stuart Rankin <sjrankin@gmail.com>
2020-08-09 17:50:38 +0900

    Update GlobeProtocol.swift
    Added NewStencilTexture.

--- FlatlandView/Views/3D/GlobeProtocol.swift
+++ FlatlandView/Views/3D/GlobeProtocol.swift
@@ -7,8 +7,10 @@
 //
 
 import Foundation
+import AppKit
 
 protocol GlobeProtocol: class
 {
     func PlotSatellite(Satellite: Satellites, At: GeoPoint2)
+    func NewStencilTexture(_ Texture: NSImage)
 }
