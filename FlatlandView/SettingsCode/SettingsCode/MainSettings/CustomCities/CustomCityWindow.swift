d11ce0a9fb1c2f3a03c2b503412943fd52e5fa0e
Stuart Rankin <sjrankin@gmail.com>
2020-08-15 16:45:25 +0900

    Create CustomCityWindow.swift
    Placeholder.

---
+++ FlatlandView/SettingsCode/SettingsCode/MainSettings/CustomCities/CustomCityWindow.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCityWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 8/13/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class CustomCityWindow: NSWindowController
+{
+}
