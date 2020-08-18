2f60e5519f0bebd3bfba0cecb67d7d9ddc2d7a9c
Stuart Rankin <sjrankin@gmail.com>
2020-08-18 11:33:01 +0900

    Create NASAMapConfirmWindow.swift
    Placeholder.

---
+++ FlatlandView/SettingsCode/SettingsCode/MainSettings/Confirmation/NASAMapConfirmWindow.swift
@@ -0,0 +1,14 @@
+//
+//  NASAMapConfirmWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 8/17/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class NASAMapConfirmWindow: NSWindowController
+{
+}
