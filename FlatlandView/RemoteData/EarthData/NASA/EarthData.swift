38de719103ef1a728d027dffbfcbbf74c9554371
Stuart Rankin <sjrankin@gmail.com>
2020-08-18 20:56:49 +0900

    Update EarthData.swift
    Ensured all queues run in the background. Added lock around access to certain variables in GetTile.

--- FlatlandView/RemoteData/EarthData/NASA/EarthData.swift
+++ FlatlandView/RemoteData/EarthData/NASA/EarthData.swift
@@ -17,7 +17,8 @@ class EarthData
     var AccessLock: NSObject = NSObject()
     
     /// Map loaded handler definition. First parameter is the completed map and the second is the duration
-    /// from the call to `LoadMap` to the call of this completion handler.
+    /// from the call to `LoadMap` to the call of this completion handler. The third parameter is the date
+    /// used to retrieve the individual map tiles.
     typealias MapLoadedHandler = ((NSImage, Double, Date) -> ())?
     
     func LoadMap(_ Map: SatelliteMap, For ImageDate: Date, Completed: MapLoadedHandler = nil)
@@ -64,6 +65,11 @@ class EarthData
         }
     }
     
+    /// Given a set of tiles from NASA, create a seamless equirectangular image.
+    /// - Parameter TilesX: Number of horizontal tiles.
+    /// - Parameter TilesY: Number of vertical tiles.
+    /// - Parameter When: The date for the images.
+    /// - Parameter Completion: Called upon map completion.
     func CreateMapFromTiles(TilesX: Int, TilesY: Int, When: Date,
                             Completion: MapLoadedHandler = nil)
     {
@@ -88,6 +94,7 @@ class EarthData
         }
         
         let Queue = OperationQueue()
+        Queue.qualityOfService = .background
         Queue.name = "Map Assembly Queue"
         Queue.addOperation
         {
@@ -96,10 +103,12 @@ class EarthData
             let BackgroundHeight = TilesY * TileSize
             let BackgroundWidth = TilesX * TileSize
             var Background = NSImage(size: NSSize(width: BackgroundWidth / 2, height: BackgroundHeight / 2))
+            print("CreateMapFromTiles: Background.size=\(Background.size)")
             Background.lockFocus()
             NSColor.systemYellow.drawSwatch(in: NSRect(origin: .zero, size: Background.size))
             Background.unlockFocus()
             Background = self.ResizeImage(Image: Background, Longest: CGFloat(TilesX * TileSize))
+            print("CreateMapFromTiles: Resized Background.size=\(Background.size)")
             autoreleasepool
             {
                 for (Row, Column, _, Tile) in self.Results
@@ -115,6 +124,8 @@ class EarthData
                     }
                 }
             }
+            //Background = self.ResizeImage(Image: Background, Longest: 3600.0)
+            //print("CreateMapFromTiles: Resized{2} Background.size=\(Background.size)")
             let Duration = CACurrentMediaTime() - Start
             Completion?(Background, Duration, When)
         }
@@ -124,11 +135,20 @@ class EarthData
     var Results = [(Row: Int, Column: Int, ID: UUID, Image: NSImage)]()
     var DownloadCount = 0
     
+    /// Call a NASA server to get an image tile.
+    /// - Parameter From: The URL of the image tile to return.
+    /// - Parameter Row: The row of the tile in the full image.
+    /// - Parameter Column: The column of the tile in the full image.
+    /// - Parameter ExpectedCount: The expected number of returned tiles.
+    /// - Parameter MaxRows: The maximum number of rows in the full image.
+    /// - Parameter MaxColumns: The maximum number of columns in the full image.
+    /// - Parameter Completed: Closure called when a tile has been received.
     func GetTile(From: URL, Row: Int, Column: Int, ExpectedCount: Int,
                  MaxRows: Int, MaxColumns: Int,
                  Completed: (() -> ())? = nil)
     {
         let Queue = OperationQueue()
+        Queue.qualityOfService = .background
         Queue.name = "Tile Retrieval Queue"
         Queue.addOperation
         {
@@ -138,12 +158,13 @@ class EarthData
                 if let Image = NSImage(data: ImageData)
                 {
                     objc_sync_enter(self.AccessLock)
-                    defer{objc_sync_exit(self.AccessLock)}
+                    //defer{objc_sync_exit(self.AccessLock)}
                     let ID = UUID()
                     self.Results.append((Row, Column, ID, Image))
                     self.TileMap[ID] = (Row, Column)
                     self.DownloadCount = self.DownloadCount + 1
                     self.MainDelegate?.SetIndicatorPercent(Double(self.DownloadCount) / Double(ExpectedCount))
+                    objc_sync_exit(self.AccessLock)
                     if self.DownloadCount == ExpectedCount
                     {
                         Completed?()
@@ -157,6 +178,11 @@ class EarthData
         }
     }
     
+    /// Blit a tile image onto the full map.
+    /// - Parameter Tile: The tile to blit onto the background image.
+    /// - Parameter On: The background image that will function as the full image once completed.
+    /// - Parameter At: The location where to blit the `Tile` image onto `On`.
+    /// - Returns: Image with the passed `Tile` blitted onto `Background` on success, nil on error.
     func BlitImage(_ Tile: NSImage, On Background: NSImage, At Point: NSPoint) -> NSImage?
     {
         autoreleasepool
@@ -179,6 +205,10 @@ class EarthData
         }
     }
     
+    /// Resize the passed image such that the longest dimension has a size proportional to `Longest`.
+    /// - Parameter Image: The image to resize.
+    /// - Parameter Longest: Used to determine the ratio of the longest dimension of the image.
+    /// - Returns: Resized image.
     public func ResizeImage(Image: NSImage, Longest: CGFloat) -> NSImage
     {
         let ImageMax = max(Image.size.width, Image.size.height)
