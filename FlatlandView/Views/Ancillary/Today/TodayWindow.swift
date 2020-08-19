db7d3d6098f28cb05b4cac79de783d6fe9ae675d
Stuart Rankin <sjrankin@gmail.com>
2020-08-19 16:56:01 +0900

    Create TodayWindow.swift
    Placeholder.

---
+++ FlatlandView/Views/Ancillary/Today/TodayWindow.swift
@@ -0,0 +1,14 @@
+//
+//  TodayWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 8/19/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class TodayWindow: NSWindowController
+{
+}
