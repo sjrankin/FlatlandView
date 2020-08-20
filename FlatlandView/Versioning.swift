5a55dff789cd63104625f06e09f32fb9139dad2d
Stuart Rankin <sjrankin@gmail.com>
2020-08-20 13:15:40 +0900

    Update Versioning.swift
    Auto updated.

--- FlatlandView/Versioning.swift
+++ FlatlandView/Versioning.swift
@@ -118,19 +118,19 @@ public class Versioning: CustomStringConvertible
     }
     
     /// Build number.
-    public static let Build: Int = 2827
+    public static let Build: Int = 2848
     
     /// Build increment.
     private static let BuildIncrement = 1
     
     /// Build ID.
-    public static let BuildID: String = "9910A163-9028-4513-B970-40532DB3CDD6"
+    public static let BuildID: String = "75F7BE8C-11EB-4E20-84F6-7BCDAA7DE35D"
     
     /// Build date.
-    public static let BuildDate: String = "19 August 2020"
+    public static let BuildDate: String = "20 August 2020"
     
     /// Build Time.
-    public static let BuildTime: String = "20:50"
+    public static let BuildTime: String = "13:05"
     
     /// Holds the release build flag.
     private static var _IsReleaseBuild: Bool = false
