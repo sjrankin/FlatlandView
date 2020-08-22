c636218817cd78dc1b504bc7b2012b506536bdb8
Stuart Rankin <sjrankin@gmail.com>
2020-08-22 21:03:17 +0900

    Create StackTracer.hpp
    Will be deleted.

---
+++ FlatlandView/CPP/StackTracer.hpp
@@ -0,0 +1,20 @@
+//
+//  StackTracer.hpp
+//  Flatland
+//
+//  Created by Stuart Rankin on 8/21/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+#ifndef StackTracer_hpp
+#define StackTracer_hpp
+
+#include <stdio.h>
+#include <cstdlib>
+#include <vector>
+
+
+   // std::vector<std::string> GetTrace();
+
+
+#endif /* StackTracer_hpp */
