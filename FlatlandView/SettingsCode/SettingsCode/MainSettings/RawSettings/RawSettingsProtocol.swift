cc64fba5d2045f464533d06b8f4da4143093309b
Stuart Rankin <sjrankin@gmail.com>
2020-08-29 17:53:59 +0900

    Create RawSettingsProtocol.swift
    Protocol for editor controllers to get information from the main raw setting editor window.

---
+++ FlatlandView/SettingsCode/SettingsCode/MainSettings/RawSettings/RawSettingsProtocol.swift
@@ -0,0 +1,21 @@
+//
+//  RawSettingsProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 8/28/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol RawSettingsProtocol: class
+{
+    func GetSettingName() -> String
+    func GetSettingType() -> String
+    func GetEnumCases() -> [String]
+    func SetEnumValue(_ SettingKey: SettingTypes, _ AsString: String)
+    func GetSettingValue() -> (Any?, String)?
+    func SetDirty(_ Key: SettingTypes)
+    func ClearDirty(_ Key: SettingTypes)
+}
