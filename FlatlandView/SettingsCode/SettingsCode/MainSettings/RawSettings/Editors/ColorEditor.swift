bf8e0785843e12c66d468d7560b04091fa5dcf62
Stuart Rankin <sjrankin@gmail.com>
2020-08-29 17:54:34 +0900

    Create ColorEditor.swift
    Color type editor.

---
+++ FlatlandView/SettingsCode/SettingsCode/MainSettings/RawSettings/Editors/ColorEditor.swift
@@ -0,0 +1,52 @@
+//
+//  ColorEditor.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 8/29/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class ColorEditor: NSViewController, EditorProtocol
+{
+    public weak var Delegate: RawSettingsProtocol? = nil
+    
+    func AssignDelegate(_ DelegateProtocol: RawSettingsProtocol?)
+    {
+        Delegate = DelegateProtocol
+        SettingNameLabel.stringValue = Delegate!.GetSettingName()
+    }
+    
+    func LoadValue(_ Value: Any?, _ Type: String)
+    {
+        if let OldColor = Value as? NSColor
+        {
+            OldColorWell.color = OldColor
+            NewColorWell.color = OldColor
+        }
+    }
+    
+    var SettingKey: SettingTypes? = nil
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func HandleSavePressed(_ sender: Any)
+    {
+        Delegate?.ClearDirty(SettingKey!)
+        Settings.SetColor(SettingKey!, NewColorWell.color)
+    }
+    
+    @IBAction func HandleNewColorChanged(_ sender: Any)
+    {
+        Delegate?.SetDirty(SettingKey!)
+    }
+    
+    @IBOutlet weak var NewColorWell: NSColorWell!
+    @IBOutlet weak var OldColorWell: NSColorWell!
+    @IBOutlet weak var SettingNameLabel: NSTextField!
+}
