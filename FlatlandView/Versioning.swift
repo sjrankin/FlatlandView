f94d7d5d19feb73906a17a7ffccd6bbd62167c80
Stuart Rankin <sjrankin@gmail.com>
2020-09-03 13:50:04 +0900

    Updated
    Updated images to remove jpeg artifacts around rim of image.

--- FlatlandView/Versioning.swift
+++ FlatlandView/Versioning.swift
@@ -118,19 +118,19 @@ public class Versioning: CustomStringConvertible
     }
     
     /// Build number.
-    public static let Build: Int = 3225
+    public static let Build: Int = 3226
     
     /// Build increment.
     private static let BuildIncrement = 1
     
     /// Build ID.
-    public static let BuildID: String = "38E4F857-EA5C-4429-B92C-7CB1D46D3FAB"
+    public static let BuildID: String = "4B374740-3496-4A94-B3BB-9BA1B2FFE2AC"
     
     /// Build date.
-    public static let BuildDate: String = "2 September 2020"
+    public static let BuildDate: String = "3 September 2020"
     
     /// Build Time.
-    public static let BuildTime: String = "14:53"
+    public static let BuildTime: String = "13:48"
     
     /// Holds the release build flag.
     private static var _IsReleaseBuild: Bool = false
