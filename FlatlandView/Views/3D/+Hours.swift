aca970cc011219c8871645968c19f0494e71957d
Stuart Rankin <sjrankin@gmail.com>
2020-09-09 16:45:19 +0900

    Update +Hours.swift
    Updated StartAngle in plot hours to ensure hours are aligned correctly.

--- FlatlandView/Views/3D/+Hours.swift
+++ FlatlandView/Views/3D/+Hours.swift
@@ -114,14 +114,13 @@ extension GlobeView
         
         for Hour in 0 ... 23
         {
-//            let SolarHours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
             let SolarHours = [23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]
             let DisplayHour = SolarHours[(Hour + 6) % 24]
             let DisplayString = GetScriptHours("\(DisplayHour)", Actual: DisplayHour)
             HourLabelList.append((DisplayString, DisplayHour))
         }
         let Color = Settings.GetColor(.HourColor, NSColor.systemOrange)
-        return PlotHourLabels(Radius: Radius, Labels: HourLabelList, LetterColor: Color, RadialOffset: 6.0)
+        return PlotHourLabels(Radius: Radius, Labels: HourLabelList, LetterColor: Color, RadialOffset: 3.0)
     }
     
     /// Make the hour node such that each label shows number of hours away from noon.
@@ -219,7 +218,7 @@ extension GlobeView
 
         let VisualScript = Settings.GetEnum(ForKey: .Script, EnumType: Scripts.self, Default: .English)
         
-        let StartAngle = 0
+        let StartAngle = 360.0 / 24.0//0
         var Angle = StartAngle
         for Label in Labels
         {
