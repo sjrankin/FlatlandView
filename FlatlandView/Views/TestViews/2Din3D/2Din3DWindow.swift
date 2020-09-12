c8b6d1b246de209d2bbaea93c7776dfc12c4eaa2
Stuart Rankin <sjrankin@gmail.com>
2020-09-12 20:28:12 +0900

    Create 2Din3DWindow.swift
    Placeholder.

---
+++ FlatlandView/Views/TestViews/2Din3D/2Din3DWindow.swift
@@ -0,0 +1,14 @@
+//
+//  2Din3DWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 9/12/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class TwoDinThreeDWindow: NSWindowController
+{
+}
