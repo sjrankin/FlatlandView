e52f68007cb20bb7b5ca490c14449cd2bfc084bf
Stuart Rankin <sjrankin@gmail.com>
2020-09-19 21:11:47 +0900

    Create GradientParameters.swift
    Parameter definitions for the Metal gradient kernel.

---
+++ FlatlandView/Metal/Gradient/GradientParameters.swift
@@ -0,0 +1,116 @@
+//
+//  GradientParameters.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 9/17/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import simd
+
+struct GradientParameters1P
+{
+    let Stop1Color: simd_float4
+    let Stop1X: simd_uint1
+    let Stop1Y: simd_uint1
+    let MaxDistance1: simd_uint1
+    let RadialTerminal1: simd_float4
+}
+
+struct GradientParameters2P
+{
+    let Stop1Color: simd_float4
+    let Stop1X: simd_uint1
+    let Stop1Y: simd_uint1
+    let MaxDistance1: simd_uint1
+    let RadialTerminal1: simd_float4
+    
+    let Stop2Color: simd_float4
+    let Stop2X: simd_uint1
+    let Stop2Y: simd_uint1
+    let MaxDistance2: simd_uint1
+    let RadialTerminal2: simd_float4
+}
+
+struct GradientParameters3P
+{
+    let Stop1Color: simd_float4
+    let Stop1X: simd_uint1
+    let Stop1Y: simd_uint1
+    let MaxDistance1: simd_uint1
+    let RadialTerminal1: simd_float4
+    
+    let Stop2Color: simd_float4
+    let Stop2X: simd_uint1
+    let Stop2Y: simd_uint1
+    let MaxDistance2: simd_uint1
+    let RadialTerminal2: simd_float4
+    
+    let Stop3Color: simd_float4
+    let Stop3X: simd_uint1
+    let Stop3Y: simd_uint1
+    let MaxDistance3: simd_uint1
+    let RadialTerminal3: simd_float4
+}
+
+struct GradientParameters4P
+{
+    let Stop1Color: simd_float4
+    let Stop1X: simd_uint1
+    let Stop1Y: simd_uint1
+    let MaxDistance1: simd_uint1
+    let RadialTerminal1: simd_float4
+    
+    let Stop2Color: simd_float4
+    let Stop2X: simd_uint1
+    let Stop2Y: simd_uint1
+    let MaxDistance2: simd_uint1
+    let RadialTerminal2: simd_float4
+    
+    let Stop3Color: simd_float4
+    let Stop3X: simd_uint1
+    let Stop3Y: simd_uint1
+    let MaxDistance3: simd_uint1
+    let RadialTerminal3: simd_float4
+    
+    let Stop4Color: simd_float4
+    let Stop4X: simd_uint1
+    let Stop4Y: simd_uint1
+    let MaxDistance4: simd_uint1
+    let RadialTerminal4: simd_float4
+}
+
+struct GradientParameters5P
+{
+    let Stop1Color: simd_float4
+    let Stop1X: simd_uint1
+    let Stop1Y: simd_uint1
+    let MaxDistance1: simd_uint1
+    let RadialTerminal1: simd_float4
+    
+    let Stop2Color: simd_float4
+    let Stop2X: simd_uint1
+    let Stop2Y: simd_uint1
+    let MaxDistance2: simd_uint1
+    let RadialTerminal2: simd_float4
+    
+    let Stop3Color: simd_float4
+    let Stop3X: simd_uint1
+    let Stop3Y: simd_uint1
+    let MaxDistance3: simd_uint1
+    let RadialTerminal3: simd_float4
+    
+    let Stop4Color: simd_float4
+    let Stop4X: simd_uint1
+    let Stop4Y: simd_uint1
+    let MaxDistance4: simd_uint1
+    let RadialTerminal4: simd_float4
+    
+    let Stop5Color: simd_float4
+    let Stop5X: simd_uint1
+    let Stop5Y: simd_uint1
+    let MaxDistance5: simd_uint1
+    let RadialTerminal5: simd_float4
+}
