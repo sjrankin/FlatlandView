2e5f049a22a05baf64ca688750779c02449888cd
Stuart Rankin <sjrankin@gmail.com>
2020-09-20 13:03:26 +0900

    Update Main2Controller.swift
    Deleted night mask functions (moved to Utility).

--- FlatlandView/Main2/Main2Controller.swift
+++ FlatlandView/Main2/Main2Controller.swift
@@ -159,7 +159,7 @@ class Main2Controller: NSViewController
             Main2DView.HideNightMask()
             return
         }
-        if let Image = GetNightMask(ForDate: Date())
+        if let Image = Utility.GetNightMask(ForDate: Date())
         {
             Main2DView.AddNightMask(Image)
         }
@@ -167,46 +167,6 @@ class Main2Controller: NSViewController
         {
             print("No night mask for \(Date()) found.")
         }
-    }
-    
-    /// Given a date, return a mask image for a flat map.
-    /// - Parameter From: The date for the night mask.
-    /// - Returns: Name of the night mask image file.
-    func MakeNightMaskName(From: Date) -> String
-    {
-        let Day = Calendar.current.component(.day, from: From)
-        let Month = Calendar.current.component(.month, from: From) - 1
-        let MonthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][Month]
-        var Prefix = ""
-        if Settings.GetEnum(ForKey: .ViewType, EnumType: ViewTypes.self, Default: .FlatSouthCenter) == .FlatNorthCenter
-        {
-            Prefix = ""
-        }
-        else
-        {
-            Prefix = "South_"
-        }
-        return "\(Prefix)\(Day)_\(MonthName)"
-    }
-    
-    /// Get a night mask image for a flat map for the specified date.
-    /// - Parameter ForDate: The date of the night mask.
-    /// - Returns: Image for the passed date (and flat map orientation). Nil returned on error.
-    func GetNightMask(ForDate: Date) -> NSImage?
-    {
-        let AlphaLevels: [NightDarknesses: CGFloat] =
-            [
-                .VeryLight: 0.25,
-                .Light: 0.4,
-                .Dark: 0.6,
-                .VeryDark: 0.75
-            ]
-        let ImageName = MakeNightMaskName(From: ForDate)
-        let DarkLevel = Settings.GetEnum(ForKey: .NightDarkness, EnumType: NightDarknesses.self, Default: .Light)
-        let MaskAlpha = AlphaLevels[DarkLevel]!
-        let MaskImage = NSImage(named: ImageName)!
-        let Final = MaskImage.Alpha(CGFloat(MaskAlpha))
-        return Final
     }
     
     @IBAction func ViewTypeNorthCentered(_ sender: Any)
