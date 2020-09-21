0f9cfa95741cee6e88011eb0667e13c258efcab2
Stuart Rankin <sjrankin@gmail.com>
2020-09-21 21:03:55 +0900

    Create +Attract.swift
    Code to run the attract mode.

---
+++ FlatlandView/Views/3D/+Attract.swift
@@ -0,0 +1,63 @@
+//
+//  +Attract.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 9/21/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import SceneKit
+
+extension GlobeView
+{
+    /// Set or reset attract mode depending on the current user settings.
+    public func SetAttractMode()
+    {
+        if Settings.GetBool(.InAttractMode)
+        {
+            EarthNode?.removeAllActions()
+            SeaNode?.removeAllActions()
+            LineNode?.removeAllActions()
+            HourNode?.removeAllActions()
+            for (_, Layer) in StencilLayers
+            {
+                Layer.removeAllActions()
+            }
+            StopClock()
+            AttractEarth()
+        }
+        else
+        {
+            EarthNode?.removeAllActions()
+            SeaNode?.removeAllActions()
+            LineNode?.removeAllActions()
+            HourNode?.removeAllActions()
+            for (_, Layer) in StencilLayers
+            {
+                Layer.removeAllActions()
+            }
+            StartClock()
+        }
+    }
+    
+    /// Display the globe in attract mode.
+    func AttractEarth()
+    {
+        let Rotate = SCNAction.rotateBy(x: 0.0, y: CGFloat(360.0.Radians), z: 0.0,
+                                        duration: Defaults.AttractRotationDuration.rawValue)
+        let RotateForever = SCNAction.repeatForever(Rotate)
+        EarthNode?.runAction(RotateForever)
+        SeaNode?.runAction(RotateForever)
+        LineNode?.runAction(RotateForever)
+        for (_, Layer) in StencilLayers
+        {
+            Layer.runAction(RotateForever)
+        }
+        if Settings.GetEnum(ForKey: .HourType, EnumType: HourValueTypes.self, Default: .None) == .RelativeToLocation
+        {
+            HourNode?.runAction(RotateForever)
+        }
+    }
+}
