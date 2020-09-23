d0c59964faaa74fd2b715b44893ef07d83ae63d9
Stuart Rankin <sjrankin@gmail.com>
2020-09-23 16:32:50 +0900

    Create +FlatViewSuns.swift
    Added code to control the sun node.

---
+++ FlatlandView/Main2/2.5D/2DExtensions/+FlatViewSuns.swift
@@ -0,0 +1,47 @@
+//
+//  +FlatViewSuns.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 9/23/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import SceneKit
+
+extension FlatView
+{
+    /// Add the sun node. The sun node uses its own ambient light so it won't cast shadows but more importantly
+    /// as a code simplification technique to not have to worry about light changes.
+    func AddSun()
+    {
+        let MapCenter = Settings.GetEnum(ForKey: .ViewType, EnumType: ViewTypes.self, Default: .FlatSouthCenter)
+        let SunLocationY = MapCenter == .FlatSouthCenter ? FlatConstants.NorthSunLocationY.rawValue : FlatConstants.SouthSunLocationY.rawValue
+        let SunShape = SCNSphere(radius: CGFloat(FlatConstants.SunRadius.rawValue))
+        SunShape.segmentCount = Int(FlatConstants.SunSegmentCount.rawValue)
+        SunNode = SCNNode(geometry: SunShape)
+        SunNode.castsShadow = false
+        SunNode.geometry?.firstMaterial?.diffuse.contents = NSImage(named: "NASASolarSurface1")
+        SunNode.geometry?.firstMaterial?.selfIllumination.contents = NSColor.yellow
+        SunNode.categoryBitMask = LightMasks2D.AmbientSun.rawValue
+        SunNode.eulerAngles = SCNVector3(90.0.Radians, 0.0, 0.0)
+        SunNode.position = SCNVector3(0.0, SunLocationY, FlatConstants.PolarLightZTerminal.rawValue)
+        self.scene?.rootNode.addChildNode(SunNode)
+    }
+    
+    /// Move the sun.
+    /// - Parameter ToNorth: If true, the sun will be moved to the "north" (upper) part of the view. Otherwise,
+    ///                      it will be moved to the "south" (lower) part of the view.
+    func MoveSun(ToNorth: Bool)
+    {
+        let SunLocationY = ToNorth ? FlatConstants.NorthSunLocationY.rawValue : FlatConstants.SouthSunLocationY.rawValue
+        SunNode.position = SCNVector3(0.0, SunLocationY, FlatConstants.PolarLightZTerminal.rawValue)
+    }
+    
+    /// Hide or show the sun node.
+    func SunVisibility(IsShowing: Bool)
+    {
+        SunNode.isHidden = !IsShowing
+    }
+}
