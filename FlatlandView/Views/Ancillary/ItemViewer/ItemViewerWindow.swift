35c93b973439fd6e63a93760f2b654302a27f021
Stuart Rankin <sjrankin@gmail.com>
2020-09-28 13:37:01 +0900

    Create ItemViewerWindow.swift
    Place holder.

---
+++ FlatlandView/Views/Ancillary/ItemViewer/ItemViewerWindow.swift
@@ -0,0 +1,14 @@
+//
+//  ItemViewerWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 9/27/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class ItemViewerWindow: NSWindowController
+{
+}
