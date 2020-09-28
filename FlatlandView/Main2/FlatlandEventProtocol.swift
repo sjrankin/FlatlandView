b59416e94dfa20badaccdfd123afec0cf3098a09
Stuart Rankin <sjrankin@gmail.com>
2020-09-28 13:36:08 +0900

    Create FlatlandEventProtocol.swift
    Event protocol to allow views to receive certain, low-level user interface events.

---
+++ FlatlandView/Main2/FlatlandEventProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  FlatlandEventProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 9/27/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol FlatlandEventProtocol: class
+{
+    func MouseAt(Point: CGPoint)
+}
