d5b6b7e4eb64f0e5cc7c163a0048bad13f45054c
Stuart Rankin <sjrankin@gmail.com>
2020-09-30 14:34:15 +0900

    Update +FlatViewWorldHeritageSites.swift
    Updated and extracted node generation.

--- FlatlandView/Main2/2.5D/2DExtensions/+FlatViewWorldHeritageSites.swift
+++ FlatlandView/Main2/2.5D/2DExtensions/+FlatViewWorldHeritageSites.swift
@@ -12,6 +12,7 @@ import SceneKit
 
 extension FlatView
 {
+    /// Plot World Heritage Sites.
     func PlotWorldHeritageSites()
     {
         for Node in WHSNodeList
@@ -55,7 +56,6 @@ extension FlatView
             }
             for Site in FinalList
             {
-                let (X, Y, _) = Utility.ToECEF(Site.Latitude, Site.Longitude, Radius: FlatConstants.FlatRadius.rawValue)
                 var DepthOffset: CGFloat = 0.0
                 switch Site.Category
                 {
@@ -71,15 +71,6 @@ extension FlatView
                     default:
                         DepthOffset = -0.08
                 }
-                let SiteShape = SCNRegular.Geometry(VertexCount: 3, Radius: 0.2, Depth: 0.1 + DepthOffset)
-                let SiteNode = SCNNode2(geometry: SiteShape)
-                SiteNode.NodeClass = UUID(uuidString: NodeClasses.WorldHeritageSite.rawValue)!
-                SiteNode.NodeID = Site.InternalID
-                SiteNode.categoryBitMask = LightMasks3D.Sun.rawValue | LightMasks3D.Moon.rawValue
-                SiteNode.scale = SCNVector3(NodeScales3D.UnescoScale.rawValue,
-                                            NodeScales3D.UnescoScale.rawValue,
-                                            NodeScales3D.UnescoScale.rawValue)
-                WHSNodeList.append(SiteNode)
                 var NodeColor = NSColor.black
                 switch Site.Category
                 {
@@ -95,18 +86,55 @@ extension FlatView
                     default:
                         NodeColor = NSColor.white
                 }
-                SiteNode.geometry?.firstMaterial?.diffuse.contents = NodeColor
-                SiteNode.geometry?.firstMaterial?.specular.contents = NSColor.white
-                SiteNode.castsShadow = true
-                SiteNode.position = SCNVector3(X, Y, 0.0)
-                let YRotation = Site.Latitude
-                let XRotation = Site.Longitude + 180.0
-                SiteNode.eulerAngles = SCNVector3(YRotation.Radians, XRotation.Radians, 0.0)
+                let SiteNode = PlotSiteAsTriangle(Latitude: Site.Latitude,
+                                        Longitude: Site.Longitude,
+                                        Radius: FlatConstants.FlatRadius.rawValue,
+                                        DepthOffset: DepthOffset,
+                                        WithColor: NodeColor)
+                SiteNode.NodeID = Site.InternalID
+                SiteNode.name = NodeNames2D.WorldHeritageSite.rawValue
+                print("Added \(Site.InternalID)")
+                SiteNode.NodeClass = UUID(uuidString: NodeClasses.WorldHeritageSite.rawValue)!
                 UNESCOPlane.addChildNode(SiteNode)
             }
         }
     }
     
+    func PlotSiteAsTriangle(Latitude: Double, Longitude: Double, Radius: Double, DepthOffset: CGFloat, WithColor: NSColor) -> SCNNode2
+    {
+        let SiteShape = SCNRegular.Geometry(VertexCount: 3,
+                                            Radius: CGFloat(FlatConstants.WHSRadius.rawValue),
+                                            Depth: 1.0)//CGFloat(FlatConstants.WHSDepth.rawValue) + DepthOffset)
+        let SiteNode = SCNNode2(geometry: SiteShape)
+        SiteNode.categoryBitMask = LightMasks2D.Sun.rawValue | LightMasks2D.Polar.rawValue
+        SiteNode.scale = SCNVector3(NodeScales2D.UnescoScale.rawValue,
+                                    NodeScales2D.UnescoScale.rawValue,
+                                    NodeScales2D.UnescoScale.rawValue)
+        WHSNodeList.append(SiteNode)
+        SiteNode.geometry?.firstMaterial?.diffuse.contents = WithColor
+        SiteNode.geometry?.firstMaterial?.specular.contents = NSColor.white
+        SiteNode.castsShadow = true
+        
+        let BearingOffset = FlatConstants.InitialBearingOffset.rawValue
+        var LongitudeAdjustment = -1.0
+        if Settings.GetEnum(ForKey: .ViewType, EnumType: ViewTypes.self, Default: .FlatSouthCenter) == .FlatSouthCenter
+        {
+            LongitudeAdjustment = 1.0
+        }
+        var Distance = Utility.DistanceFromContextPole(To: GeoPoint(Latitude, Longitude))
+        let Ratio = Radius / PhysicalConstants.HalfEarthCircumference.rawValue
+        Distance = Distance * Ratio
+        var LocationBearing = Utility.Bearing(Start: GeoPoint(90.0, 0.0), End: GeoPoint(Latitude, Longitude * LongitudeAdjustment))
+        LocationBearing = (LocationBearing + 90.0 + BearingOffset).ToRadians()
+        let PointX = Distance * cos(LocationBearing)
+        let PointY = Distance * sin(LocationBearing)
+        let PointZ = Double(NodeScales2D.UnescoScale.rawValue * 1.0 * 0.5)
+        SiteNode.position = SCNVector3(PointX, PointY, PointZ)//Double(DepthOffset))
+        SiteNode.eulerAngles = SCNVector3(180.0.Radians, 0.0, 0.0)
+        return SiteNode
+    }
+    
+    /// Remove World Heritage Sites.
     func HideWorldHeritageSites()
     {
         RemoveNodeWithName(NodeNames2D.WorldHeritageSite.rawValue, FromParent: UNESCOPlane)
