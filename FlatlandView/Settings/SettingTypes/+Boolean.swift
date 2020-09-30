de4a5c08ae17dd6d9fa59012b46e2689ddab76b3
Stuart Rankin <sjrankin@gmail.com>
2020-09-30 20:26:13 +0900

    Update +Boolean.swift
    Added InvertBool and QueryInvertBool.

--- FlatlandView/Settings/SettingTypes/+Boolean.swift
+++ FlatlandView/Settings/SettingTypes/+Boolean.swift
@@ -61,4 +61,40 @@ extension Settings
         UserDefaults.standard.set(NewValue, forKey: Setting.rawValue)
         NotifySubscribers(Setting: Setting, OldValue: OldValue, NewValue: NewValue)
     }
+    
+    /// Inverts the boolean value at the specified setting and returns the new value. The inverted
+    /// value is saved and any subscribers are notified of changes.
+    /// - Parameter Setting: The setting where the boolean value to invert lives.
+    /// - Parameter SendNotification: If true, a notification is sent when this function is called.
+    ///                               If false, subscribers are not notified.
+    /// - Returns: Inverted value found at `Setting`.
+    public static func InvertBool(_ Setting: SettingKeys, SendNotification: Bool = true) -> Bool
+    {
+        let OldBool = GetBool(Setting)
+        let NewBool = !OldBool
+        UserDefaults.standard.set(NewBool, forKey: Setting.rawValue)
+        if SendNotification
+        {
+            NotifySubscribers(Setting: Setting, OldValue: OldBool, NewValue: NewBool)
+        }
+        return NewBool
+    }
+    
+    /// Queries an inverted boolean setting value.
+    /// - Note: The boolean value is inverted and saved back to the same `Setting` location prior to the
+    ///         closure being called.
+    /// - Parameter Setting: The setting whose inverted boolean value will be passed to the completion handler.
+    /// - Parameter Completion: Code to execute after the value is retrieved. The inverted value is passed
+    ///                         to the completion handler.
+    public static func QueryInvertBool(_ Setting: SettingKeys, Completion: (Bool) -> Void)
+    {
+        if !TypeIsValid(Setting, Type: Bool.self)
+        {
+            fatalError("\(Setting) is not a boolean")
+        }
+        var BoolValue = UserDefaults.standard.bool(forKey: Setting.rawValue)
+        BoolValue = !BoolValue
+        UserDefaults.standard.set(BoolValue, forKey: Setting.rawValue)
+        Completion(BoolValue)
+    }
 }
