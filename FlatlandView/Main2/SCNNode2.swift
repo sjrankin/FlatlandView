d3fb2100b11c3c934c4108171aa479fe9aa492c8
Stuart Rankin <sjrankin@gmail.com>
2020-10-04 16:15:27 +0900

    Update SCNNode2.swift
    Added capability to draw (and optionally rotate) a bounding box around the node. The bounding box line thickness is dependent on the passed parameter as well as the overall scaling of the node. Lines are implemented by four-sided cylinders.

--- FlatlandView/Main2/SCNNode2.swift
+++ FlatlandView/Main2/SCNNode2.swift
@@ -121,6 +121,169 @@ class SCNNode2: SCNNode
             }
         }
     }
+    
+    var _CanShowBoundingBox: Bool = true
+    {
+        didSet
+        {
+            if !_CanShowBoundingBox
+            {
+                HideBoundingBox()
+            }
+        }
+    }
+    /// Determines whether bounding boxes can be shown. If set to true, bounding boxes can be shown with a
+    /// call to `ShowBoundingBox`. If set to false, bounding boxes are not shown and if a bounding box is
+    /// currently showing, it is hidden.
+    var CanShowBoundingBox: Bool
+    {
+        get
+        {
+            return _CanShowBoundingBox
+        }
+        set
+        {
+            _CanShowBoundingBox = newValue
+        }
+    }
+    
+    /// Draw a bounding box around the node.
+    /// - Note: Bounding boxes do not cast shadows.
+    /// - Parameter LineColor: The line color of the bounding box. Defaults to `NSColor.red`.
+    /// - Parameter RotateBox: If true, the box rotates. If false, no rotation occurs. Defaults to `true`.
+    /// - Parameter LineThickness: The thickness of the lines making up the bounding box. Scaling the parent
+    ///                            `SCNNode2` will affect the visual thickness of the bounding box lines.
+    ///                            Defaults to `0.005`.
+    func ShowBoundingBox(LineColor: NSColor = NSColor.red,
+                         RotateBox: Bool = true,
+                         LineThickness: CGFloat = 0.005)
+    {
+        if !CanShowBoundingBox
+        {
+            return
+        }
+        HideBoundingBox()
+        if ShowingBoundingBox
+        {
+            return
+        }
+        let (BoxMin, BoxMax) = self.boundingBox
+        _ShowingBoundingBox = true
+        let Point1 = SCNVector3(BoxMin.x, BoxMin.y, BoxMin.z)
+        let Point2 = SCNVector3(BoxMin.x, BoxMax.y, BoxMin.z)
+        let Point3 = SCNVector3(BoxMax.x, BoxMax.y, BoxMin.z)
+        let Point4 = SCNVector3(BoxMax.x, BoxMin.y, BoxMin.z)
+        let Point5 = SCNVector3(BoxMin.x, BoxMin.y, BoxMax.z)
+        let Point6 = SCNVector3(BoxMin.x, BoxMax.y, BoxMax.z)
+        let Point7 = SCNVector3(BoxMax.x, BoxMax.y, BoxMax.z)
+        let Point8 = SCNVector3(BoxMax.x, BoxMin.y, BoxMax.z)
+        let Line1 = BoundingBoxLine(Start: Point1, End: Point2, Color: LineColor, LineThickness: LineThickness)
+        let Line2 = BoundingBoxLine(Start: Point2, End: Point3, Color: LineColor, RotateZ: 90.0, LineThickness: LineThickness)
+        let Line3 = BoundingBoxLine(Start: Point3, End: Point4, Color: LineColor, LineThickness: LineThickness)
+        let Line4 = BoundingBoxLine(Start: Point4, End: Point1, Color: LineColor, RotateZ: 90.0, LineThickness: LineThickness)
+        let Line5 = BoundingBoxLine(Start: Point5, End: Point6, Color: LineColor, RotateY: 90.0, LineThickness: LineThickness)
+        let Line6 = BoundingBoxLine(Start: Point6, End: Point7, Color: LineColor, RotateZ: 90.0, LineThickness: LineThickness)
+        let Line7 = BoundingBoxLine(Start: Point7, End: Point8, Color: LineColor, LineThickness: LineThickness)
+        let Line8 = BoundingBoxLine(Start: Point8, End: Point5, Color: LineColor, RotateZ: 90.0, LineThickness: LineThickness)
+        let Line9 = BoundingBoxLine(Start: Point1, End: Point5, Color: LineColor, RotateX: 90.0, LineThickness: LineThickness)
+        let Line10 = BoundingBoxLine(Start: Point2, End: Point6, Color: LineColor, RotateX: 90.0, LineThickness: LineThickness)
+        let Line11 = BoundingBoxLine(Start: Point3, End: Point7, Color: LineColor, RotateX: 90.0, LineThickness: LineThickness)
+        let Line12 = BoundingBoxLine(Start: Point4, End: Point8, Color: LineColor, RotateX: 90.0, LineThickness: LineThickness)
+        BoundingBoxLines.append(Line1)
+        BoundingBoxLines.append(Line2)
+        BoundingBoxLines.append(Line3)
+        BoundingBoxLines.append(Line4)
+        BoundingBoxLines.append(Line5)
+        BoundingBoxLines.append(Line6)
+        BoundingBoxLines.append(Line7)
+        BoundingBoxLines.append(Line8)
+        BoundingBoxLines.append(Line9)
+        BoundingBoxLines.append(Line10)
+        BoundingBoxLines.append(Line11)
+        BoundingBoxLines.append(Line12)
+        let BoxNode = SCNNode()
+        BoxNode.castsShadow = false
+        BoxNode.addChildNode(Line1)
+        BoxNode.addChildNode(Line2)
+        BoxNode.addChildNode(Line3)
+        BoxNode.addChildNode(Line4)
+        BoxNode.addChildNode(Line5)
+        BoxNode.addChildNode(Line6)
+        BoxNode.addChildNode(Line7)
+        BoxNode.addChildNode(Line8)
+        BoxNode.addChildNode(Line9)
+        BoxNode.addChildNode(Line10)
+        BoxNode.addChildNode(Line11)
+        BoxNode.addChildNode(Line12)
+        self.addChildNode(BoxNode)
+        BoundingBoxLines.append(BoxNode)
+        if RotateBox
+        {
+            let Rotation = SCNAction.rotateBy(x: CGFloat(90.0.Radians),
+                                              y: CGFloat(90.0.Radians),
+                                              z: CGFloat(90.0.Radians),
+                                              duration: 3.0)
+            let RotateForever = SCNAction.repeatForever(Rotation)
+            BoxNode.runAction(RotateForever)
+        }
+    }
+    
+    private var _ShowingBoundingBox: Bool = false
+    /// Gets the current state of the bounding box. The state is changed by calls to `ShowBoundingBox` and
+    /// `HideBoundingBox`.
+    public var ShowingBoundingBox: Bool
+    {
+        get
+        {
+            return _ShowingBoundingBox
+        }
+    }
+    
+    /// Hide the bounding box around the node.
+    func HideBoundingBox()
+    {
+        for Line in BoundingBoxLines
+        {
+            Line.removeAllActions()
+            Line.removeAllAnimations()
+            Line.removeFromParentNode()
+        }
+        BoundingBoxLines.removeAll()
+        _ShowingBoundingBox = false
+    }
+    
+    /// Array of bounding box lines.
+    private var BoundingBoxLines = [SCNNode]()
+    
+    /// Return an `SCNNode` that acts looks like a line.
+    /// - Parameter Start: The starting point of the line.
+    /// - Parameter End: The ending point of the line.
+    /// - Parameter Color: The color of the line.
+    /// - Parameter RotateX: How to rotate the line on the X axis. In degrees. Defaults to 0.0.
+    /// - Parameter RotateY: How to rotate the line on the Y axis. In degrees. Defaults to 0.0.
+    /// - Parameter RotateZ: How to rotate the line on the Z axis. In degrees. Defaults to 0.0.
+    /// - Parameter LineThickness: The thickness of the line. Defaults to 0.005.
+    private func BoundingBoxLine(Start: SCNVector3, End: SCNVector3, Color: NSColor, RotateX: Double = 0.0,
+                         RotateY: Double = 0.0, RotateZ: Double = 0.0,
+                         LineThickness: CGFloat = 0.005) -> SCNNode
+    {
+        let Vector = SCNVector3(Start.x - End.x, Start.y - End.y, Start.z - End.z)
+        let Distance = sqrt(Vector.x * Vector.x + Vector.y * Vector.y + Vector.z * Vector.z)
+        let MidPosition = SCNVector3(x: (Start.x + End.x) / 2,
+                                     y: (Start.y + End.y) / 2,
+                                     z: (Start.z + End.z) / 2)
+        let Line =  SCNCylinder()
+        Line.radius = LineThickness
+        Line.height = CGFloat(Distance)
+        Line.radialSegmentCount = 4
+        Line.firstMaterial?.diffuse.contents = Color
+        Line.firstMaterial?.selfIllumination.contents = Color
+        let LineNode = SCNNode(geometry: Line)
+        LineNode.castsShadow = false
+        LineNode.position = MidPosition
+        LineNode.eulerAngles = SCNVector3(RotateX.Radians, RotateY.Radians, RotateZ.Radians)
+        return LineNode
+    }
 }
 
 
