88613d008b3fdb188b37e5b392dabd264f8565fd
Stuart Rankin <sjrankin@gmail.com>
2020-10-09 14:28:01 +0900

    Create City2.swift
    New class to encapsulate a city.

---
+++ FlatlandView/Cities/City2.swift
@@ -0,0 +1,155 @@
+//
+//  City2.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 10/6/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class City2: Hashable
+{
+    /// Default initializer. No properties set.
+    init()
+    {
+    }
+    
+    /// Initializer.
+    /// - Parameters:
+    ///   - Name: The name of the city.
+    ///   - Country: The country where the city is.
+    ///   - IsCapital: Capital city flag.
+    ///   - Population: The population of the city or nil if not known.
+    ///   - MetroPopulation: The population of the metropolitan area or nil if not known.
+    ///   - Latitude: The latitude of the city.
+    ///   - Longitude: The longitude of the city.
+    ///   - Continent: Name of the continent of the city.
+    ///   - ID: ID of the city.
+    ///   - IsAddition: If true, the city is from the additional cities table. Otherwise,
+    ///                 the city is from the standard city table. Defaults to `false`.
+    ///   - SubNational: The subnational location. Defaults to empty string.
+    init(_ Name: String, _ Country: String, _ IsCapital: Bool, _ Population: Int?, _ MetroPopulation: Int?,
+         _ Latitude: Double, _ Longitude: Double, _ Continent: String, _ ID: UUID,
+         _ IsAddition: Bool = false, _ SubNational: String = "")
+    {
+        var FinalContinent = Continent
+        if FinalContinent == "NorthAmerica"
+        {
+            FinalContinent = Continents.NorthAmerica.rawValue
+        }
+        if FinalContinent == "SouthAmerica"
+        {
+            FinalContinent = Continents.SouthAmerica.rawValue
+        }
+        self.Continent = Continents(rawValue: FinalContinent)!
+        self.Country = Country
+        self.Name = Name
+        self.IsCapital = IsCapital
+        self.Population = Population
+        self.MetropolitanPopulation = MetroPopulation
+        self.Latitude = Latitude
+        self.Longitude = Longitude
+        self.CityID = ID
+        self.IsAdditionalCity = IsAddition
+        self.SubNational = SubNational
+    }
+    
+    /// Return a population for the city. Always returns a value.
+    /// - Parameter MetroPopulation: If true, the metropolitan population is returned.
+    /// - Returns: Population of the city. If `MetroPopulation` is true and there is no metropolitan
+    ///            population available, the city population is returned. If no city population is
+    ///            available, `0` is returned.
+    public func GetPopulation(_ MetroPopulation: Bool = true) -> Int
+    {
+        if MetroPopulation
+        {
+            if let Metro = MetropolitanPopulation
+            {
+                return Metro
+            }
+            else
+            {
+                if let CityPop = Population
+                {
+                    return CityPop
+                }
+            }
+        }
+        else
+        {
+            if let CityPop = Population
+            {
+                return CityPop
+            }
+        }
+        return 0
+    }
+    
+    /// Convenience property that holds a base-line location of the city after it was initially plotted.
+    public var PlottedPoint: CGPoint? = nil
+    
+    /// City name.
+    public var Name: String = ""
+    
+    /// Country name.
+    public var Country: String = ""
+    
+    /// Capital city flag.
+    public var IsCapital: Bool = false
+    
+    /// Population of the city. Nil if not known.
+    public var Population: Int? = nil
+    
+    /// Population of the metropolitan area, or nil if not known.
+    public var MetropolitanPopulation: Int? = nil
+    
+    /// Latitude of the city.
+    public var Latitude: Double = 0.0
+    
+    /// Longitude of the city.
+    public var Longitude: Double = 0.0
+    
+    /// Continent of the city.
+    public var Continent: Continents = .Asia
+    
+    /// Color to use to render the city.
+    public var CityColor: NSColor = NSColor.yellow
+    
+    /// User city flag.
+    public var IsUserCity: Bool = false
+    
+    /// World city flag.
+    public var IsWorldCity: Bool = false
+    
+    /// City ID.
+    public var CityID: UUID = UUID()
+    
+    /// Used by the UI when creating custom city lists.
+    public var IsCustomCity: Bool = false
+    
+    /// Subnational location. Used by additional cities.
+    public var SubNational: String = ""
+    
+    /// Determines which table in the database the city is from.
+    public var IsAdditionalCity: Bool = false
+    
+    /// The date the city was added.
+    public var Added: Date? = nil
+    
+    /// The date the city was modified.
+    public var Modified: Date? = nil
+    
+    // MARK: - Required for Hashable protocol.
+    
+    static func == (lhs: City2, rhs: City2) -> Bool
+    {
+        return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+    
+    func hash(into hasher: inout Hasher)
+    {
+        hasher.combine(ObjectIdentifier(self))
+    }
+}
