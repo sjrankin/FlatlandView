5ba9569878ea20937ef24e6de84b18bb45154518
Stuart Rankin <sjrankin@gmail.com>
2020-10-09 14:26:42 +0900

    Create POIManager.swift
    Manages points of interest.

---
+++ FlatlandView/PointsOfInterest/POIManager.swift
@@ -0,0 +1,37 @@
+//
+//  POIManager.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 10/8/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class POIManager
+{
+    public static func Initialize()
+    {
+        AllPOIs = Main2Controller.GetAllPOIs()
+    }
+    
+    public static var AllPOIs: [POI]? = nil
+    
+    public static func GetPOIs(By POIType: POITypes) -> [POI]
+    {
+        if AllPOIs == nil
+        {
+            return [POI]()
+        }
+        var Result = [POI]()
+        for SomePOI in AllPOIs!
+        {
+            if SomePOI.POIType == POIType.rawValue
+            {
+                Result.append(SomePOI)
+            }
+        }
+        return Result
+    }
+}
