dae2d1121a98d692e9183ef39e09df755047e844
Stuart Rankin <sjrankin@gmail.com>
2020-10-12 06:57:39 +0900

    Create GroundTrack.swift
    Planned to convert TLEs into ground tracks.

---
+++ FlatlandView/Astronomy/GroundTrack.swift
@@ -0,0 +1,18 @@
+//
+//  GroundTrack.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 10/10/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class GroundTrack
+{
+    public static func FromTLE(_ ObjectTLE: TLE, On: Date) -> (X: Double, Y: Double, Z: Double)
+    {
+        return (0,0,0)
+    }
+}
