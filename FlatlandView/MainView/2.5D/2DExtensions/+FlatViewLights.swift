2f57daefb784b196de06d5dc22a222c32e14606b
Stuart Rankin <sjrankin@gmail.com>
2020-10-12 16:34:15 +0900

    Update +FlatViewLights.swift
    Removed old methods to determine path for sun (and camera) to take when switching from north to south and south to north centered maps.

--- FlatlandView/MainView/2.5D/2DExtensions/+FlatViewLights.swift
+++ FlatlandView/MainView/2.5D/2DExtensions/+FlatViewLights.swift
@@ -100,100 +100,8 @@ extension FlatView
         let PolarNodeY = CGFloat(FlatConstants.FlatRadius.rawValue) + CGFloat(FlatConstants.PolarSunRimOffset.rawValue)
         PolarNode.position = SCNVector3(0.0, PolarNodeY, CGFloat(FlatConstants.PolarLightZTerminal.rawValue))
         let XOrientation = CGFloat(FlatConstants.PolarLightXOrientation.rawValue.Radians)
-        PolarNode.eulerAngles = SCNVector3(-XOrientation, 0.0, 0.0)
+        PolarNode.eulerAngles = SCNVector3(XOrientation, 0.0, 0.0)
         self.scene?.rootNode.addChildNode(PolarNode)
-    }
-    
-    /// Create a not-ver-smooth arc of points (smoothness depends on the `Step` count) along the Y axis with
-    /// varying Z values. All X values are set to 0.
-    /// - Warning: A fatal error is thrown if the number of steps is less than `1`.
-    /// - Parameter Start: The starting point.
-    /// - Parameter MaxHeight: The maximum height at the peak of the arc. Assumed to be in the middled of
-    ///                        the Y span.
-    /// - Parameter Steps: Number of steps between `Start` and `End`.
-    /// - Parameter End: The ending point.
-    /// - Returns: Array of points that define a crude arc.
-    func MakeArcPoints(Start: SCNVector3, MaxHeight: CGFloat, Steps: Int, End: SCNVector3) -> [SCNVector3]
-    {
-        if Steps < 1
-        {
-            fatalError("Steps must be 1 or greater. \(#function)")
-        }
-        var Points = [SCNVector3]()
-        Points.append(Start)
-        let YLength = abs(Start.y - End.y)
-        let YStep = YLength / CGFloat(Steps + 1)
-        let MiddleZ = Int(Double(Steps) / 2.0)
-        let MZStep = (MaxHeight) / CGFloat(MiddleZ)
-        var ZSteps = [CGFloat]()
-        for z in 1 ... Steps
-        {
-            ZSteps.append((CGFloat(z) * MZStep) + Start.z)
-        }
-
-        if Steps.isMultiple(of: 2)
-        {
-            var scratch = [CGFloat]()
-            while ZSteps.count > MiddleZ
-            {
-                ZSteps.removeLast()
-            }
-            scratch = ZSteps
-            ZSteps.removeLast()
-            for Index in stride(from: ZSteps.count - 1, to: -1, by: -1)
-            {
-                scratch.append(ZSteps[Index])
-            }
-            ZSteps = scratch
-        }
-        else
-        {
-            var scratch = [CGFloat]()
-            while ZSteps.count > MiddleZ + 1
-            {
-                ZSteps.removeLast()
-            }
-            scratch = ZSteps
-            ZSteps.removeLast()
-            for Index in stride(from: ZSteps.count - 1, to: -1, by: -1)
-            {
-                scratch.append(ZSteps[Index])
-            }
-            ZSteps = scratch
-        }
-        ZSteps.insert(Start.z, at: 0)
-        ZSteps.append(End.z)
-        
-        let YMultiplier: CGFloat = Start.y < End.y ? 1.0 : -1.0
-        var PreviousY  = Start.y
-        for Y in 1 ... Steps
-        {
-            let PointY = PreviousY + (YStep * YMultiplier)
-            PreviousY = PointY
-            let Point = SCNVector3(0.0, PointY, ZSteps[Y])
-            Points.append(Point)
-        }
-        Points.append(End)
-        return Points
-    }
-    
-    func MakeSemiCirclePoints(Radius: Double, Steps: Int, ZBase: Double) -> [SCNVector3]
-    {
-        var Results = [SCNVector3]()
-        
-        for Step in 1 ... Steps
-        {
-            let Angle = Double(Step) / 180.0
-            let Radians = Angle.Radians
-            let X = Radius * cos(Radians)
-            let Y = Radius * sin(Radians)
-            //Since the result will be used for elevation, we "reassign" coordinates - X becomes Y and Y becomes Z.
-            //X is assumed to be zero.
-            let Point = SCNVector3(0.0, X, Y + ZBase)
-            Results.append(Point)
-        }
-        
-        return Results
     }
     
     /// Move the polar light to the appropriate pole to cast shadows.
@@ -201,35 +109,14 @@ extension FlatView
     /// - Parameter ToNorth: Determines if the polar light is moved to the north or south pole.
     func MovePolarLight(ToNorth: Bool)
     {
-        var LightPath = [SCNVector3]()
+        var LightPath = Utility.PointsOnArc(Radius: FlatConstants.FlatRadius.rawValue + FlatConstants.PolarSunRimOffset.rawValue,
+                                            Count: 10)
         if ToNorth
         {
-            let EndingY = CGFloat(FlatConstants.FlatRadius.rawValue) + CGFloat(FlatConstants.PolarSunRimOffset.rawValue)
-            #if false
-            let PathRadius = FlatConstants.FlatRadius.rawValue + FlatConstants.PolarSunRimOffset.rawValue
-            LightPath = MakeSemiCirclePoints(Radius: PathRadius, Steps: 10, ZBase: Double(PolarNode.position.z))
-            #else
-            LightPath = MakeArcPoints(Start: PolarNode.position,
-                                      MaxHeight: CGFloat(FlatConstants.MaxArcHeight.rawValue),
-                                      Steps: Int(FlatConstants.ArcStepCount.rawValue),
-                                      End: SCNVector3(0.0, EndingY, PolarNode.position.z))
-            #endif
+            LightPath = LightPath.reversed()
         }
-        else
-        {
-            let EndingY = -(CGFloat(FlatConstants.FlatRadius.rawValue) + CGFloat(FlatConstants.PolarSunRimOffset.rawValue))
-            #if false
-            let PathRadius = FlatConstants.FlatRadius.rawValue + FlatConstants.PolarSunRimOffset.rawValue
-            LightPath = MakeSemiCirclePoints(Radius: PathRadius, Steps: 10, ZBase: Double(PolarNode.position.z))
-            LightPath.reverse()
-            #else
-            LightPath = MakeArcPoints(Start: PolarNode.position,
-                                      MaxHeight: CGFloat(FlatConstants.MaxArcHeight.rawValue),
-                                      Steps: Int(FlatConstants.ArcStepCount.rawValue),
-                                      End: SCNVector3(0.0, EndingY, PolarNode.position.z))
-            #endif
-        }
-        
+        LightPath = Utility.AdjustPointsOnArc(LightPath, XValue: 0.0, ZOffset: Double(PolarNode.position.z))
+        LightNode.position = LightPath[0]
         let OverallDuration = FlatConstants.PolarAnimationDuration.rawValue
         let XOrientation = CGFloat(FlatConstants.PolarLightXOrientation.rawValue.Radians)
         var NewPitch: CGFloat = 0.0
@@ -241,10 +128,10 @@ extension FlatView
         {
             NewPitch = XOrientation
         }
-        
         let SegmentDuration = OverallDuration / Double(LightPath.count - 1)
         var ArcSegments = [SCNAction]()
         for Index in 1 ..< LightPath.count
+        
         {
             let Motion = SCNAction.move(to: SCNVector3(0.0, LightPath[Index].y, LightPath[Index].z), duration: SegmentDuration)
             ArcSegments.append(Motion)
