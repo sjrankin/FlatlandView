769d6654859c3d1fc9f2ce5372959a33e134de5e
Stuart Rankin <sjrankin@gmail.com>
2020-10-16 15:58:29 +0900

    Create UInt64.swift
    Added UInt64 extension to return delimited strings.

---
+++ FlatlandView/Utilities/Extensions/UInt64.swift
@@ -0,0 +1,47 @@
+//
+//  UInt64.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 10/16/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+extension UInt64
+{
+    /// Returns the instance value as a delimited string.
+    /// - Parameter Delimiter: The character (or string if the caller prefers) to use to delimit thousands blocks.
+    /// - Returns: Value as a character delimited string.
+    func Delimited(Delimiter: String = ",") -> String
+    {
+        let Raw = "\(self)"
+        if Raw.count <= 3
+        {
+            return Raw
+        }
+        var RawArray = Array(Raw)
+        var Working = ""
+        while RawArray.count > 0
+        {
+            let Last3 = RawArray.suffix(3)
+            var Sub = ""
+            for C in Last3
+            {
+                Sub = Sub + String(C)
+            }
+            if RawArray.count >= 3
+            {
+                RawArray.removeLast(3)
+            }
+            else
+            {
+                RawArray.removeAll()
+            }
+            let Separator = RawArray.count > 0 ? Delimiter : ""
+            Working = "\(Separator)\(Sub)" + Working
+        }
+        return Working
+    }
+}
