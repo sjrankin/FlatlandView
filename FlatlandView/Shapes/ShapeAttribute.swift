a2fc45400d7409ea601daaab4081deea9986877f
Stuart Rankin <sjrankin@gmail.com>
2020-10-18 20:45:18 +0900

    Create ShapeAttribute.swift
    Added protocol for shape attributes for SCNNode2 items.

---
+++ FlatlandView/Shapes/ShapeAttribute.swift
@@ -0,0 +1,36 @@
+//
+//  ShapeAttribute.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 10/18/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import SceneKit
+
+/// Protocol for setting shape attributes.
+protocol ShapeAttribute: class
+{
+    /// Sets the first diffuse material to the specified color.
+    /// - Parameter Color: The color to use to set the first diffuse material.
+    func SetMaterialColor(_ Color: NSColor)
+    
+    /// Sets the first material's emission color.
+    /// - Parameter Color: The color to use to set the first material's emission
+    ///                    color. If nil, the color is removed.
+    func SetEmissionColor(_ Color: NSColor?)
+    
+    /// Sets the first material's lighting model.
+    /// - Parameter Model: The lighting model to use.
+    func SetLightingModel(_ Model: SCNMaterial.LightingModel)
+    
+    /// Sets the first material's metalness value.
+    /// - Parameter Value: The value to use for metalness. If nil, not set.
+    func SetMetalness(_ Value: Double?)
+    
+    /// Sets the first material's roughness value.
+    /// - Parameter Value: The value to use for roughness. If nil, not set.
+    func SetRoughness(_ Value: Double?)
+}
