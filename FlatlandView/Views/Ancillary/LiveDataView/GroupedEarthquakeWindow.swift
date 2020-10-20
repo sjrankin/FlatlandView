6342d91fa5feea0e1731f8c0eac00764b34da011
Stuart Rankin <sjrankin@gmail.com>
2020-10-20 20:43:34 +0900

    Create GroupedEarthquakeWindow.swift
    Place holder.

---
+++ FlatlandView/Views/Ancillary/LiveDataView/GroupedEarthquakeWindow.swift
@@ -0,0 +1,14 @@
+//
+//  GroupedEarthquakeWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 10/20/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class GroupedEarthquakeWindow: NSWindowController
+{
+}
