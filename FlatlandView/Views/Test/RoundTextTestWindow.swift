8cd67ea7fe38b5ec986cfd7a4dff889b7c0d601a
Stuart Rankin <sjrankin@gmail.com>
2020-10-23 21:01:18 +0900

    Create RoundTextTestWindow.swift
    Place holder.

---
+++ FlatlandView/Views/Test/RoundTextTestWindow.swift
@@ -0,0 +1,14 @@
+//
+//  RoundTextTestWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 10/21/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class RoundTextTestWindow: NSWindowController
+{
+}
