5b5afdc2f24854cd2025427d35971f98ae8dea6d
Stuart Rankin <sjrankin@gmail.com>
2020-10-25 21:51:31 +0900

    Create ImageShiftParameters.swift
    Parameters for the shifting of images.

---
+++ FlatlandView/Metal/ImageShift/ImageShiftParameters.swift
@@ -0,0 +1,17 @@
+//
+//  ImageShiftParameters.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 10/25/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import simd
+
+struct ImageShiftParameters
+{
+    let XOffset: simd_uint1
+    let YOffset: simd_uint1
+}
