caa7ee7770e43dd5b3343fb28d9572a27500b81d
Stuart Rankin <sjrankin@gmail.com>
2020-10-30 21:15:10 +0900

    Create UInt.swift
    UInt extensions.

---
+++ FlatlandView/Extensions/UInt.swift
@@ -0,0 +1,30 @@
+//
+//  UInt.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 10/26/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+/// UInt extensions.
+extension UInt
+{
+    /// Returns the layout size of a `UInt` for an instance value.
+    /// - Returns: Layout size of a `UInt`.
+    func SizeOf() -> Int
+    {
+        return MemoryLayout.size(ofValue: self)
+    }
+    
+    /// Returns the layout size of a `UInt` when used against the `UInt` type.
+    /// - Returns: Layout size of a `UInt`.
+    static func SizeOf() -> Int
+    {
+        return MemoryLayout.size(ofValue: UInt(0))
+    }
+}
+
+
