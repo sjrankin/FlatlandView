eb07ec4a07c0140f7a4eb4060f314339e2a9371f
Stuart Rankin <sjrankin@gmail.com>
2020-11-04 13:13:24 +0900

    Create EarthquakePreferences.swift
    Earthquake preferences panel.

---
+++ FlatlandView/PreferencePanel/Panels/EarthquakePreferences.swift
@@ -0,0 +1,18 @@
+//
+//  EarthquakePreferences.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/3/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class EarthquakePreferences: NSViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
