ef3eb1aca1b5e37cbbc2b5da488438a999667516
Stuart Rankin <sjrankin@gmail.com>
2020-11-04 13:13:32 +0900

    Create GeneralPreferences.swift
    General preferences panel.

---
+++ FlatlandView/PreferencePanel/Panels/GeneralPreferences.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralPreferences.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/3/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class GeneralPreferences: NSViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
