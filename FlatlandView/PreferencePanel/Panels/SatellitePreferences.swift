cc511fcbc5ad5a4bad550d506944df4f586c5d16
Stuart Rankin <sjrankin@gmail.com>
2020-11-04 13:14:17 +0900

    Create SatellitePreferences.swift
    Satellites preferences panel.

---
+++ FlatlandView/PreferencePanel/Panels/SatellitePreferences.swift
@@ -0,0 +1,18 @@
+//
+//  SatellitePreferences.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/3/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class SatellitePreferences: NSViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
