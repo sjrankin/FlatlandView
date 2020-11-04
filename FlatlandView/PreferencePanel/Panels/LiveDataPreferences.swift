f30da24a49ab32d3a76adf00c546e5c14b2accc8
Stuart Rankin <sjrankin@gmail.com>
2020-11-04 13:13:41 +0900

    Create LiveDataPreferences.swift
    Live data preferences panel.

---
+++ FlatlandView/PreferencePanel/Panels/LiveDataPreferences.swift
@@ -0,0 +1,18 @@
+//
+//  LiveDataPreferences.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/3/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class LiveDataPreferences: NSViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
