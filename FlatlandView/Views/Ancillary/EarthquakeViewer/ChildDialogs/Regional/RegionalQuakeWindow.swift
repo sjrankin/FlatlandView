8f71339755feb80da81df18560104649e19fdfcc
Stuart Rankin <sjrankin@gmail.com>
2020-11-04 13:08:37 +0900

    Create RegionalQuakeWindow.swift
    Placeholder for the window to show regional earthquake settings.

---
+++ FlatlandView/Views/Ancillary/EarthquakeViewer/ChildDialogs/Regional/RegionalQuakeWindow.swift
@@ -0,0 +1,14 @@
+//
+//  RegionalQuakeWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/1/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class RegionalQuakeWindow: NSWindowController
+{
+}
