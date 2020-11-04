3752d20744998e802f2e8b4c7075adca3f4d2a74
Stuart Rankin <sjrankin@gmail.com>
2020-11-04 13:04:51 +0900

    Update Double.swift
    Added RoundedTo(Count, PadTo).

--- FlatlandView/Extensions/Double.swift
+++ FlatlandView/Extensions/Double.swift
@@ -16,12 +16,39 @@ extension Double
     /// Returns a rounded value of the instance double.
     /// - Note: This "rounding" is nothing more than truncation.
     /// - Parameter Count: Number of places to round to.
+    /// - Parameter PadTo: If specified, the number of digits to pad the least significant portion to if less
+    ///                    than `Count`.
     /// - Returns: Rounded value.
     func RoundedTo(_ Count: Int) -> Double
     {
         let Multiplier = pow(10.0, Count)
         let Value = Int(self * Double(truncating: Multiplier as NSNumber))
         return Double(Value) / Double(truncating: Multiplier as NSNumber)
+    }
+    
+    /// Returns a rounded value of the instance double as a string.
+    /// - Note: This "rounding" is nothing more than truncation.
+    /// - Parameter Count: Number of places to round to.
+    /// - Parameter PadTo: If specified, the number of digits to pad the least significant portion to if less
+    ///                    than `Count`.
+    /// - Returns: Rounded value.
+    func RoundedTo(_ Count: Int, PadTo: Int = 3) -> String
+    {
+        let Raw = self.RoundedTo(Count)
+        let RawString = "\(Raw)"
+        let Parts = RawString.split(separator: ".", omittingEmptySubsequences: true)
+        let MostSignificant = String(Parts[0])
+        var RawLeast = String(Parts[1])
+        if RawLeast.count >= PadTo
+        {
+            return RawString
+        }
+        let Delta = PadTo - RawLeast.count
+        for _ in 0 ..< Delta
+        {
+            RawLeast.append("0")
+        }
+        return "\(MostSignificant).\(RawLeast)"
     }
     
     /// Converts the instance value from (an assumed) degrees to radians.
