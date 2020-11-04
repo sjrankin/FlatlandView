e1b5a68880a23fbab5f114e720db32ccae49061b
Stuart Rankin <sjrankin@gmail.com>
2020-11-04 13:13:52 +0900

    Create MapAttributesPreferences.swift
    Map attributes preferences panel.

---
+++ FlatlandView/PreferencePanel/Panels/MapAttributesPreferences.swift
@@ -0,0 +1,18 @@
+//
+//  MapAttributesPreferences.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/4/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class MapAttributesPreferences: NSViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
