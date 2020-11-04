7e12bee2a1c26aa84ca1e8275a9b46940aa08433
Stuart Rankin <sjrankin@gmail.com>
2020-11-04 13:14:07 +0900

    Create POIPreferences.swift
    POI preference panel.

---
+++ FlatlandView/PreferencePanel/Panels/POIPreferences.swift
@@ -0,0 +1,18 @@
+//
+//  POIPreferences.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/3/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class POIPreferences: NSViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
