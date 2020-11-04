3a8d814ea541fc64a36d2a9be8e3f94da13b6407
Stuart Rankin <sjrankin@gmail.com>
2020-11-04 13:09:31 +0900

    Create DetailQuakeWindow.swift
    Placeholder for window to show quake information.

---
+++ FlatlandView/Views/Ancillary/EarthquakeViewer/ChildDialogs/Details/DetailQuakeWindow.swift
@@ -0,0 +1,14 @@
+//
+//  DetailQuakeWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/1/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class DetailQuakeWindow: NSWindowController
+{
+}
