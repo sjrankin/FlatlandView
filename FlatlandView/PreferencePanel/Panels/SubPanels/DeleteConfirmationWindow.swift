266658b4f81f66a1e08ae22db7bd225936d99fdc
Stuart Rankin <sjrankin@gmail.com>
2020-11-07 20:56:34 +0900

    Create DeleteConfirmationWindow.swift
    Placeholder.

---
+++ FlatlandView/PreferencePanel/Panels/SubPanels/DeleteConfirmationWindow.swift
@@ -0,0 +1,14 @@
+//
+//  DeleteConfirmationWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/5/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class DeleteConfirmationWindow: NSWindowController
+{
+}
