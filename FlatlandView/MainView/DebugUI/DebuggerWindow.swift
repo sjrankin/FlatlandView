caf180314aa94bc13b912f4b948839f7517fe95b
Stuart Rankin <sjrankin@gmail.com>
2020-11-11 14:23:43 +0900

    Create DebuggerWindow.swift
    Debugger window code.

---
+++ FlatlandView/MainView/DebugUI/DebuggerWindow.swift
@@ -0,0 +1,61 @@
+//
+//  DebuggerWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/8/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class DebuggerWindow: NSWindowController
+{
+    override func windowDidLoad()
+    {
+        ClockControlBarItem.view?.wantsLayer = true
+        ClockControlBarItem.view?.layer?.cornerRadius = 3.0
+        ClockControlBarItem.view?.layer?.borderWidth = 1.0
+        ClockControlBarItem.view?.layer?.borderColor = NSColor.clear.cgColor
+        DebugLogBarItem.view?.wantsLayer = true
+        DebugLogBarItem.view?.layer?.cornerRadius = 3.0
+        DebugLogBarItem.view?.layer?.borderWidth = 1.0
+        DebugLogBarItem.view?.layer?.borderColor = NSColor.clear.cgColor
+        
+        ButtonMap[ClockControlButton] = ClockControlBarItem
+        ButtonMap[DebugLogButton] = DebugLogBarItem
+    }
+    
+    var ButtonMap = [NSButton: NSToolbarItem]()
+    
+    var PreviousButton: NSButton? = nil
+    
+    func DeHighlight(_ Button: NSButton)
+    {
+        if let Item = ButtonMap[Button]
+        {
+            Item.view?.layer?.backgroundColor = NSColor.clear.cgColor
+            Item.view?.layer?.borderColor = NSColor.clear.cgColor
+        }
+    }
+    
+    func Highlight(_ Button: NSButton)
+    {
+        if PreviousButton != nil
+        {
+            DeHighlight(PreviousButton!)
+        }
+        PreviousButton = Button
+        if let Item = ButtonMap[Button]
+        {
+            Item.view?.layer?.backgroundColor = NSColor.systemPink.cgColor
+            Item.view?.layer?.borderColor = NSColor.systemRed.cgColor
+        }
+    }
+    
+    @IBOutlet weak var ClockControlBarItem: NSToolbarItem!
+    @IBOutlet weak var DebugLogBarItem: NSToolbarItem!
+    
+    @IBOutlet weak var ClockControlButton: NSButton!
+    @IBOutlet weak var DebugLogButton: NSButton!
+}
