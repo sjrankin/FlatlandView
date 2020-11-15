cdf9a0bfee36cb560ec934df10703fadcfd3a28f
Stuart Rankin <sjrankin@gmail.com>
2020-11-15 21:22:18 +0900

    Update String.swift
    Added comments. Added Words and ~=

--- FlatlandView/Extensions/String.swift
+++ FlatlandView/Extensions/String.swift
@@ -13,6 +13,11 @@ import AppKit
 
 extension String
 {
+    /// Converts the passed Double value to a string and ensures it has
+    /// a suffix of ".0".
+    /// - Parameter Raw: The double value to convert.
+    /// - Returns: The string equivalent of `Raw` with a trailing ".0" if the
+    ///            conversion results in no trailing decimal.
     public static func WithTrailingZero(_ Raw: Double) -> String
     {
         let Converted = "\(Raw)"
@@ -25,5 +30,32 @@ extension String
             return Converted + ".0"
         }
         return Converted
+    }
+    
+    /// Returns a list of words in the instance.
+    /// - Note: See [Three ways to enumerate the words in a string using swift](https://medium.com/@sorenlind/three-ways-to-enumerate-the-words-in-a-string-using-swift-7da5504f0062)
+    /// - Returns: Array of words detected by `enumerateSubStrings`.
+    func Words() -> [String]
+    {
+        let FullRange = self.startIndex ..< self.endIndex
+        var WordList = [String]()
+        self.enumerateSubstrings(in: FullRange, options: .byWords)
+        {
+            (substring, _, _, _) -> () in
+            WordList.append(String(substring!))
+        }
+        
+        return WordList
+    }
+    
+    //https://www.hackingwithswift.com/articles/108/how-to-use-regular-expressions-in-swift
+    static func ~= (lhs: String, rhs: String) -> Bool
+    {
+        guard let regex = try? NSRegularExpression(pattern: rhs) else
+        {
+            return false
+        }
+        let range = NSRange(location: 0, length: lhs.utf16.count)
+        return regex.firstMatch(in: lhs, options: [], range: range) != nil
     }
 }
