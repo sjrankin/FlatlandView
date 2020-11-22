772860e12a098b2906a0ba622c6f7ab260421089
Stuart Rankin <sjrankin@gmail.com>
2020-11-22 16:58:54 +0900

    Create StencilPipelineProtocol.swift
    Protocol consumers of stencilled maps must implement.

---
+++ FlatlandView/MainView/StencilPipelineProtocol.swift
@@ -0,0 +1,32 @@
+//
+//  StencilPipelineProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/22/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+/// Protocol that consumers of stenciled images must conform to when using the pipeline stenciler.
+protocol StencilPipelineProtocol: class
+{
+    /// Called when a given pipeline stage has been completed.
+    /// - Parameter Image: The image after having undergone stenciling at the specified pipeline stage. If
+    ///                    nil, an error occurred.
+    /// - Parameter Stage: The completed stage. If nil, an error occurred and `Image` is undefined.
+    /// - Parameter Time: The time in seconds from the start of the pipeline execution. If nil, `Image` is
+    ///                   undefined.
+    func StageCompleted(_ Image: NSImage?, _ Stage: StencilStages?, _ Time: Double?)
+    
+    /// Called at the start of the pipeline process.
+    /// - Parameter Time: The start time of the pipeline process. It is not intended to be used directly but
+    ///                   as the base to the returned time from `StencilPipelineCompleted`.
+    func StencilPipelineStarted(Time: Double)
+    
+    /// Called a tthe end of the pipeline process.
+    /// - Parameter Time: The end time of the pipeline process. To determine duration, subtract the value from
+    ///                   `StencilPipelineStarted` from this value to get the duration in seconds.
+    func StencilPipelineCompleted(Time: Double)
+}
