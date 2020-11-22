7e2be977db0034943742d546b32b4bb736030001
Stuart Rankin <sjrankin@gmail.com>
2020-11-22 11:12:26 +0900

    Update GlobeProtocol.swift
    Added comments.

--- FlatlandView/MainView/3D/GlobeProtocol.swift
+++ FlatlandView/MainView/3D/GlobeProtocol.swift
@@ -9,7 +9,11 @@
 import Foundation
 import AppKit
 
+/// Protocol for the 3D Globe Node.
 protocol GlobeProtocol: class
 {
+    /// Plot a satellite.
+    /// - Parameter Satellite: The satellite to plot.
+    /// - Parameter At: Where to plot the satellite.
     func PlotSatellite(Satellite: Satellites, At: GeoPoint)
 }
