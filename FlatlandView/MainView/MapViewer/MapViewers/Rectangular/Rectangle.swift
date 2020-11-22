95859638c23a89510126618137710c4dc414aac3
Stuart Rankin <sjrankin@gmail.com>
2020-11-22 11:11:27 +0900

    Create Rectangle.swift
    Started initial implementation of first scene for new methodology for viewing maps.

---
+++ FlatlandView/MainView/MapViewer/MapViewers/Rectangular/Rectangle.swift
@@ -0,0 +1,40 @@
+//
+//  Rectangle.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/22/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import SceneKit
+
+class Rectangle: MapScene, MapSceneProtocol
+{
+    // MARK: - MapSceneProtocol functions
+    
+    func SetMapTime(_ Percent: Double)
+    {
+    }
+    
+    func Hide(_ Duration: Double)
+    {
+    }
+    
+    func Show(_ Duration: Double)
+    {
+    }
+    
+    func LockView()
+    {
+    }
+    
+    func UnlockView()
+    {
+    }
+    
+    func ResetCamera()
+    {
+    }
+}
