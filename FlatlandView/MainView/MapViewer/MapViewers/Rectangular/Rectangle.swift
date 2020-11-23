753eff63ab596b706fd8ea0edd407a51e3f8d619
Stuart Rankin <sjrankin@gmail.com>
2020-11-23 21:17:51 +0900

    Update Rectangle.swift
    Updated for changed protocol.

--- FlatlandView/MainView/MapViewer/MapViewers/Rectangular/Rectangle.swift
+++ FlatlandView/MainView/MapViewer/MapViewers/Rectangular/Rectangle.swift
@@ -14,27 +14,51 @@ class Rectangle: MapScene, MapSceneProtocol
 {
     // MARK: - MapSceneProtocol functions
     
+    func SetMapImage(_ Image: NSImage)
+    {
+    }
+    
+    func PlotObject(_ Object: SCNNode2, Latitude: Double, Longitude: Double)
+    {
+    }
+    
+    func PlotObject(_ Object: SCNNode2, X: Double, Y: Double)
+    {
+    }
+    
+    func PlottedObjects() -> [SCNNode2]
+    {
+        return [SCNNode2]()
+    }
+    
+    func RemoveObject(ID: UUID)
+    {
+    }
+    
+    func RemoveObjectClass(ID: UUID)
+    {
+        
+    }
+    
     func SetMapTime(_ Percent: Double)
     {
     }
     
     func Hide(_ Duration: Double)
     {
+        let HideAnimation = SCNAction.fadeOut(duration: Duration)
+        self.rootNode.runAction(HideAnimation)
+        {
+            self.rootNode.opacity = 0.0
+        }
     }
     
     func Show(_ Duration: Double)
     {
-    }
-    
-    func LockView()
-    {
-    }
-    
-    func UnlockView()
-    {
-    }
-    
-    func ResetCamera()
-    {
+        let ShowAnimation = SCNAction.fadeIn(duration: Duration)
+        self.rootNode.runAction(ShowAnimation)
+        {
+            self.rootNode.opacity = 1.0
+        }
     }
 }
