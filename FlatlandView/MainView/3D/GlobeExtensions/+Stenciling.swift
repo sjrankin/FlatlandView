69607a2ea76d24065f2e105e765e470d14acf630
Stuart Rankin <sjrankin@gmail.com>
2020-11-23 21:19:17 +0900

    Update +Stenciling.swift
    Added comments. Removed trialing closure for ApplyStencils. Renamed ApplyStencils to ApplyAllStencils. Removed some commented out code.

--- FlatlandView/MainView/3D/GlobeExtensions/+Stenciling.swift
+++ FlatlandView/MainView/3D/GlobeExtensions/+Stenciling.swift
@@ -14,25 +14,17 @@ extension GlobeView
 {
     // MARK: - Surface stenciling
     
-    /// Apply stencils to `GlobalBaseMap` as needed. When the stenciled map is ready,
-    /// `GotStenciledMap` is called.
+    /// Apply stencils to `GlobalBaseMap` as needed. Stenciled images returned via the `StencilPipelineProtocol`.
     /// - Notes:
     ///    - Control returns almost immediately.
     ///    - The user can change settings such that no stenciling is applied. In that case,
     ///      the non-stenciled map will be available very quickly.
+    ///    - All stencils are applied here (assuming they are enabled by the user).
     /// - Parameter Caller: Name of the caller.
-    /// - Parameter Final: Called after the stencil has been applied.
-    func ApplyStencils(Caller: String? = nil, Final: (() -> ())? = nil)
+    func ApplyAllStencils(Caller: String? = nil)
     {
-        #if DEBUG
-        if let CallerName = Caller
-        {
-            Debug.Print("ApplyStencils called by \(CallerName)")
-        }
-        #endif
         if let Map = GlobalBaseMap
         {
-            #if true
             var Quakes: [Earthquake]? = nil
             var Stages = [StencilStages]()
             if Settings.GetBool(.MagnitudeValuesDrawnOnMap)
@@ -56,56 +48,50 @@ extension GlobeView
             {
                 Stages.append(.GridLines)
             }
-            print("Stencil with \(Stages)")
             Stenciler.RunStencilPipeline(To: Map, Quakes: Quakes, Stages: Stages, Caller: self)
-            #else
-            let ShowEarthquakes = Settings.GetBool(.MagnitudeValuesDrawnOnMap)
-            var Quakes: [Earthquake]? = nil
-            if ShowEarthquakes
-            {
-                if Settings.GetEnum(ForKey: .EarthquakeMagnitudeViews, EnumType: EarthquakeMagnitudeViews.self, Default: .No) == .Stenciled
-                {
-                    Quakes = EarthquakeList
-                }
-            }
-            let ShowUNESCO = Settings.GetBool(.ShowWorldHeritageSites) && Settings.GetBool(.PlotSitesAs2D)
-            Stenciler.AddStencils(To: Map,
-                                  Quakes: Quakes,
-                                  ShowRegions: Settings.GetBool(.ShowEarthquakeRegions),
-                                  PlotCities: Settings.GetBool(.CityNamesDrawnOnMap),
-                                  GridLines: Settings.GetBool(.GridLinesDrawnOnMap),
-                                  UNESCOSites: ShowUNESCO,
-                                  CalledBy: Caller,
-                                  FinalNotify: Final,
-                                  Completed: GotStenciledMap)
-            #endif
         }
     }
     
-    /// Closure for receiving a stenciled map.
-    /// - Parameter Image: The (potentially) stenciled map. Depending on user settings, it is very
-    ///                    possible no stenciling will be done and the map will be made available
-    ///                    very quickly.
-    /// - Parameter Duration: The duration, in seconds, of the stenciling process. If no steciling
-    ///                       was applied, this value will be 0.0.
-    /// - Parameter CalledBy: The name of the caller. May be nil.
-    func GotStenciledMap(_ Image: NSImage, _ Duration: Double, _ CalledBy: String?,
-                         Notify: (() -> ())? = nil)
+    /// Apply stencils to `InitialMap` as needed. Stenciled images returned via the `StencilPipelineProtocol`.
+    /// - Notes: This function only applies earthquake-related stencils. All stenciling is done to the
+    ///          passed image.
+    /// - Parameter Caller: Name of the caller.
+    func ApplyEarthquakeStencils(InitialMap: NSImage, Caller: String? = nil)
     {
-        #if DEBUG
-        let StackFrames = Debug.StackFrameContents(10)
-        Debug.Print(Debug.PrettyStackTrace(StackFrames))
-        if let Caller = CalledBy
+        var Quakes: [Earthquake]? = nil
+        Quakes = EarthquakeList
+        Stenciler.RunStencilPipeline(To: InitialMap, Quakes: Quakes, Stages: [.Earthquakes], Caller: self)
+    }
+    
+    /// Apply initial stencils to `GlobalBaseMap`. Initial stencils are those stencils whose visibility flags
+    /// are known at start-up and do not need remote data. Stenciled images returned via the
+    /// `StencilPipelineProtocol`.
+    /// - Note: Earthquake stenciling is not done in this function - see `ApplyEarthquakeStencils` and
+    ///         `ApplyAllStencils`.
+    /// - Parameter Caller: Name of the caller.
+    func ApplyInitialStencils(Caller: String? = nil)
+    {
+        if let Map = GlobalBaseMap
         {
-            Debug.Print("Stencil available: called by \(Caller), duration: \(Duration.RoundedTo(2))")
+            InitialStenciledMap = nil
+            var Stages = [StencilStages]()
+            if Settings.GetBool(.ShowWorldHeritageSites) && Settings.GetBool(.PlotSitesAs2D)
+            {
+                Stages.append(.UNESCOSites)
+            }
+            if Settings.GetBool(.ShowEarthquakeRegions)
+            {
+                Stages.append(.EarthquakeRegions)
+            }
+            if Settings.GetBool(.CityNamesDrawnOnMap)
+            {
+                Stages.append(.CityNames)
+            }
+            if Settings.GetBool(.GridLinesDrawnOnMap)
+            {
+                Stages.append(.GridLines)
+            }
+            Stenciler.RunStencilPipeline(To: Map, Quakes: nil, Stages: Stages, Caller: self)
         }
-        else
-        {
-            Debug.Print("Stenciling duration: \(Duration)")
-        }
-        #endif
-        EarthNode?.geometry?.firstMaterial?.diffuse.contents = Image
-        Debug.Print("Applied stenciled map.")
-        Notify?()
     }
 }
