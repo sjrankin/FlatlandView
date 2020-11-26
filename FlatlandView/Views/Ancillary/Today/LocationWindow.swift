74ebec8557c87b86991644851c9d58605dd9a4d5
Stuart Rankin <sjrankin@gmail.com>
2020-11-26 21:56:06 +0900

    Create LocationWindow.swift
    Placeholder.

---
+++ FlatlandView/Views/Ancillary/Today/LocationWindow.swift
@@ -0,0 +1,14 @@
+//
+//  LocationWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/26/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class LocationWindow: NSWindowController
+{
+}
