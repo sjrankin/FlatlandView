879373c0404fb01c379d2eeb4ca96ffb0eafa807
Stuart Rankin <sjrankin@gmail.com>
2020-11-27 22:10:58 +0900

    Create ErrorReporterWindow.swift
    Placeholder.

---
+++ FlatlandView/Views/Ancillary/Errors/ErrorReporterWindow.swift
@@ -0,0 +1,14 @@
+//
+//  ErrorReporterWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/27/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class ErrorReporterWindow: NSWindowController
+{
+}
