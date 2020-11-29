fbfbc947b853a10790634f6f44919ec5b158ebeb
Stuart Rankin <sjrankin@gmail.com>
2020-11-29 20:50:48 +0900

    Create CommandLineProtocol.swift
    Protocol for individual commands to refer back to the command line processor.

---
+++ FlatlandView/MainView/DebugUI/DebugPanels/Commands/CommandLineProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  CommandLineProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 11/29/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol CommandLineProtocol: class
+{
+    func LateResults(_ Results: [String])
+}
