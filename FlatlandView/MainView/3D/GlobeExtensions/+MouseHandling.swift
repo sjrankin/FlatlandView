c3c6bf1b4d5bb082c932953f39be02dfb13b25a3
Stuart Rankin <sjrankin@gmail.com>
2020-12-01 21:30:00 +0900

    Update +MouseHandling.swift
    Removed commented out debug statement. Added statement to call MoutAtLocation.

--- FlatlandView/MainView/3D/GlobeExtensions/+MouseHandling.swift
+++ FlatlandView/MainView/3D/GlobeExtensions/+MouseHandling.swift
@@ -190,7 +190,7 @@ extension GlobeView
                     let Where = HitObject[0].worldCoordinates
                     let (Latitude, Longitude) = Mouse3DToGeographicPoint(Mouse: Where)
                     PlotMouseIndicator(Latitude: Latitude, Longitude: Longitude)
-                    //print("Mouse=\(Where.RoundedTo(3)), Percent=\(PrettyPercent.RoundedTo(3))")
+                    MainDelegate?.MouseAtLocation(Latitude: 0.0, Longitude: 0.0, Double(Where.x), Double(Where.y), Double(Where.z))
                 }
             }
         }
@@ -213,8 +213,6 @@ extension GlobeView
     {
         if let CameraPOV = CameraPointOfView
         {
-            //print("Camera Orientation: \(CameraOrientation!.RoundedTo(3))")
-            //print("Camera Rotation: \(CameraRotation!.RoundedTo(3))")
             return (0.0, 0.0)
         }
         else
