24e37e1a95e90a7cbc3f19da95747017d8defb09
Stuart Rankin <sjrankin@gmail.com>
2020-12-10 13:57:36 +0900

    Create PreferencePanelProtocol.swift
    Initial implementation.

---
+++ FlatlandView/PreferencePanel/PreferencePanelProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  PreferencePanelProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 12/9/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol PreferencePanelProtocol: class
+{
+    var Parent: PreferencePanelControllerProtocol? {get set}
+}
