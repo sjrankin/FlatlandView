59cc177fd4058ac6fe11460bfc5deb88492bc914
Stuart Rankin <sjrankin@gmail.com>
2020-12-10 13:59:52 +0900

    Create PreferenceHelpProtocol.swift
    Shows help text.

---
+++ FlatlandView/PreferencePanel/PreferenceHelpProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  PreferenceHelpProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 12/9/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol PreferenceHelpProtocol: class
+{
+    func SetHelpText(_ Text: String)
+}
