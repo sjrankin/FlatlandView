cd430ffdc7a9a27ea19c069d73aef44b6d1be392
Stuart Rankin <sjrankin@gmail.com>
2020-12-10 13:56:07 +0900

    Create StaticTextManager.swift
    Initial implementation (with compile-time errors).

---
+++ FlatlandView/StaticText/StaticTextManager.swift
@@ -0,0 +1,22 @@
+//
+//  StaticTextManager.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 12/9/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+typealias TextID = UUID
+
+class StaticTextManager
+{
+    
+}
+
+enum TextIDs: UUID, CaseIterable
+{
+    case test = ""
+}
