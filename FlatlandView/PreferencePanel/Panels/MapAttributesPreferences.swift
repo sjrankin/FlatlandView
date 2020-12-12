c2f23b77b19611a8ab92359760dac51b379ffd95
Stuart Rankin <sjrankin@gmail.com>
2020-12-12 20:33:27 +0900

    Update MapAttributesPreferences.swift
    Added help button support.

--- FlatlandView/PreferencePanel/Panels/MapAttributesPreferences.swift
+++ FlatlandView/PreferencePanel/Panels/MapAttributesPreferences.swift
@@ -17,4 +17,42 @@ class MapAttributesPreferences: NSViewController, PreferencePanelProtocol
     {
         super.viewDidLoad()
     }
+    
+    @IBAction func HandleHelpButton(_ sender: Any)
+    {
+        if let Button = sender as? NSButton
+        {
+            switch Button
+            {
+                case ShowGridLineHelpButton:
+                    Parent?.ShowHelp(For: .ShowGridLines, Where: Button.bounds, What: ShowGridLineHelpButton)
+                    
+                case GridLineColorHelpButton:
+                    Parent?.ShowHelp(For: .GridLineColor, Where: Button.bounds, What: GridLineColorHelpButton)
+                    
+                case BackgroundColorHelpButton:
+                    Parent?.ShowHelp(For: .BackgroundColor, Where: Button.bounds, What: BackgroundColorHelpButton)
+                    
+                case FlatMapNightLevelHelpButton:
+                    Parent?.ShowHelp(For: .FlatNightDarkness, Where: Button.bounds, What: FlatMapNightLevelHelpButton)
+                    
+                case ShowMoonlightHelpButton:
+                    Parent?.ShowHelp(For: .ShowMoonlight, Where: Button.bounds, What: ShowMoonlightHelpButton)
+                    
+                case PoleShapeHelpButton:
+                    Parent?.ShowHelp(For: .PoleShape, Where: Button.bounds, What: PoleShapeHelpButton)
+            
+                default:
+                    break
+            }
+        }
+    }
+    
+    @IBOutlet weak var ShowGridLineHelpButton: NSButton!
+    @IBOutlet weak var GridLineColorHelpButton: NSButton!
+    @IBOutlet weak var BackgroundColorHelpButton: NSButton!
+    @IBOutlet weak var FlatMapNightLevelHelpButton: NSButton!
+    @IBOutlet weak var ShowMoonlightHelpButton: NSButton!
+    @IBOutlet weak var PoleShapeHelpButton: NSButton!
+    
 }
