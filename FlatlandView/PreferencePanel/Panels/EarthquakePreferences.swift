3c20aa1384c2f7c612f735b9dbdf596223eade36
Stuart Rankin <sjrankin@gmail.com>
2020-12-12 20:33:11 +0900

    Update EarthquakePreferences.swift
    Added help button support.

--- FlatlandView/PreferencePanel/Panels/EarthquakePreferences.swift
+++ FlatlandView/PreferencePanel/Panels/EarthquakePreferences.swift
@@ -17,4 +17,45 @@ class EarthquakePreferences: NSViewController, PreferencePanelProtocol
     {
         super.viewDidLoad()
     }
+    
+    @IBAction func HandleHelpButton(_ sender: Any)
+    {
+        if let Button = sender as? NSButton
+        {
+            switch Button
+            {
+                case AnimateEarthquakeShapeHelpButton:
+                    Parent?.ShowHelp(For: .AnimateQuakes, Where: Button.bounds, What: AnimateEarthquakeShapeHelpButton)
+                    
+                case QuakeRegionsHelpButton:
+                    Parent?.ShowHelp(For: .QuakeRegions, Where: Button.bounds, What: QuakeRegionsHelpButton)
+                    
+                case DisplayQuakesHelpButton:
+                    Parent?.ShowHelp(For: .DisplayQuakes, Where: Button.bounds, What: DisplayQuakesHelpButton)
+                    
+                case EnableRegionsHelpButton:
+                    Parent?.ShowHelp(For: .EnableQuakeRegions, Where: Button.bounds, What: EnableRegionsHelpButton)
+                    
+                case QuakeFetchFrequencyHelpButton:
+                    Parent?.ShowHelp(For: .QuakeFetchFrequency, Where: Button.bounds, What: QuakeFetchFrequencyHelpButton)
+                    
+                case SelectQuakeShapeHelpButton:
+                    Parent?.ShowHelp(For: .QuakeShape, Where: Button.bounds, What: SelectQuakeShapeHelpButton)
+                    
+                case QuakeHighlightHelpButton:
+                    Parent?.ShowHelp(For: .QuakeHighlight, Where: Button.bounds, What: QuakeHighlightHelpButton)
+                    
+                default:
+                    return
+            }
+        }
+    }
+    
+    @IBOutlet weak var AnimateEarthquakeShapeHelpButton: NSButton!
+    @IBOutlet weak var QuakeRegionsHelpButton: NSButton!
+    @IBOutlet weak var DisplayQuakesHelpButton: NSButton!
+    @IBOutlet weak var EnableRegionsHelpButton: NSButton!
+    @IBOutlet weak var QuakeFetchFrequencyHelpButton: NSButton!
+    @IBOutlet weak var SelectQuakeShapeHelpButton: NSButton!
+    @IBOutlet weak var QuakeHighlightHelpButton: NSButton!
 }
