742a0dde097b395e3adadc8dfa89a522999bf7dc
Stuart Rankin <sjrankin@gmail.com>
2020-12-15 16:40:22 +0900

    Update MainWindow.swift
    Deleted code no longer needed due to controls removed from the toolbar.

--- FlatlandView/MainView/MainWindow.swift
+++ FlatlandView/MainView/MainWindow.swift
@@ -16,71 +16,6 @@ class MainWindow: NSWindowController, NSWindowDelegate
     {
         window?.acceptsMouseMovedEvents = true
         WorldLockButton.toolTip = "Locks or unlocks camera motion for views."
-        InitializeViewSegments()
-    }
-    
-    func InitializeViewSegments()
-    {
-        HourSegment.wantsLayer = true
-        HourSegment.layer?.borderWidth = 0.5
-        HourSegment.layer?.borderColor = NSColor.gray.cgColor
-        HourSegment.layer?.cornerRadius = 5.0
-        ViewSegment.wantsLayer = true
-        ViewSegment.layer?.borderWidth = 0.5
-        ViewSegment.layer?.borderColor = NSColor.gray.cgColor
-        ViewSegment.layer?.cornerRadius = 5.0
-        
-        let HourArea = NSTrackingArea(rect: HourSegment.bounds,
-                                      options: [.mouseEnteredAndExited, .activeAlways],
-                                      owner: self,
-                                      userInfo: ["Segment": "Hour"])
-        HourSegment.addTrackingArea(HourArea)
-        let ViewArea = NSTrackingArea(rect: ViewSegment.bounds,
-                                      options: [.mouseEnteredAndExited, .activeAlways],
-                                      owner: self,
-                                      userInfo: ["Segment": "View"])
-        ViewSegment.addTrackingArea(ViewArea)
-    }
-    
-    /// Handle mouse entered a tracking area. This is used to highlight a segment control.
-    /// - Parameter with: The passed event.
-    override func mouseEntered(with event: NSEvent)
-    {
-        if let Segment = event.trackingArea?.userInfo?.values.first as? String
-        {
-            let AccentColor = NSColor.controlAccentColor
-            switch Segment
-            {
-                case "Hour":
-                    HourSegment.layer?.borderColor = AccentColor.cgColor
-                    
-                case "View":
-                    ViewSegment.layer?.borderColor = AccentColor.cgColor
-                    
-                default:
-                    break
-            }
-        }
-    }
-
-    /// Handle mouse exited a tracking area. This is used to unhighlight a segment control.
-    /// - Parameter with: The passed event.
-    override func mouseExited(with event: NSEvent)
-    {
-        if let Segment = event.trackingArea?.userInfo?.values.first as? String
-        {
-            switch Segment
-            {
-                case "Hour":
-                    HourSegment.layer?.borderColor = NSColor.gray.cgColor
-                    
-                case "View":
-                    ViewSegment.layer?.borderColor = NSColor.gray.cgColor
-                    
-                default:
-                    break
-            }
-        }
     }
     
     /// Handle resize window events.
@@ -141,18 +76,8 @@ class MainWindow: NSWindowController, NSWindowDelegate
         Item.image = Image
     }
     
-    /// Change the image of the show info button.
-    /// - Parameter To: The new image.
-    func ChangeShowInfoImage(To Image: NSImage)
-    {
-        ChangeToolbarItemImage(Item: ItemInfoButton, To: Image)
-    }
-    
     @IBOutlet weak var DebuggerButton: NSToolbarItem!
     @IBOutlet weak var WorldLockButton: NSButton!
     @IBOutlet weak var WorldLockBarButton: NSToolbarItem!
-    @IBOutlet weak var ItemInfoButton: NSToolbarItem!
     @IBOutlet weak var MainToolBar: NSToolbar!
-    @IBOutlet weak var HourSegment: NSSegmentedControl!
-    @IBOutlet weak var ViewSegment: NSSegmentedControl!
 }
