222dd478f2060bb01057d876e78644552aafdbca
Stuart Rankin <sjrankin@gmail.com>
2020-12-15 16:42:33 +0900

    Update MouseInfoProtocol.swift
    Added Caller to SetLocation(Double, Double) for debug purposes.

--- FlatlandView/Views/Floating/MouseInfo/MouseInfoProtocol.swift
+++ FlatlandView/Views/Floating/MouseInfo/MouseInfoProtocol.swift
@@ -11,5 +11,5 @@ import Foundation
 protocol MouseInfoProtocol: class
 {
     func SetLocation(Latitude: String, Longitude: String)
-    func SetLocation(Latitude: Double, Longitude: Double)
+    func SetLocation(Latitude: Double, Longitude: Double, Caller: String)
 }
