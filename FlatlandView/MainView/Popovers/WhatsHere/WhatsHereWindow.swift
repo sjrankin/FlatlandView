d923f1de47634f0ed229516c5605837d797063ff
Stuart Rankin <sjrankin@gmail.com>
2020-12-15 16:41:54 +0900

    Create WhatsHereWindow.swift
    Placeholder.

---
+++ FlatlandView/MainView/Popovers/WhatsHere/WhatsHereWindow.swift
@@ -0,0 +1,14 @@
+//
+//  WhatsHereWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 12/13/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class WhatsHereWindow: NSWindowController
+{
+}
