ab5f7712e39978b552c601f78e18710f10380534
Stuart Rankin <sjrankin@gmail.com>
2020-12-22 14:23:19 +0900

    Create HomeLocationWindow.swift
    Placeholder.

---
+++ FlatlandView/PreferencePanel/HelperPanels/HomeLocationWindow.swift
@@ -0,0 +1,14 @@
+//
+//  HomeLocationWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 12/21/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class HomeLocationWindow: NSWindowController
+{
+}
