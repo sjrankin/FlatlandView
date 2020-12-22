c1cbb29b95498920ae816bc2805d9a183183ac32
Stuart Rankin <sjrankin@gmail.com>
2020-12-22 14:22:16 +0900

    Update InputValidation.swift
    Added validation for latitude and longitude. Added latitude/longitude error codes in ValidationResults.

--- FlatlandView/Utilities/InputValidation.swift
+++ FlatlandView/Utilities/InputValidation.swift
@@ -12,6 +12,130 @@ import AppKit
 /// Class that helps with validating user input.
 class InputValidation
 {
+    /// Extract alpha (but not "+" or "-" from the passed string.
+    /// - Parameter From: The source string to extract alpha characters from.
+    /// - Returns: Tuple with a string with alpha characters removed and an array with the removed characters.
+    private static func ExtractAlphaComponents(From: String) -> (Cleaned: String, Alpha: [String])
+    {
+        var Working = From.replacingOccurrences(of: " ", with: "")
+        Working = Working.lowercased()
+        var Result = ""
+        var AlphaValues = [String]()
+        for Char in Working
+        {
+            let ActualChar = String(Char)
+            if Range(uncheckedBounds: ("a","z")).contains(ActualChar)
+            {
+                AlphaValues.append(ActualChar)
+                continue
+            }
+            Result.append(ActualChar)
+        }
+        return (Cleaned: Result, Alpha: AlphaValues)
+    }
+    
+    /// Validate a string that may contain a latitude value entered by the user.
+    /// - Note: Valid input formats are:
+    ///     - Any double value in the range of -90.0 to 90.0.
+    ///     - Any positive double value in the range of 0.0 to 90.0 with a hemispherical suffix of either
+    ///       `s` or `n`.
+    /// - Note: All spaces are removed before validation.
+    /// - Parameter Raw: The raw string entered by the user. The value is treated as case insensitive.
+    /// - Returns: Result code with the validated value on success, an error code on failure.
+    public static func LatitudeValidation(_ Raw: String) -> Result<Double, ValidationResult>
+    {
+        if Raw.isEmpty
+        {
+            return .failure(.MissingInput)
+        }
+        let Working = Raw.lowercased()
+        let (Stripped, Chars) = ExtractAlphaComponents(From: Working)
+        if Chars.count > 1
+        {
+            return .failure(.TooManyHemispheres)
+        }
+        if Chars.count == 1
+        {
+            if Chars[0] == "e" || Chars[0] == "w"
+            {
+                return .failure(.WrongHemisphere)
+            }
+        }
+        var Multiplier = 1.0
+        if Chars.count == 1
+        {
+            if Chars[0] == "s"
+            {
+                Multiplier = -1.0
+            }
+        }
+        if var ActualValue = Double(Stripped)
+        {
+            if ActualValue < 0.0 || Multiplier == -1.0
+            {
+                Multiplier = 1.0
+            }
+            ActualValue = ActualValue * Multiplier
+            if ActualValue < -90.0 || ActualValue > 90.0
+            {
+                return .failure(.LatitudeOutOfRange)
+            }
+            return .success(ActualValue)
+        }
+        return .failure(.CannotParse)
+    }
+    
+    /// Validate a string that may contain a longitude value entered by the user.
+    /// - Note: Valid input formats are:
+    ///     - Any double value in the range of -180.0 to 180.0.
+    ///     - Any positive double value in the range of 0.0 to 180.0 with a hemispherical suffix of either
+    ///       `w` or `e`.
+    /// - Note: All spaces are removed before validation.
+    /// - Parameter Raw: The raw string entered by the user. The value is treated as case insensitive.
+    /// - Returns: Result code with the validated value on success, an error code on failure.
+    public static func LongitudeValidation(_ Raw: String) -> Result<Double, ValidationResult>
+    {
+        if Raw.isEmpty
+        {
+            return .failure(.MissingInput)
+        }
+        let Working = Raw.lowercased()
+        let (Stripped, Chars) = ExtractAlphaComponents(From: Working)
+        if Chars.count > 1
+        {
+            return .failure(.TooManyHemispheres)
+        }
+        if Chars.count == 1
+        {
+            if Chars[0] == "n" || Chars[0] == "s"
+            {
+                return .failure(.WrongHemisphere)
+            }
+        }
+        var Multiplier = 1.0
+        if Chars.count == 1
+        {
+            if Chars[0] == "w"
+            {
+                Multiplier = -1.0
+            }
+        }
+        if var ActualValue = Double(Stripped)
+        {
+            if ActualValue < 0.0 || Multiplier == -1.0
+            {
+                Multiplier = 1.0
+            }
+            ActualValue = ActualValue * Multiplier
+            if ActualValue < -180.0 || ActualValue > 180.0
+            {
+                return .failure(.LongitudeOutOfRange)
+            }
+            return .success(ActualValue)
+        }
+        return .failure(.CannotParse)
+    }
+    
     /// Array of unit names.
     public static let UnitNames = ["km", "mi"]
     
@@ -102,4 +226,14 @@ enum ValidationResult: String, CaseIterable, Error
     case UnknownUnit = "Unknown unit"
     /// Too many units were specified.
     case TooManyUnits = "Too many units"
+    /// Unknown hemisphere found.
+    case UnknownHemisphere = "Unknown hemisphere - must be E, W, N, or S."
+    /// Too many hemispheres specified.
+    case TooManyHemispheres = "Too many hemispheres specified."
+    /// Invalid latitude.
+    case LatitudeOutOfRange = "Latitude must be in range of -90.0 to 90.0"
+    /// Invalid longitude.
+    case LongitudeOutOfRange = "Longitude must be in range of -180.0 to 180.0"
+    /// Invalid hemisphere, eg, N for longitude.
+    case WrongHemisphere = "Invalid hemisphere for coorindate"
 }
