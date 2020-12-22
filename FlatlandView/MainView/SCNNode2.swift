97c5dd73e66d0a196e2c9623328123bdb775c70d
Stuart Rankin <sjrankin@gmail.com>
2020-12-22 14:18:42 +0900

    Update SCNNode2.swift
    Refactoring automatic event generation and attribute changes.

--- FlatlandView/MainView/SCNNode2.swift
+++ FlatlandView/MainView/SCNNode2.swift
@@ -908,20 +908,30 @@ enum NodeStates: String
 
 extension SCNNode2
 {
+    /// Add an event and associated attributes to the node.
     func AddEventAttributes(Event: NodeEvents, Attributes: EventAttributes)
     {
         EventMap[Event] = Attributes
     }
     
+    /// Trigger the passed event.
+    /// - Note:
+    ///   - This has the effect of change attributes of the node. Which attributes are defined in the
+    ///     `Attributes` parameter when `AddEventAttributes` is called.
+    ///   - The event is applied to the node and all `SCNNode2` child nodes.
+    ///   - If an event is passed that has no associated attributes, no action is taken.
+    /// - Parameter Event: The event to trigger.
     func TriggerEvent(_ Event: NodeEvents)
     {
+        if Event == .NoEvent
+        {
+            return
+        }
         if let Attributes = EventMap[Event]
         {
-            print("Setting \(Event.rawValue) attributes")
             self.geometry?.firstMaterial?.diffuse.contents = Attributes.Diffuse
             self.geometry?.firstMaterial?.specular.contents = Attributes.Specular
-            let EmissionColor = Attributes.Emission ?? NSColor.clear
-            self.geometry?.firstMaterial?.emission.contents = EmissionColor//Attributes.Emission
+            self.geometry?.firstMaterial?.emission.contents = Attributes.Emission ?? NSColor.clear
             self.geometry?.firstMaterial?.lightingModel = Attributes.LightModel
             self.geometry?.firstMaterial?.metalness.contents = Attributes.Metalness
             self.geometry?.firstMaterial?.roughness.contents = Attributes.Roughness
@@ -940,24 +950,52 @@ extension SCNNode2
     }
 }
 
+/// Holds Event attributes.
 class EventAttributes
 {
+    /// Get or set the event the attribute is intended for. Used mainly for debugging.
     var ForEvent: NodeEvents? = nil
+    
+    /// Diffuse material color.
     var Diffuse: NSColor? = nil
+    
+    /// Specular material color.
     var Specular: NSColor? = nil
+    
+    /// Emission material color. If nil, a transparent color is applied.
     var Emission: NSColor? = nil
+    
+    /// Lighting mode.
     var LightModel: SCNMaterial.LightingModel = .phong
+    
+    /// Metalness material value.
     var Metalness: Double? = nil
+    
+    /// Roughness material value.
     var Roughness: Double? = nil
+    
+    /// Scale value.
     var Scale: Double? = nil
 }
 
+/// Events that can potentially change `SCNNode2` attributes.
 enum NodeEvents: String
 {
+    /// No event. Nothing will be changed.
     case NoEvent = "Nothing Happened"
+    
+    /// The node moved into day light.
     case SwitchToDay = "SwitchToDay"
+    
+    /// The node moved into night time.
     case SwitchToNight = "SwitchToNight"
+    
+    /// The node appeared.
     case Appear = "Appeared"
+    
+    /// The node disappeared.
     case Disappeared = "Disappeared"
+    
+    /// A magnitude associated with the node changed.
     case MagnitudeChanged = "MagnitudeChanged"
 }
