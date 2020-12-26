76ea1319d452ecfee97408fa946e204cec86cb7e
Stuart Rankin <sjrankin@gmail.com>
2020-12-26 10:50:10 +0900

    Update GeneralPreferences.swift
    Added MainDelegate.

--- FlatlandView/PreferencePanel/Panels/GeneralPreferences.swift
+++ FlatlandView/PreferencePanel/Panels/GeneralPreferences.swift
@@ -12,6 +12,7 @@ import AppKit
 class GeneralPreferences: NSViewController, PreferencePanelProtocol
 {
     weak var Parent: PreferencePanelControllerProtocol? = nil
+    weak var MainDelegate: MainProtocol? = nil
     
     override func viewDidLoad()
     {
