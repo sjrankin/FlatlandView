85123e5bd73a83596aa9f70b2ec32c2822f360ac
Stuart Rankin <sjrankin@gmail.com>
2020-12-26 16:56:39 +0900

    Update +Initialize.swift
    Moved ResetCamera to +Camera.swift.

--- FlatlandView/MainView/3D/GlobeExtensions/+Initialize.swift
+++ FlatlandView/MainView/3D/GlobeExtensions/+Initialize.swift
@@ -295,30 +295,4 @@ extension GlobeView
         CameraNode.position = Settings.GetVector(.InitialCameraPosition, SCNVector3(0.0, 0.0, Defaults.InitialZ.rawValue))
         self.scene?.rootNode.addChildNode(CameraNode)
     }
-    
-    /// Resets the default camera to its original location.
-    /// - Note: In order to prevent the Earth from flying around wildly during the reset transition, a
-    ///         look-at constraint is added for the duration of the transition, and removed once the rotation
-    ///         transition is completed.
-    func ResetCamera()
-    {
-        let Constraint = SCNLookAtConstraint(target: SystemNode)
-        Constraint.isGimbalLockEnabled = false
-        SCNTransaction.begin()
-        SCNTransaction.animationDuration = Defaults.ResetCameraAnimationDuration.rawValue
-        self.pointOfView?.constraints = [Constraint]
-        SCNTransaction.commit()
-        
-        let InitialPosition = Settings.GetVector(.InitialCameraPosition, SCNVector3(0.0, 0.0, Defaults.InitialZ.rawValue))
-        let PositionAction = SCNAction.move(to: InitialPosition, duration: Defaults.ResetCameraAnimationDuration.rawValue)
-        PositionAction.timingMode = .easeOut
-        self.pointOfView?.runAction(PositionAction)
-        
-        let RotationAction = SCNAction.rotateTo(x: 0.0, y: 0.0, z: 0.0, duration: Defaults.ResetCameraAnimationDuration.rawValue)
-        RotationAction.timingMode = .easeOut
-        self.pointOfView?.runAction(RotationAction)
-        {
-            self.pointOfView?.constraints = []
-        }
-    }
 }
