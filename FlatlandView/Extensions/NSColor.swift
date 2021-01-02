6f2a6f4182683fd23a93010f5fa56becafdd623e
Stuart Rankin <sjrankin@gmail.com>
2021-01-02 16:05:36 +0900

    Update NSColor.swift
    Rewrote convenience init?(HexValue: RGBA:) to two functions, one for RGBA values and one for RGB values. Fixed bug that confused bit-wise operators.

--- FlatlandView/Extensions/NSColor.swift
+++ FlatlandView/Extensions/NSColor.swift
@@ -50,36 +50,54 @@ extension NSColor
         }
     }
     
-    /// Create an NSColor using a value interpreted as a hex color value. Interpretation of the
-    /// channels is dependent on the value of `RGBA`.
-    /// - Parameter HexValue: Value to convert to a color.
-    /// - Parameter RGBA: If true, the value in `HexValue` is interpreted as `rrggbbaa`. If false
-    ///                   the value is interpreted as `rrggbb`. Defaults to true.
-    /// - Returns: Nil on error, NSColor on success.
-    convenience init?(HexValue: UInt, RGBA: Bool = true)
+    /// Create an NSColor using a value interpreted as a hex color value. See also `NSColor(RGB:)`.
+    /// - Parameter RGBA: Value to convert to a color. Value is assumed to be in the format of `rrggbbaa`.
+    /// - Returns: `NSColor` based on the passed value.
+    convenience init(RGBA: UInt)
     {
         var Red: UInt = 0
         var Green: UInt = 0
         var Blue: UInt = 0
         var Alpha: UInt = 0xff
-        if RGBA
-        {
-             Red = HexValue & 0xff000000 >> 24
-             Green = HexValue & 0x00ff0000 >> 16
-             Blue = HexValue & 0x0000ff00 >> 8
-             Alpha = HexValue & 0x000000ff >> 0
-        }
-        else
-        {
-             Red = HexValue & 0x00ff0000 >> 16
-             Green = HexValue & 0x0000ff00 >> 8
-             Blue = HexValue & 0x000000ff >> 0
-        }
-        let FinalRed: CGFloat = max(CGFloat(Red) / 255.0, 1.0)
-        let FinalGreen: CGFloat = max(CGFloat(Green) / 255.0, 1.0)
-        let FinalBlue: CGFloat = max(CGFloat(Blue) / 255.0, 1.0)
-        let FinalAlpha: CGFloat = max(CGFloat(Alpha) / 255.0, 1.0)
+        
+        Red = RGBA & 0xff000000
+        Red = Red >> 24
+        Green = RGBA & 0x00ff0000
+        Green = Green >> 16
+        Blue = RGBA & 0x0000ff00
+        Blue = Blue >> 8
+        Alpha = RGBA & 0x000000ff
+        Alpha = Alpha >> 0
+        
+        let FinalRed: CGFloat = CGFloat(Red) / 255.0
+        let FinalGreen: CGFloat = CGFloat(Green) / 255.0
+        let FinalBlue: CGFloat = CGFloat(Blue) / 255.0
+        let FinalAlpha: CGFloat = CGFloat(Alpha) / 255.0
         self.init(red: FinalRed, green: FinalGreen, blue: FinalBlue, alpha: FinalAlpha)
+    }
+    
+    /// Create an NSColor using a value interpreted as a hex color value. The value to convert is assumed
+    /// to be an RGB value. See also `NSColor(RGBA:)`.
+    /// - Warning: If the value of `RGB` is greater than `0xffffff` a fatal error will be thrown.
+    /// - Parameter RGB: Value to convert to a color in the format `rrggbb`. Alpha is assigned 1.0.
+    /// - Returns: `NSColor` base on the passed value.
+    convenience init(RGB: Int)
+    {
+        if RGB > 0xffffff
+        {
+            Debug.FatalError("RGB value of \(RGB) is too big to convert to a color.")
+        }
+        
+        var Red = RGB & 0xff0000
+        Red = Red >> 16
+        var Green = RGB & 0x00ff00
+        Green = Green >> 8
+        let Blue = RGB & 0x0000ff >> 0
+        
+        let FinalRed: CGFloat = CGFloat(Red) / 255.0
+        let FinalGreen: CGFloat = CGFloat(Green) / 255.0
+        let FinalBlue: CGFloat = CGFloat(Blue) / 255.0
+        self.init(red: FinalRed, green: FinalGreen, blue: FinalBlue, alpha: 1.0)
     }
     
     /// Returns an interpolated color between the two passed colors.
