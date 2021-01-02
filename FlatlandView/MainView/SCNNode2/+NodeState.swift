99b19890a9c353908607838c82ae2e48a2f94a8f
Stuart Rankin <sjrankin@gmail.com>
2021-01-02 16:07:02 +0900

    Create +NodeState.swift
    Contains node state variables and definitions for SCNNode2.

---
+++ FlatlandView/MainView/SCNNode2/+NodeState.swift
@@ -0,0 +1,49 @@
+//
+//  +NodeState.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 1/1/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import SceneKit
+
+/// Visual state description for `SCNNode2` states.
+struct NodeState
+{
+    /// Node state.
+    let State: NodeStates
+    
+    /// Diffuse color.
+    let Color: NSColor
+    
+    /// Diffuse surface image. If nil, no image supplied.
+    let Diffuse: NSImage?
+    
+    /// Emission color. If nil, `NSColor.clear` is applied to the emission contents.
+    let Emission: NSColor?
+    
+    /// Specular color.
+    let Specular: NSColor?
+    
+    /// Lighting model.
+    let LightModel: SCNMaterial.LightingModel
+    
+    /// Metalness value. If nil, not used.
+    let Metalness: Double?
+    
+    /// Roughness value. If nil, not used.
+    let Roughness: Double?
+}
+
+/// States for `SCNNode2` instances.
+enum NodeStates: String
+{
+    /// Node is in the daylight.
+    case Day = "Day"
+    
+    // Node is in the dark.
+    case Night = "Night"
+}
