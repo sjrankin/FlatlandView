37e87bbb8af521d2320a1b30eef1f3f8d99b54cd
Stuart Rankin <sjrankin@gmail.com>
2021-01-07 16:05:35 +0900

    Create Notification.swift
    Added notification constant.

---
+++ FlatlandView/Extensions/Notification.swift
@@ -0,0 +1,17 @@
+//
+//  Notification.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 1/7/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+// MARK: - Notification.Name extensions.
+
+extension Notification.Name
+{
+    static let AppleInterfaceThemeChangedNotification = Notification.Name("AppleInterfaceThemeChangedNotification")
+}
