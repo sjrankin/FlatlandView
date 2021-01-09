1a89d0c466fbc707989bf6f532ca832ac6cf2075
Stuart Rankin <sjrankin@gmail.com>
2021-01-09 20:50:06 +0900

    Update EarthquakePreferences.swift
    Continued implementation.

--- FlatlandView/PreferencePanel/Panels/EarthquakePreferences.swift
+++ FlatlandView/PreferencePanel/Panels/EarthquakePreferences.swift
@@ -17,7 +17,35 @@ class EarthquakePreferences: NSViewController, PreferencePanelProtocol
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        FetchFrequencyCombo.removeAllItems()
+        FetchFrequencyCombo.addItem(withObjectValue: "30 seconds")
+        FetchFrequencyCombo.addItem(withObjectValue: "1 minute")
+        FetchFrequencyCombo.addItem(withObjectValue: "5 minutes")
+        FetchFrequencyCombo.addItem(withObjectValue: "10 minutes")
+        FetchFrequencyCombo.addItem(withObjectValue: "30 minutes")
+        FetchFrequencyCombo.addItem(withObjectValue: "1 hour")
+        let FetchInterval = Settings.GetDouble(.EarthquakeFetchInterval, 60.0 * 5.0)
+        if let Index = FetchFrequencies.firstIndex(of: FetchInterval)
+        {
+            FetchFrequencyCombo.selectItem(at: Index)
+        }
+        else
+        {
+            FetchFrequencyCombo.selectItem(at: 2)
+        }
+        QuakeShapeCombo.removeAllItems()
+        let EShape = Settings.GetEnum(ForKey: .EarthquakeShapes, EnumType: EarthquakeShapes.self, Default: .Sphere)
+        for SomeShape in EarthquakeShapes.allCases
+        {
+            QuakeShapeCombo.addItem(withObjectValue: SomeShape.rawValue)
+        }
+        QuakeShapeCombo.selectItem(withObjectValue: EShape.rawValue)
+        DisplayQuakesSwitch.state = Settings.GetBool(.EnableEarthquakes) ? .on : .off
+        HighlightNewQuakesSwitch.state = Settings.GetState(.HighlightRecentEarthquakes)
     }
+    
+    let FetchFrequencies: [Double] = [30.0, 60.0, 300.0, 600.0, 1800.0, 3600.0]
     
     @IBAction func HandleHelpButton(_ sender: Any)
     {
@@ -25,9 +53,6 @@ class EarthquakePreferences: NSViewController, PreferencePanelProtocol
         {
             switch Button
             {
-                case AnimateEarthquakeShapeHelpButton:
-                    Parent?.ShowHelp(For: .AnimateQuakes, Where: Button.bounds, What: AnimateEarthquakeShapeHelpButton)
-                    
                 case QuakeRegionsHelpButton:
                     Parent?.ShowHelp(For: .QuakeRegions, Where: Button.bounds, What: QuakeRegionsHelpButton)
                     
@@ -46,6 +71,78 @@ class EarthquakePreferences: NSViewController, PreferencePanelProtocol
                 case QuakeHighlightHelpButton:
                     Parent?.ShowHelp(For: .QuakeHighlight, Where: Button.bounds, What: QuakeHighlightHelpButton)
                     
+                case CheckEarthquakesNowHelpbutton:
+                    Parent?.ShowHelp(For: .QuakeCheckNow, Where: Button.bounds, What: CheckEarthquakesNowHelpbutton)
+                    
+                default:
+                    return
+            }
+        }
+    }
+    
+    @IBAction func SetRegionsButtonHandler(_ sender: Any)
+    {
+        let Storyboard = NSStoryboard(name: "PreferencePanel", bundle: nil)
+        if let WindowController = Storyboard.instantiateController(withIdentifier: "EarthquakeRegionWindow3") as? EarthquakeRegionWindow3
+        {
+            let Window = WindowController.window
+            self.view.window?.beginSheet(Window!)
+            {
+                _ in
+            }
+        }
+    }
+    
+    @IBAction func HandleCheckForEarthquakesNow(_ sender: Any)
+    {
+        MainDelegate?.GetEarthquakeController()?.ForceFetch()
+    }
+    
+    @IBAction func HandleHighlightEarthquakesChanged(_ sender: Any)
+    {
+        if let Switch = sender as? NSSwitch
+        {
+            Settings.SetState(.HighlightRecentEarthquakes, Switch.state)
+        }
+    }
+    
+    @IBAction func HandleEnableRegionsSwitchChanged(_ sender: Any)
+    {
+
+    }
+    
+    @IBAction func HandleEnableQuakesChanged(_ sender: Any)
+    {
+        if let Switch = sender as? NSSwitch
+        {
+            Settings.SetBool(.EnableEarthquakes, Switch.state == .on ? true : false)
+        }
+    }
+    
+    @IBAction func HandleComboChanged(_ sender: Any)
+    {
+        if let Combo = sender as? NSComboBox
+        {
+            let Index = Combo.indexOfSelectedItem
+            switch Combo
+            {
+                case QuakeShapeCombo:
+                    if let RawValue = Combo.objectValueOfSelectedItem as? String
+                    {
+                        if let Raw = EarthquakeShapes(rawValue: RawValue)
+                        {
+                            Settings.SetEnum(Raw, EnumType: EarthquakeShapes.self, ForKey: .EarthquakeShapes)
+                        }
+                    }
+                    
+                case FetchFrequencyCombo:
+                    if Index >= FetchFrequencies.count
+                    {
+                        return
+                    }
+                    let Frequency = FetchFrequencies[Index]
+                    Settings.SetDouble(.EarthquakeFetchInterval, Frequency)
+                    
                 default:
                     return
             }
@@ -54,10 +151,14 @@ class EarthquakePreferences: NSViewController, PreferencePanelProtocol
     
     func SetDarkMode(To: Bool)
     {
-        
     }
     
-    @IBOutlet weak var AnimateEarthquakeShapeHelpButton: NSButton!
+    @IBOutlet weak var QuakeShapeCombo: NSComboBox!
+    @IBOutlet weak var FetchFrequencyCombo: NSComboBox!
+    @IBOutlet weak var HighlightNewQuakesSwitch: NSSwitch!
+    @IBOutlet weak var EnableRegionsSwitch: NSSwitch!
+    @IBOutlet weak var DisplayQuakesSwitch: NSSwitch!
+    @IBOutlet weak var CheckEarthquakesNowHelpbutton: NSButton!
     @IBOutlet weak var QuakeRegionsHelpButton: NSButton!
     @IBOutlet weak var DisplayQuakesHelpButton: NSButton!
     @IBOutlet weak var EnableRegionsHelpButton: NSButton!
