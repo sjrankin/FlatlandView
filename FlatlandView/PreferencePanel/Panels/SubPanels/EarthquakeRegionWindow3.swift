f79289788b65079392e7a0d38ac7aa95c65a0cbb
Stuart Rankin <sjrankin@gmail.com>
2021-01-09 20:45:05 +0900

    Create EarthquakeRegionWindow3.swift
    Place holder.

---
+++ FlatlandView/PreferencePanel/Panels/SubPanels/EarthquakeRegionWindow3.swift
@@ -0,0 +1,14 @@
+//
+//  EarthquakeRegionWindow3.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 1/9/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class EarthquakeRegionWindow3: NSWindowController
+{
+}
