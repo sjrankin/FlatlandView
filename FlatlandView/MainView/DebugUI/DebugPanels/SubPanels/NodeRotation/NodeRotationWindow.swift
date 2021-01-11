04546dc52e46df953ba97704eb4192856403f87b
Stuart Rankin <sjrankin@gmail.com>
2021-01-11 18:03:27 +0900

    Create NodeRotationWindow.swift
    Place holder.

---
+++ FlatlandView/MainView/DebugUI/DebugPanels/SubPanels/NodeRotation/NodeRotationWindow.swift
@@ -0,0 +1,14 @@
+//
+//  NodeRotationWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 1/10/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class NodeRotationWindow: NSWindowController
+{
+}
