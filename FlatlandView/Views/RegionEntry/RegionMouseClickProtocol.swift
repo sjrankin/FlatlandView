f253f35bd392c937ee8055de672a44ae7c32e846
Stuart Rankin <sjrankin@gmail.com>
2021-01-13 17:59:39 +0900

    Create RegionMouseClickProtocol.swift
    Protocol for notifying the region editor where the user clicked on the globe.

---
+++ FlatlandView/Views/RegionEntry/RegionMouseClickProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  RegionMouseClickProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 1/12/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol RegionMouseClickProtocol: class
+{
+    func MouseClicked(At: GeoPoint)
+}
