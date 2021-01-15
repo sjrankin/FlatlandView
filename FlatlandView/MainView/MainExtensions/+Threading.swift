2400120690e3ff8e5fcc3fce9bca51007a55914f
Stuart Rankin <sjrankin@gmail.com>
2021-01-15 16:49:53 +0900

    Create +Threading.swift
    Placeholder for thread management.

---
+++ FlatlandView/MainView/MainExtensions/+Threading.swift
@@ -0,0 +1,17 @@
+//
+//  +Threading.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 1/15/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+extension MainController
+{
+    func ForceKillThreads()
+    {
+    }
+}
