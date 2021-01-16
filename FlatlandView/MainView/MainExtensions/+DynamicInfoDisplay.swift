d547c2b8a74702f4edea55a0880ec04daf58f2d3
Stuart Rankin <sjrankin@gmail.com>
2021-01-16 15:58:09 +0900

    Update +DynamicInfoDisplay.swift
    Added UpdateMouseWindowLocation to keep the mouse window in the same relative location regardless of the size of the main window.

--- FlatlandView/MainView/MainExtensions/+DynamicInfoDisplay.swift
+++ FlatlandView/MainView/MainExtensions/+DynamicInfoDisplay.swift
@@ -13,6 +13,22 @@ extension MainController
 {
     // MARK: - Geographic coordinate display
     
+    /// Update the location of the mouse information window. Called in response to window size changes.
+    func UpdateMouseWindowLocation()
+    {
+        if Settings.GetBool(.FollowMouse)
+        {
+            if let Controller = MouseLocationController
+            {
+                let ViewWidth = Controller.view.frame.width
+                Controller.view.frame = NSRect(x: self.view.frame.size.width - ViewWidth - 10,
+                                               y: 40.0,
+                                               width: ViewWidth,
+                                               height: 100.0)
+            }
+        }
+    }
+    
     /// Show or hide the mouse location view.
     /// - Parameter Visible: If true, the mouse location is shown. If false, it is hidden. Calling this function
     ///                      with `Visible` set to true twice (or more) in a row results in no action being
@@ -28,6 +44,7 @@ extension MainController
             let Storyboard = NSStoryboard(name: "FloatingViews", bundle: nil)
             if let Controller = Storyboard.instantiateController(withIdentifier: "MouseInfoController") as? MouseInfoController
             {
+                MouseLocationController = Controller
                 Controller.view.wantsLayer = true
                 Controller.view.layer?.zPosition = 100000
                 Controller.view.alphaValue = 0.0
