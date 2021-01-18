6d9bc37e6a77ade29145ffd5861883f14cbbd26a
Stuart Rankin <sjrankin@gmail.com>
2021-01-18 17:03:39 +0900

    Create CaptiveDialogManagementProtocol.swift
    Protocol to control the captive dialog container class.

---
+++ FlatlandView/MainView/MainExtensions/CaptiveDialog/CaptiveDialogManagementProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  CaptiveDialogManagementProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 1/18/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol CaptiveDialogManagementProtocol: class
+{
+    func CloseCaptiveDialog()
+}
