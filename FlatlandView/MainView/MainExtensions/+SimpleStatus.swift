12696ce57bd930bba636813a66e477ae61459218
Stuart Rankin <sjrankin@gmail.com>
2021-01-21 20:46:47 +0900

    Update +SimpleStatus.swift
    Commented out all functionality prior to deleting file. Moved QueuedMessage to other file.

--- FlatlandView/MainView/MainExtensions/+SimpleStatus.swift
+++ FlatlandView/MainView/MainExtensions/+SimpleStatus.swift
@@ -11,6 +11,7 @@ import AppKit
 
 extension MainController
 {
+    #if false
     // MARK: - Simple status handling.
     
     /// Initialize the simple status bar.
@@ -307,37 +308,6 @@ extension MainController
         }
         PushedMessage = QueuedMessage(Text, Expiry: Duration, ID: ID)
     }
+    #endif
 }
 
-/// Holds one enqueued message.
-class QueuedMessage
-{
-    /// Initializer.
-    /// - Parameter Message: The message to display.
-    /// - Parameter Expiry: How long to display the message in seconds.
-    /// - Parameter ID: The ID of the message.
-    init(_ Message: String, Expiry: Double, ID: UUID)
-    {
-        self.Message = Message
-        ExpiresIn = Expiry
-        self.ID = ID
-    }
-    
-    /// The message to display.
-    var Message: String = ""
-    
-    /// How long to display the text.
-    var ExpiresIn: Double = 60.0
-    
-    /// ID of the message.
-    var ID: UUID = UUID()
-    
-    /// Timer used for push messages.
-    var PushTimer: Timer? = nil
-    
-    /// The time the pushed message first appeared.
-    var PushStartTime: Double = 0
-    
-    /// Time the message has been visible.
-    var ShownFor: Double = 0
-}
