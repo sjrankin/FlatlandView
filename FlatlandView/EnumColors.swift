0c1d5391964d97106dc4e7aff10ee2972546c2b6
Stuart Rankin <sjrankin@gmail.com>
2021-01-21 20:42:59 +0900

    Update EnumColors.swift
    Added status bar colors. The value of .StatusBorder will depend on whether #DEBUG is defined when Flatland is compiled.

--- FlatlandView/EnumColors.swift
+++ FlatlandView/EnumColors.swift
@@ -13,16 +13,21 @@ enum Colors3D: Int, CaseIterable
 {
     /// Required to keep the compiler happy.
     typealias RawValue = Int
-    
     /// Color of the floating hours.
     case HourColor = 0xff7518
-    
     /// Color of glowing hours.
     case GlowingHourColor = 0xa00000
-    
     /// Specular color for floating hours.
     case HourSpecular = 0xffffff
-    
     /// Color for specular surface contents in general.
     case GeneralSpecular = 0xfffffe
+    #if DEBUG
+    /// Color for the status bar when compiled with #DEBUG.
+    case StatusBorder = 0xffff00
+    #else
+    /// Color for the status bar when compiled for release
+    case StatusBorder = 0x343434
+    #endif
+    /// Color for the status bar background.
+    case StatusBackground = 0x000000
 }
