bdf3b543d6f8b0afa612cf855ad248f66537fc48
Stuart Rankin <sjrankin@gmail.com>
2021-01-21 14:32:06 +0900

    Update MainWindow.swift
    Removed code related to NSWindowDelegate (moved to other file).

--- FlatlandView/MainView/MainWindow.swift
+++ FlatlandView/MainView/MainWindow.swift
@@ -10,64 +10,13 @@ import Foundation
 import AppKit
 import CoreGraphics
 
-class MainWindow: NSWindowController, NSWindowDelegate
+class MainWindow: NSWindowController
 {
     override func windowDidLoad()
     {
         super.windowDidLoad()
-        window?.delegate = self
         window?.acceptsMouseMovedEvents = true
         WorldLockButton.toolTip = "Locks or unlocks camera motion for views."
-    }
-    
-    /// Handle resize window events.
-    func windowWillResize(_ sender: NSWindow, to frameSize: NSSize) -> NSSize
-    {
-        let VC = window?.contentViewController as? MainController
-        VC?.WindowResized(To: frameSize)
-        return frameSize
-    }
-    
-    /// Returns the current window location.
-    /// - Returns: location of the upper-left corner of the window.
-    func WindowLocation() -> CGPoint
-    {
-        if window!.windowNumber < 0
-        {
-            return CGPoint.zero
-        }
-        let CurrentID = CGWindowID(window!.windowNumber)
-        if let WindowList = CGWindowListCopyWindowInfo([.optionAll], kCGNullWindowID) as? [[String: AnyObject]]
-        {
-            for SomeWindow in WindowList
-            {
-                let Number = SomeWindow[kCGWindowNumber as String]!
-                let Bounds = CGRect(dictionaryRepresentation: SomeWindow[kCGWindowBounds as String] as! CFDictionary)!
-                let WindowName = SomeWindow[kCGWindowName as String] as? String ?? ""
-                let WindowNumber = Number as! UInt32
-                if WindowNumber == CurrentID && WindowName == "FlatlandView"
-                {
-                    return CGPoint(x: Bounds.origin.x, y: Bounds.origin.y)
-                }
-            }
-        }
-        return CGPoint.zero
-    }
-    
-    /// Handle window moved notifications.
-    /// - Parameter notification: The notification with window position information.
-    func windowDidMove(_ notification: Notification)
-    {
-        let WindowOrigin = WindowLocation()
-        let VC = window?.contentViewController as? MainController
-        VC?.WindowMovedTo(WindowOrigin)
-    }
-
-    /// Handle closing window events.
-    func windowWillClose(_ notification: Notification)
-    {
-        let VC = window?.contentViewController as? MainController
-        VC?.WillClose()
     }
     
     /// Change the image of the passed toolbar item to the passed image.
