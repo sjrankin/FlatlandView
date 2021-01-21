4a37198a96e08e58017f00ca09b66a4809871ad0
Stuart Rankin <sjrankin@gmail.com>
2021-01-21 14:34:07 +0900

    Update RegionEntryProtocol.swift
    Added comments and functions for transient regions and mouse indicator handling.

--- FlatlandView/Views/RegionEntry/RegionEntryProtocol.swift
+++ FlatlandView/Views/RegionEntry/RegionEntryProtocol.swift
@@ -9,12 +9,35 @@
 import Foundation
 import AppKit
 
+/// Protocol for the region entry dialog to communicate with the globe.
 protocol RegionEntryProtocol: class
 {
+    /// Notify implementors that editing was successfully completed.
     func RegionEntryCompleted(Name: String, Color: NSColor, Corner1: GeoPoint, Corner2: GeoPoint)
+    /// Notify implementors that polar region editing was successfully completed.
+    func PolarRegionEntryCompleted(Name: String, Color: NSColor, Radius: Double)
+    /// Notify implementors that region creation was canceled.
     func RegionEntryCanceled()
+    /// Plot the upper-left (north west) corner of the region.
     func PlotUpperLeftCorner(Latitude: Double, Longitude: Double)
+    /// Plot the lower-right (south east) corner of the region.
     func PlotLowerRightCorner(Latitude: Double, Longitude: Double)
+    /// Remove the upper-left corner of the region.
     func RemoveUpperLeftCorner()
+    /// Remove the lower-right corner of the region.
     func RemoveLowerRightCorner()
+    /// Set the mouse pointer to a starting pin.
+    func SetStartPin()
+    /// Set the mouse pointer to an ending pin.
+    func SetEndPin()
+    /// Set the mouse point to its normal state.
+    func ResetMousePointer()
+    /// Plot a transient region from region data entered by the user.
+    func PlotTransient(ID: UUID, Point1: GeoPoint, Point2: GeoPoint, Color: NSColor)
+    /// Update a transient region.
+    func UpdateTransient(ID: UUID, Point1: GeoPoint, Point2: GeoPoint, Color: NSColor)
+    /// Remove transient regions.
+    func RemoveTransientRegions()
+    /// Remove the specified transient region.
+    func RemoveTransientRegion(ID: UUID)
 }
