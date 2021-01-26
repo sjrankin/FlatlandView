d68de2e75fb22c55d8e6685fa8fa7f663f2b4c2a
Stuart Rankin <sjrankin@gmail.com>
2021-01-26 13:42:22 +0900

    Create NewColorProtocol.swift
    New color communication protocol.

---
+++ FlatlandView/ColorPicker/EntryPanels/NewColorProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  NewColorProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 1/25/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol NewColorProtocol: class
+{
+    func NewColorEntered(_ Color: NSColor)
+}
+
