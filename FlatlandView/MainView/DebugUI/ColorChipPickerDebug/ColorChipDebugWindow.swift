b12db94abbe5967c1cdbd716f67db1bd9ac0406d
Stuart Rankin <sjrankin@gmail.com>
2021-01-26 13:37:09 +0900

    Create ColorChipDebugWindow.swift
    Placeholder

---
+++ FlatlandView/MainView/DebugUI/ColorChipPickerDebug/ColorChipDebugWindow.swift
@@ -0,0 +1,14 @@
+//
+//  ColorChipDebugWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 1/25/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class ColorChipDebugWindow: NSWindowController
+{
+}
