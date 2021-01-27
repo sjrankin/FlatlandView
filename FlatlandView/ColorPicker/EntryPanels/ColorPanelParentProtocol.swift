e733d4c3e922ed95f2c59761c65d166eab01a485
Stuart Rankin <sjrankin@gmail.com>
2021-01-27 10:39:41 +0900

    Create ColorPanelParentProtocol.swift
    Protocol to communicate new colors to the main color picker code.

---
+++ FlatlandView/ColorPicker/EntryPanels/ColorPanelParentProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  ColorPanelParentProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 1/26/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol ColorPanelParentProtocol: class
+{
+    func NewColorFromPanel(_ Color: NSColor, From: ColorPanelTypes)
+}
