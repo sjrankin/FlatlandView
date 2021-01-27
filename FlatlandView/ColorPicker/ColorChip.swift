185c559a6e05a9566e3b12ada2068514db6afe00
Stuart Rankin <sjrankin@gmail.com>
2021-01-27 10:40:17 +0900

    Update ColorChip.swift
    Added comments. Added overridden draw function to make sure colors and layers are updated as appropriate.

--- FlatlandView/ColorPicker/ColorChip.swift
+++ FlatlandView/ColorPicker/ColorChip.swift
@@ -9,6 +9,7 @@
 import Foundation
 import AppKit
 
+/// Creates a code-only color chip that supports transparent colors.
 class ColorChip: NSView, ColorPickerDelegate
 {
     override init(frame frameRect: NSRect)
@@ -23,6 +24,12 @@ class ColorChip: NSView, ColorPickerDelegate
         InitializeUI()
     }
     
+    override func draw(_ dirtyRect: NSRect)
+    {
+        CheckLayer.frame = bounds
+        ColorLayer.frame = bounds
+    }
+    
     func InitializeUI()
     {
         wantsLayer = true
@@ -30,7 +37,7 @@ class ColorChip: NSView, ColorPickerDelegate
         layer?.cornerRadius = 5.0
         layer?.borderWidth = 4.0
         layer?.borderColor = IsStatic ? NSColor.systemGray.cgColor : NSColor.black.cgColor
-        let CheckLayer = CATiledLayer()
+        CheckLayer = CATiledLayer()
         CheckLayer.frame = bounds
         CheckLayer.contents = NSImage(named: "SquareCheckerboard")
         CheckLayer.contentsGravity = .resizeAspectFill
@@ -44,8 +51,10 @@ class ColorChip: NSView, ColorPickerDelegate
         layer?.addSublayer(ColorLayer)
     }
     
+    var CheckLayer = CATiledLayer()
     var ColorLayer = CALayer()
     
+    /// Holds the color to display as well as updating the control when new colors are set.
     private var _Color: NSColor = NSColor.white
     {
         didSet
@@ -53,6 +62,7 @@ class ColorChip: NSView, ColorPickerDelegate
             UpdateColor(_Color)
         }
     }
+    /// Get or set the color to display.
     public var Color: NSColor
     {
         get
@@ -65,6 +75,7 @@ class ColorChip: NSView, ColorPickerDelegate
         }
     }
     
+    /// Holds the static flag.
     private var _IsStatic: Bool = true
     /// Determines whether the user can change the color by clicking on this control. If `true`, when the user
     /// clicks the control, the `ColorPicker` is instantiated to let the user change the color. If `false`,
@@ -81,11 +92,14 @@ class ColorChip: NSView, ColorPickerDelegate
         }
     }
     
+    /// Updates the color of the display.
     private func UpdateColor(_ NewColor: NSColor)
     {
         ColorLayer.backgroundColor = NewColor.cgColor
     }
     
+    /// Handle mouse down events.
+    /// - Note: If `IsStatic` is false and the user clicks with the left mouse, the color picker will be run.
     override func mouseDown(with event: NSEvent)
     {
         if !IsStatic
