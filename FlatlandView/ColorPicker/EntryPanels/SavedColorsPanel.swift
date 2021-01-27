d123e95e58caa3dbe00f0a0dcbfe5e94906e8918
Stuart Rankin <sjrankin@gmail.com>
2021-01-27 10:38:47 +0900

    Create SavedColorsPanel.swift
    Code to run the saved color panel UI.

---
+++ FlatlandView/ColorPicker/EntryPanels/SavedColorsPanel.swift
@@ -0,0 +1,45 @@
+//
+//  SavedColorsPanel.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 1/26/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class SavedColorsPanel: NSViewController, NSTableViewDelegate, NSTableViewDataSource, ColorPanelProtocol
+{
+    public weak var Parent: ColorPanelParentProtocol? = nil
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear()
+    {
+        super.viewDidAppear()
+    }
+    
+    func SetColor(_ Color: NSColor, From: ColorPanelTypes)
+    {
+        if From != .SavedColors
+        {
+            
+        }
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int
+    {
+        return 0
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView?
+    {
+        return nil
+    }
+    
+    @IBOutlet weak var SavedColorsTable: NSTableView!
+}
