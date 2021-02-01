f9e25935e4aa706083a3f03ed03e9983b646e569
Stuart Rankin <sjrankin@gmail.com>
2021-02-01 09:16:45 +0900

    Create RadialRegionEntryWindow.swift
    Placeholder.

---
+++ FlatlandView/Views/RegionEntry/Radial/RadialRegionEntryWindow.swift
@@ -0,0 +1,14 @@
+//
+//  RadialRegionEntryWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 1/30/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class RadialRegionEntryWindow: NSWindowController
+{
+}
