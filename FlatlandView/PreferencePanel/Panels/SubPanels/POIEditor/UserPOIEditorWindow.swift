fb281a72c5eceafd58b3b1ae49403ff39c19649b
Stuart Rankin <sjrankin@gmail.com>
2021-02-07 11:32:18 +0900

    Create UserPOIEditorWindow.swift
    Placeholder.

---
+++ FlatlandView/PreferencePanel/Panels/SubPanels/POIEditor/UserPOIEditorWindow.swift
@@ -0,0 +1,14 @@
+//
+//  UserPOIEditorWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 2/6/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class UserPOIEditorWindow: NSWindowController
+{
+}
