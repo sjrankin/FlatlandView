f071ec312ba3f2a40bafaf5cb95b04db42543f6f
Stuart Rankin <sjrankin@gmail.com>
2021-02-14 17:20:58 +0900

    Update GlobeView.swift
    Added MouseIsVisible flag to ensure NSCursor doesn't get confused.

--- FlatlandView/MainView/3D/GlobeView.swift
+++ FlatlandView/MainView/3D/GlobeView.swift
@@ -405,6 +405,8 @@ class GlobeView: SCNView, FlatlandEventProtocol, StencilPipelineProtocol
     
     // MARK: - Variables for extensions.
     
+    var MouseIsVisible = true
+    
     /// List of hours in Japanese Kanji.
     let JapaneseHours = [0: "〇", 1: "一", 2: "二", 3: "三", 4: "四", 5: "五", 6: "六", 7: "七", 8: "八", 9: "九",
                          10: "十", 11: "十一", 12: "十二", 13: "十三", 14: "十四", 15: "十五", 16: "十六", 17: "十七",
