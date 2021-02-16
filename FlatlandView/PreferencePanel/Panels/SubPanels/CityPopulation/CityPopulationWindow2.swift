0bef076ec3243206d004fba501b53f57b0ef4f0a
Stuart Rankin <sjrankin@gmail.com>
2021-02-16 15:36:02 +0900

    Create CityPopulationWindow2.swift
    Placeholder.

---
+++ FlatlandView/PreferencePanel/Panels/SubPanels/CityPopulation/CityPopulationWindow2.swift
@@ -0,0 +1,14 @@
+//
+//  CityPopulationWindow2.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 2/16/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class CityPopulationWindow2: NSWindowController
+{
+}
