1d0dddf99de4199c4cbfd12688740a9b913be205
Stuart Rankin <sjrankin@gmail.com>
2021-03-06 18:03:28 +0900

    Create MemoryUIWindow.swift
    Placeholder.

---
+++ FlatlandView/MainView/DebugUI/Memory/MemoryUIWindow.swift
@@ -0,0 +1,14 @@
+//
+//  MemoryUIWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 3/6/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class MemoryUIWindow: NSWindowController
+{
+}
