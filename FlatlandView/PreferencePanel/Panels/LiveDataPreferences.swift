d3a4d2f5a8b619aa90ae1962f6761fa5146005cd
Stuart Rankin <sjrankin@gmail.com>
2021-03-16 16:13:03 +0900

    Update LiveDataPreferences.swift
    Started implementation.

--- FlatlandView/PreferencePanel/Panels/LiveDataPreferences.swift
+++ FlatlandView/PreferencePanel/Panels/LiveDataPreferences.swift
@@ -19,7 +19,60 @@ class LiveDataPreferences: NSViewController, PreferencePanelProtocol
         super.viewDidLoad()
         
         HelpButtons.append(LiveDataHelpButton)
+        HelpButtons.append(EnableNASATilesHelp)
+        HelpButtons.append(FetchFrequencyHelp)
+        
         SetHelpVisibility(To: Settings.GetBool(.ShowUIHelp))
+    }
+    
+    override func viewDidAppear()
+    {
+        super.viewDidAppear()
+        EnableNASATilesSwitch.state = Settings.GetBool(.EnableNASATiles) ? .on : .off
+        if let LastFetch = Settings.GetDoubleNil(.LastNASAFetchTime)
+        {
+            let Fetched = Date(timeIntervalSince1970: LastFetch)
+            LastFetchTimeField.stringValue = Fetched.PrettyDateTime()
+        }
+        else
+        {
+            LastFetchTimeField.stringValue = "No prior fetch"
+        }
+        NASAFetchCombo.removeAllItems()
+        for FetchWhen in NASAFetchTimes
+        {
+            NASAFetchCombo.addItem(withObjectValue: FetchWhen.1)
+        }
+    }
+    
+    let NASAFetchTimes: [(Int, String)] =
+    [
+        (0, "On demand"),
+        (12, "Every 12 hours"),
+        (18, "Every 18 hours"),
+        (24, "Every 24 hours"),
+        (25, "Every 25 hours")
+    ]
+    
+    @IBAction func HandleEnableNASATilesSwitchChanged(_ sender: Any)
+    {
+        if let Switch = sender as? NSSwitch
+        {
+            Settings.SetBool(.EnableNASATiles, Switch.state == .on ? true : false)
+        }
+    }
+    
+    @IBAction func HandleFetchIntervalChanged(_ sender: Any)
+    {
+        if let Combo = sender as? NSComboBox
+        {
+            let Index = Combo.indexOfSelectedItem
+            if Index > -1
+            {
+                let Value = NASAFetchTimes[Index].0
+                Settings.SetInt(.NASATilesFetchInterval, Value)
+            }
+        }
     }
     
     @IBAction func HandleHelpButtonPressed(_ sender: Any)
@@ -30,6 +83,12 @@ class LiveDataPreferences: NSViewController, PreferencePanelProtocol
             {
                 case LiveDataHelpButton:
                     Parent?.ShowHelp(For: .LiveDataHelp, Where: Button.bounds, What: LiveDataHelpButton)
+                    
+                case EnableNASATilesHelp:
+                    Parent?.ShowHelp(For: .EnableNASATilesHelp, Where: Button.bounds, What: EnableNASATilesHelp)
+                    
+                case FetchFrequencyHelp:
+                    Parent?.ShowHelp(For: .NASATilesFetchFrequencyHelp, Where: Button.bounds, What: FetchFrequencyHelp)
                     
                 default:
                     return
@@ -53,5 +112,10 @@ class LiveDataPreferences: NSViewController, PreferencePanelProtocol
     
     var HelpButtons: [NSButton] = [NSButton]()
     
+    @IBOutlet weak var LastFetchTimeField: NSTextField!
+    @IBOutlet weak var NASAFetchCombo: NSComboBox!
+    @IBOutlet weak var EnableNASATilesSwitch: NSSwitch!
+    @IBOutlet weak var FetchFrequencyHelp: NSButton!
+    @IBOutlet weak var EnableNASATilesHelp: NSButton!
     @IBOutlet weak var LiveDataHelpButton: NSButton!
 }
