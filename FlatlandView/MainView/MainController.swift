9a7f47a3a53763d6be82040a5eb7bd4d9a503aa8
Stuart Rankin <sjrankin@gmail.com>
2021-03-16 16:15:54 +0900

    Update MainController.swift
    Deleted windowDidBecomeMain.

--- FlatlandView/MainView/MainController.swift
+++ FlatlandView/MainView/MainController.swift
@@ -146,33 +146,6 @@ class MainController: NSViewController
         SetWindowDelegate()
     }
     
-    /// Handle the window did become main event.
-    /// - Note:
-    ///   - See [How to set the position and size programmatically](https://stackoverflow.com/questions/32882548/how-to-set-the-position-and-size-programmatically-for-the-main-window)
-    ///   - We need to set the position of the window in this function, *not* viewDidLayout.
-    /// - Parameter notification: Not used.
-    func windowDidBecomeMain(_ notification: Notification)
-    {
-        #if false
-        if !InitialWindowPositionSet
-        {
-            InitialWindowPositionSet = true
-            let MainWindow = self.view.window
-            if let UpperLeft = Settings.GetCGPoint(.WindowOrigin)
-            {
-                let FinalY = NSScreen.main!.frame.height - UpperLeft.y
-                let FinalPoint = CGPoint(x: UpperLeft.x, y: FinalY)
-                MainWindow?.setFrameTopLeftPoint(FinalPoint)
-            }
-            if let ContentsSize = Settings.GetNSSize(.PrimaryViewSize)
-            {
-                Debug.Print("ContentsSize=\(ContentsSize)")
-                //MainWindow?.setContentSize(ContentsSize)
-            }
-        }
-        #endif
-    }
-    
     /// Initialize things that require a fully set-up window.
     override func viewDidLayout()
     {
