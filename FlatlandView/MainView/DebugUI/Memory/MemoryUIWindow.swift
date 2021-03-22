37de33a04ecd56b3057bed157f86de845a6e83bb
Stuart Rankin <sjrankin@gmail.com>
2021-03-22 17:41:02 +0900

    Update MemoryUIWindow.swift
    Added toolbar button handling.

--- FlatlandView/MainView/DebugUI/Memory/MemoryUIWindow.swift
+++ FlatlandView/MainView/DebugUI/Memory/MemoryUIWindow.swift
@@ -11,4 +11,29 @@ import AppKit
 
 class MemoryUIWindow: NSWindowController
 {
+    override func windowDidLoad()
+    {
+        super.windowDidLoad()
+        FullValueCheck.state = .off
+    }
+    
+    var Controller: MemoryUI? = nil
+    
+    @IBAction func HandleFullValueChanged(_ sender: Any)
+    {
+        if let Controller = self.contentViewController as? MemoryUI
+        {
+            Controller.HandleFullValueChanged(ShowFullValue: FullValueCheck.state == .on ? true : false)
+        }
+    }
+    
+    @IBAction func HandleRefreshPressed(_ sender: Any)
+    {
+        if let Controller = self.contentViewController as? MemoryUI
+        {
+            Controller.HandleRefreshButton()
+        }
+    }
+    
+    @IBOutlet weak var FullValueCheck: NSButton!
 }
