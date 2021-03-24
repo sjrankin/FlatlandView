9ffc6832a0bc92a50518fe49ae4e72d56c893e97
Stuart Rankin <sjrankin@gmail.com>
2021-03-24 21:48:49 +0900

    Update SCNTriangle.swift
    Added new initializer for when vertices are used to specify the triangle. Added CastsShadow to control child nodes' shadow casting. Added MakeGeometryWithVertices to use passed vertices to create triangle.s

--- FlatlandView/Shapes/SCNTriangle.swift
+++ FlatlandView/Shapes/SCNTriangle.swift
@@ -3,7 +3,7 @@
 //  FlatlandView
 //
 //  Created by Stuart Rankin on 6/14/20.
-//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//  Copyright © 2020, 2021 Stuart Rankin. All rights reserved.
 //
 
 import Foundation
@@ -30,7 +30,7 @@ class SCNTriangle: SCNNode2, ShapeAttribute
         MakeGeometry()
     }
     
-    /// Initializer.
+    /// Initializer based on the shape of the triangle.
     /// - Parameters:
     ///   - Base: Lenght of the base of the triangle.
     ///   - Height: Height of the triangle.
@@ -38,17 +38,70 @@ class SCNTriangle: SCNNode2, ShapeAttribute
     ///   - Color: Color of the diffuse material.
     ///   - Specular: Color of the specular material.
     ///   - LightMask: The light mask value. Defaults to 0.
+    ///   - UseState: Sets the `UseProtocolToSetState` flag.
     init(Base: CGFloat, Height: CGFloat, Extrusion: CGFloat, Color: NSColor = NSColor.systemYellow,
-         Specular: NSColor = NSColor.white, LightMask: Int = 0)
+         Specular: NSColor = NSColor.white, LightMask: Int = 0, UseState: Bool = true)
     {
         super.init()
-        self.UseProtocolToSetState = true
+        self.UseProtocolToSetState = UseState
         self.Base = Base
         self.Height = Height
         self.Extrusion = Extrusion
         self.Color = Color
         self.Specular = Specular
         self.LightMask = LightMask
+    }
+    
+    /// Initializer based on the vertices of the triangle.
+    /// - Parameters:
+    ///   - Vertex1: First vertex.
+    ///   - Vertex2: Second vertex.
+    ///   - Vertex3: Third vertex.
+    ///   - Extrusion: Depth of the triangle.
+    ///   - Color: Color of the diffuse material.
+    ///   - Specular: Color of the specular material.
+    ///   - LightMask: The light mask value. Defaults to 0.
+    ///   - UseState: Sets the `UseProtocolToSetState` flag. Defaults to `true`.
+    init(Vertex1: NSPoint, Vertex2: NSPoint, Vertex3: NSPoint, Extrusion: CGFloat,
+         Color: NSColor = NSColor.systemYellow, Specular: NSColor = NSColor.white,
+         LightMask: Int = 0, UseState: Bool = true)
+    {
+        super.init()
+        self.UseProtocolToSetState = UseState
+        self.Vertices = [Vertex1, Vertex2, Vertex3]
+        self.Extrusion = Extrusion
+        self.Color = Color
+        self.Specular = Specular
+        self.LightMask = LightMask
+    }
+    
+    /// Holds the set of vertices for the triangle.
+    private var _Vertices: [NSPoint]? = nil
+    {
+        didSet
+        {
+            MakeGeometryWithVertices()
+        }
+    }
+    /// Get or set the triangle's vertices.
+    /// - Warning: If the number of points in the passed array is not 3, a fatal error will be thrown.
+    public var Vertices: [NSPoint]?
+    {
+        get
+        {
+            return _Vertices
+        }
+        set
+        {
+            if let Raw = newValue
+            {
+                if Raw.count != 3
+                {
+                    Debug.FatalError("Incorrect number of vertices set. Must be 3.")
+                }
+            }
+            _Vertices = newValue
+        }
     }
     
     /// Holds the length of the base of the triangle.
@@ -281,10 +334,76 @@ class SCNTriangle: SCNNode2, ShapeAttribute
         }
     }
     
+    private var _CastsShadow: Bool = true
+    public var CastsShadow: Bool
+    {
+        get
+        {
+            return _CastsShadow
+        }
+        set
+        {
+            _CastsShadow = newValue
+            for Child in self.childNodes
+            {
+                Child.castsShadow = newValue
+            }
+        }
+    }
+    
+    /// Create a triangle using the vertex data in `Vertices`. Only the first three points are used. All prior
+    /// shapes are removed.
+    /// - Note: If no vertices have been set, no triangle is created. *This implies setting attributes
+    ///         before setting the vertices will effectively have no result.*
+    func MakeGeometryWithVertices()
+    {
+        for Child in self.childNodes
+        {
+            Child.removeFromParentNode()
+            Child.geometry = nil
+        }
+        if let LocalVertices = Vertices
+        {
+            let Path = NSBezierPath()
+            Path.move(to: LocalVertices[0])
+            Path.line(to: LocalVertices[1])
+            Path.line(to: LocalVertices[2])
+            Path.line(to: LocalVertices[0])
+            Path.close()
+            TriangleVertices = [CGPoint]()
+            TriangleVertices?.append(LocalVertices[0])
+            TriangleVertices?.append(LocalVertices[1])
+            TriangleVertices?.append(LocalVertices[2])
+            let Geo = SCNShape(path: Path, extrusionDepth: _Extrusion)
+            GeoNode = SCNNode2(geometry: Geo)
+            GeoNode.categoryBitMask = _LightMask
+            if let Texture = DiffuseTexture
+            {
+                GeoNode.geometry?.firstMaterial?.diffuse.contents = Texture
+            }
+            else
+            {
+                GeoNode.geometry?.firstMaterial?.diffuse.contents = Color
+            }
+            GeoNode.geometry?.firstMaterial?.specular.contents = Specular
+            GeoNode.geometry?.firstMaterial?.emission.contents = Emission
+            GeoNode.geometry?.firstMaterial?.metalness.contents = Metalness
+            GeoNode.geometry?.firstMaterial?.roughness.contents = Roughness
+            self.addChildNode(GeoNode)
+        }
+    }
+    
     /// Create the geometric triangle and add it to `self`. All prior shapes
     /// are removed first.
+    /// - Note: If vertices are available (in `Vertices`), `MakeGeometryWithVertices` will be called and used
+    ///         instead of the code here.
     func MakeGeometry()
     {
+        if Vertices != nil
+        {
+            MakeGeometryWithVertices()
+            return
+        }
         for Child in self.childNodes
         {
             Child.removeFromParentNode()
@@ -292,9 +411,13 @@ class SCNTriangle: SCNNode2, ShapeAttribute
         }
         let Path = NSBezierPath()
         let HCenter = _Base / 2.0
+        TriangleVertices = [CGPoint]()
         Path.move(to: NSPoint(x: HCenter, y: _Height))
+        TriangleVertices?.append(CGPoint(x: HCenter, y: _Height))
         Path.line(to: NSPoint(x: HCenter + _Base / 2.0, y: 0.0))
+        TriangleVertices?.append(CGPoint(x: HCenter + _Base / 2.0, y: 0.0))
         Path.line(to: NSPoint(x: 0.0, y: 0.0))
+        TriangleVertices?.append((CGPoint.zero))
         Path.line(to: NSPoint(x: HCenter, y: _Height))
         Path.close()
         let Geo = SCNShape(path: Path, extrusionDepth: _Extrusion)
@@ -313,6 +436,16 @@ class SCNTriangle: SCNNode2, ShapeAttribute
         GeoNode.geometry?.firstMaterial?.metalness.contents = Metalness
         GeoNode.geometry?.firstMaterial?.roughness.contents = Roughness
         self.addChildNode(GeoNode)
+    }
+    
+    /// Holds the triangle's vertices.
+    private var TriangleVertices: [CGPoint]? = nil
+    
+    /// Returns an array of triangle vertices.
+    /// - Returns: Array of generated vertices. If no vertices available, nil is returned.
+    func GetVertices() -> [CGPoint]?
+    {
+        return TriangleVertices
     }
     
     var GeoNode = SCNNode2()
