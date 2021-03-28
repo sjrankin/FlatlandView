11756b5c2b29853d2131b089a3727d30c48d3690
Stuart Rankin <sjrankin@gmail.com>
2021-03-28 11:01:08 +0900

    Create EventSoundEditorWindow.swift
    Placeholder

---
+++ FlatlandView/PreferencePanel/Panels/SubPanels/EventSoundEditor/EventSoundEditorWindow.swift
@@ -0,0 +1,14 @@
+//
+//  EventSoundEditorWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 3/27/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class EventSoundEditorWindow: NSWindowController
+{
+}
