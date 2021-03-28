7e19301870e94a74dddc449d89c0ef9800821b5a
Stuart Rankin <sjrankin@gmail.com>
2021-03-28 11:00:46 +0900

    Create EventSettingProtocol.swift
    Protocol for setting edited events.

---
+++ FlatlandView/PreferencePanel/Panels/SubPanels/EventSoundEditor/EventSettingProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  EventSettingProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 3/27/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol EventSettingProtocol: AnyObject
+{
+    func SetEditedEvent(_ Edited: EventRecord)
+}
