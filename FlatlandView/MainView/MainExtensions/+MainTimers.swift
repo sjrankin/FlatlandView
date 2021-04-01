5a2bfb2577021d929338a24fee4fcb2f22f5fe3a
Stuart Rankin <sjrankin@gmail.com>
2021-04-01 20:21:58 +0900

    Update +MainTimers.swift
    Deleted some commented out code.

--- FlatlandView/MainView/MainExtensions/+MainTimers.swift
+++ FlatlandView/MainView/MainExtensions/+MainTimers.swift
@@ -13,107 +13,6 @@ extension MainController
 {
     // MARK: - Main Timer
     
-    #if false
-    /// Usually called once a second. Handles the primary clock and updating of 3D objects.
-    @objc func MainTimerHandler()
-    {
-        let LabelType = Settings.GetEnum(ForKey: .TimeLabel, EnumType: TimeLabels.self, Default: .None)
-        let Now = GetUTC()
-        let Formatter = DateFormatter()
-        Formatter.dateFormat = "HH:mm:ss"
-        var TimeZoneAbbreviation = ""
-        if LabelType == .UTC
-        {
-            TimeZoneAbbreviation = "UTC"
-        }
-        else
-        {
-            TimeZoneAbbreviation = GetLocalTimeZoneID() ?? "UTC"
-        }
-        let TZ = TimeZone(abbreviation: TimeZoneAbbreviation)
-        Formatter.timeZone = TZ
-        var Final = Formatter.string(from: Now)
-        let Parts = Final.split(separator: ":")
-        if !Settings.GetBool(.TimeLabelSeconds)
-        {
-            Final = "\(Parts[0]):\(Parts[1])"
-        }
-        let FinalText = Final + " " + TimeZoneAbbreviation
-        var IsNewHour = false
-        if PreviousHourValue.isEmpty
-        {
-            PreviousHourValue = String(Parts[0])
-        }
-        else
-        {
-            if PreviousHourValue != String(Parts[0])
-            {
-                IsNewHour = true
-                PreviousHourValue = String(Parts[0])
-            }
-        }
-        if LabelType == .None
-        {
-            MainTimeLabelTop.stringValue = ""
-            MainTimeLabelBottom.stringValue = ""
-        }
-        else
-        {
-            MainTimeLabelTop.stringValue = FinalText
-            MainTimeLabelBottom.stringValue = FinalText
-        }
-        
-        let CurrentSeconds = Now.timeIntervalSince1970
-        var ElapsedSeconds = 0
-        if CurrentSeconds != OldSeconds
-        {
-            OldSeconds = CurrentSeconds
-            var Cal = Calendar(identifier: .gregorian)
-            //Use UTC time zone for rotational calculations, not the local time zone (if the user
-            //is using the local zone). All calculations are based on UTC and so if local time zones
-            //are used, the map wil be rotated incorrectly.
-            Cal.timeZone = TimeZone(abbreviation: "UTC")!
-            let Hour = Cal.component(.hour, from: Now)
-            let Minute = Cal.component(.minute, from: Now)
-            let Second = Cal.component(.second, from: Now)
-            ElapsedSeconds = Second + (Minute * 60) + (Hour * 60 * 60)
-            let Percent = Double(ElapsedSeconds) / Double(24 * 60 * 60)
-            let PrettyPercent = Double(Int(Percent * 1000.0)) / 1000.0
-            Main2DView.RotateImageTo(PrettyPercent)
-            #if true
-            if Settings.GetEnum(ForKey: .HourType, EnumType: HourValueTypes.self, Default: .WallClock) == .WallClock
-            {
-                Main3DView?.UpdateWallClockHours(NewTime: Now)
-                Main2DView?.UpdateWallClockHours(NewTime: Now)
-            }
-            #else
-            if Settings.GetEnum(ForKey: .ViewType, EnumType: ViewTypes.self, Default: .Globe3D) == .Globe3D
-            {
-                if Settings.GetEnum(ForKey: .HourType, EnumType: HourValueTypes.self, Default: .WallClock) == .WallClock
-                {
-                    Main3DView?.UpdateWallClockHours(NewTime: Now)
-                    Main2DView?.UpdateWallClockHours(NewTime: Now)
-                }
-            }
-            #endif
-            if Settings.GetBool(.EnableHourEvent)
-            {
-                if Minute == 0 && !HourSoundTriggered
-                {
-                    Main3DView.FlashAllHours(Count: 3)
-                    Main2DView.FlashAllHours(Count: 3)
-                    HourSoundTriggered = true
-                    SoundManager.Play(ForEvent: .HourChime)
-                }
-            }
-            if Minute != 0
-            {
-                HourSoundTriggered = false
-            }
-        }
-    }
-    #endif
-    
     /// Returns the date in UTC time zone. (Given the documentation, returning a new instance of `Date`
     /// is sufficient.)
     func GetUTC() -> Date
@@ -139,18 +38,9 @@ extension MainController
     #if DEBUG
     @objc func DebugTimerHandler()
     {
-        #if true
         let DurationValue = Utility.DurationBetween(Seconds1: CACurrentMediaTime(),
                                                     Seconds2: UptimeStart)
         UptimeValue.stringValue = DurationValue
-        #else
-        if Date.timeIntervalSinceReferenceDate - StartDebugCount >= 1.0
-        {
-            StartDebugCount = Date.timeIntervalSinceReferenceDate
-            UptimeSeconds = UptimeSeconds + 1
-            UptimeValue.stringValue = "\(UptimeSeconds)"
-        }
-        #endif
     }
     #endif
 }
