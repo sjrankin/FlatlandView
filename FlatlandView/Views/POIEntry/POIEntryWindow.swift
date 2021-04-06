afa308d717b0b1e81bce83545476325de9213b0b
Stuart Rankin <sjrankin@gmail.com>
2021-04-06 20:38:27 +0900

    Create POIEntryWindow.swift
    Placeholder.

---
+++ FlatlandView/Views/POIEntry/POIEntryWindow.swift
@@ -0,0 +1,14 @@
+//
+//  POIEntryWindow.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 4/6/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class POIEntryWindow: NSWindowController
+{
+}
