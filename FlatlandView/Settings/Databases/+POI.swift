60325ec9f2c36ce2e5bf4700c07811c16ec4b553
Stuart Rankin <sjrankin@gmail.com>
2021-04-07 14:16:38 +0900

    Update +POI.swift
    Added code to save largest PK in user POI table. Added InsertPOI, UserPOIFor, EditUserPOI, DeleteUserPOI, AddUserPOI

--- FlatlandView/Settings/Databases/+POI.swift
+++ FlatlandView/Settings/Databases/+POI.swift
@@ -54,8 +54,22 @@ extension DBIF
             let UPOI = POI2(Meta: .User, PKID, POIID, Name, Description, Latitude, Longitude,
                             Color, POIType, Numeric, Added, Modified)
             Results.append(UPOI)
+            if PKID > GreatestUserPOIPK
+            {
+                GreatestUserPOIPK = PKID
+            }
         }
         return Results
+    }
+    
+    /// The largest PK found in the user POI table.
+    public static var GreatestUserPOIPK: Int = -1
+    
+    /// Return an unused PK value for inserting into tables.
+    public static func GetUnusuedUserPOIPK() -> Int
+    {
+        GreatestUserPOIPK = GreatestUserPOIPK + 1
+        return GreatestUserPOIPK
     }
     
     /// Returns all built-in points of interest.
@@ -104,8 +118,191 @@ extension DBIF
         return Results
     }
     
+    /// Insert a new POI into the mappable database.
+    /// - Parameter POI: The POI to insert.
+    /// - Parameter Into: The name of the table.
+    public static func InsertPOI(_ POI: POI2, Into Table: String)
+    {
+        if DBIF.MappableHandle == nil
+        {
+            Debug.Print("Call to \(#function) before database initialized.")
+            return
+        }
+        let NextPK = GetUnusuedUserPOIPK()
+        var InsertStatement = "INSERT INTO \(Table) "
+        var Values = "VALUES ("
+        var Names = "(ID,"
+        Values.append("\(NextPK),")
+        Names.append("POIID,")
+        Values.append("\"\(POI.ID.uuidString)\",")
+        Names.append("Name,")
+        Values.append("\"\(POI.Name)\",")
+        Names.append("Latitude,")
+        Values.append("\(POI.Latitude),")
+        Names.append("Longitude,")
+        Values.append("\(POI.Longitude),")
+        if !POI.Description.isEmpty
+        {
+            Names.append("Description,")
+            Values.append("\"\(POI.Description)\",")
+        }
+        Names.append("Numeric,")
+        Values.append("\(POI.Numeric),")
+        Names.append("POIType,")
+        Values.append("\(POI.POIType),")
+        if let Added = POI.Added
+        {
+            Names.append("Added,")
+            Values.append("\"\(Added.PrettyDate())\",")
+        }
+        if let Modified = POI.Modified
+        {
+            Names.append("Modified,")
+            Values.append("\"\(Modified.PrettyDate())\",")
+        }
+        Names.append("Color")
+        Values.append("\"\(POI.Color.Hex)\"")
+        Names.append(")")
+        Values.append(")")
+        InsertStatement.append("\(Names) \(Values);")
+        var InsertHandle: OpaquePointer? = nil
+        guard sqlite3_prepare_v2(DBIF.MappableHandle, InsertStatement, -1, &InsertHandle, nil) == SQLITE_OK else
+        {
+            Debug.Print("Failure in insert statement preparation: \(InsertStatement)")
+            let (Message, Value) = SQL.ExtendedError(From: DBIF.MappableHandle)
+            Debug.Print("  \(Message) [\(Value)]")
+            return
+        }
+        guard sqlite3_step(InsertHandle) == SQLITE_DONE else
+        {
+            Debug.Print("Insert execution failed: \(InsertStatement)")
+            let (Message, Value) = SQL.ExtendedError(From: DBIF.MappableHandle)
+            Debug.Print("  \(Message) [\(Value)]")
+            return
+        }
+    }
+    
+    /// Write the array of user POIs to the database.
+    /// - Important: After calling this function, the user POI table should be reloaded. See `ReloadTables`.
+    /// - Note:
+    ///   - POIs that are not dirty are not modified.
+    ///   - POIs that are marked as `DeleteMe` are removed.
+    ///   - Dirty POIs are updated in place
+    /// - Parameter POIs: Array of user POIs to write.
     public static func SetUserPOIs(_ POIs: [POI2])
     {
-        
+        for POI in POIs
+        {
+            if POI.IsDirty
+            {
+                POI.Saved()
+                var Columns = "SET Name=\"\(POI.Name)\","
+                Columns.append("Color=\"\(POI.Color.Hex)\",")
+                Columns.append("Latitude=\(POI.Latitude),")
+                Columns.append("Longitude=\(POI.Longitude)")
+                let ForRow = "POIID=\"\(POI.ID)\""
+                SQL.UpdateRow(Database: DBIF.MappableHandle!,
+                              Table: POITableNames.UserPOI.rawValue,
+                              Columns: Columns, Where: ForRow)
+                continue
+            }
+            if POI.DeleteMe
+            {
+                let DeletePhrase = "POIID = \(POI.ID.uuidString)"
+                let Result = SQL.DeleteRow(In: DBIF.MappableHandle!,
+                                           Table: POITableNames.UserPOI.rawValue,
+                                           DeletePhrase: DeletePhrase)
+                switch Result
+                {
+                    case .failure(let Why):
+                        Debug.Print("Error deleting POI \(POI.Name): \(Why)")
+                        
+                    default:
+                        break
+                }
+                continue
+            }
+        }
+    }
+    
+    /// Return the user POI for the specified ID.
+    /// - Parameter ID: The ID of the user POI the caller wants.
+    /// - Returns: The user POI with the specified ID on success, nil if not found.
+    public static func UserPOIFor(ID: UUID) -> POI2?
+    {
+        for SomePOI in UserPOIs
+        {
+            if SomePOI.ID == ID
+            {
+                return SomePOI
+            }
+        }
+        return nil
+    }
+    
+    /// Edit an existing user POI.
+    /// - Note:
+    ///   - Changes will be saved immediately.
+    ///   - If the specified ID is not found, no action is taken.
+    /// - Parameter ID: The ID of the POI to edit.
+    /// - Parameter Name: Name of the user POI.
+    /// - Parameter Color: Color for the user POI.
+    /// - Parameter Point: Location of the user POI.
+    public static func EditUserPOI(ID: UUID, Name: String, Color: NSColor, Point: GeoPoint)
+    {
+        for SomePOI in UserPOIs
+        {
+            if SomePOI.ID == ID
+            {
+                SomePOI.Name = Name
+                SomePOI.Color = Color
+                SomePOI.Latitude = Point.Latitude
+                SomePOI.Longitude = Point.Longitude
+                SetUserPOIs(UserPOIs)
+                return
+            }
+        }
+    }
+    
+    /// Delete an existing user POI.
+    /// - Note:
+    ///   - Changes will be saved immediately.
+    ///   - If the ID of the specified user POI is not found, no action is taken.
+    /// - Parameter ID: The ID of the user POI to delete.
+    public static func DeleteUserPOI(ID: UUID)
+    {
+        for SomePOI in UserPOIs
+        {
+            if SomePOI.ID == ID
+            {
+                SomePOI.DeleteMe = true
+                SetUserPOIs(UserPOIs)
+                return
+            }
+        }
+        Debug.Print("Could not delete user POI with ID \(ID)")
+    }
+    
+    /// Adds a new user POI.
+    /// - Note: The updated user POI list will be saved immediately.
+    /// - Parameter Name: Name of the user POI.
+    /// - Parameter Color: Color for the user POI.
+    /// - Parameter Point: Location of the user POI.
+    public static func AddUserPOI(Name: String, Color: NSColor, Point: GeoPoint)
+    {
+        let NewPOI = POI2()
+        NewPOI.MetaType = .User
+        NewPOI.Added = Date()
+        NewPOI.Category = .none
+        NewPOI.Color = Color
+        NewPOI.DeleteMe = false
+        NewPOI.Description = ""
+        NewPOI.ID = UUID()
+        NewPOI.IsNew = true
+        NewPOI.Latitude = Point.Latitude
+        NewPOI.Longitude = Point.Longitude
+        NewPOI.Name = Name
+        NewPOI.Show = true
+        InsertPOI(NewPOI, Into: POITableNames.UserPOI.rawValue)
     }
 }
